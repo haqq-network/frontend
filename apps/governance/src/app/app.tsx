2c08a2c57d8e2c03c7550f10384898d943739cd9
Oleg Shilov <me@olegshilov.com>
2022-09-07 21:22:11 +0300

    fix(governance): add wrapper and change app bg

--- apps/governance/src/app/app.tsx
+++ apps/governance/src/app/app.tsx
@@ -1,20 +1,27 @@
 import styled from '@emotion/styled';
 import { Card, Container } from '@haqq/ui-kit';
+import { ReactNode } from 'react';
 
-const StyledApp = styled.div``;
+const StyledApp = styled.div`
+  background-color: #eef9f5;
+`;
+
+export function AppWrapper({ children }: { children: ReactNode }) {
+  return <div className="flex flex-col min-h-screen">{children}</div>;
+}
 
 export function App() {
   return (
-    <StyledApp className="flex-1">
+    <StyledApp className="flex flex-1 min-h-fit">
       <Container className="py-10">
         <Card>
-          <h1 className="font-medium text-4xl leading-relaxed">
-            Governance app
-          </h1>
+          <div className="flex flex-col space-y-2">
+            <h1 className="font-medium text-4xl leading-relaxed text-center">
+              Governance app
+            </h1>
+          </div>
         </Card>
       </Container>
     </StyledApp>
   );
 }
-
-export default App;
