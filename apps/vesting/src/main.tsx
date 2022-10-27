e937f2e3f7d4a1df50c76d621102e11f3d48250c
Oleg Shilov <me@olegshilov.com>
2022-10-27 20:03:18 +0400

    feat(vesting): integrate sentry

--- apps/vesting/src/main.tsx
+++ apps/vesting/src/main.tsx
@@ -2,7 +2,21 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app/app';
 import { AppContainer } from './app/app-container';
+import { environment } from './environments/environment';
 import './index.css';
+
+if (process.env['NODE_ENV'] === 'production') {
+  const sentryDsn = environment.sentryDsn;
+  if (sentryDsn && sentryDsn !== '') {
+    import('@haqq/sentry').then(({ initSentry }) => {
+      initSentry(sentryDsn, true);
+    });
+  } else {
+    console.warn(
+      'NX_VESTING_SENTRY_DSN is undefined. Sentry is not initialized. Check environments variables',
+    );
+  }
+}
 
 function startApp() {
   const rootElement = document.getElementById('root');
