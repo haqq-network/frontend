28e181404643344f4521a152ba1ca1e910e2286c
Oleg Shilov <me@olegshilov.com>
2022-10-31 03:45:13 +0400

    chore(faucet): bootstrap faucet application

---
+++ apps/faucet/src/app/app.tsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="faucet" />
+    </StyledApp>
+  );
+}
+
+export default App;
