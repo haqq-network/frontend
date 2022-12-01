a3631776ca4761be38594afefab6db233379630f
Oleg Shilov <me@olegshilov.com>
2022-12-01 16:12:34 +0400

    fix(website): change viewport sizes

--- libs/website/ui-kit/src/lib/header/header.tsx
+++ libs/website/ui-kit/src/lib/header/header.tsx
@@ -23,16 +23,16 @@ export function Header() {
   return (
     <header
       className={clsx(
-        'border-t border-b border-haqq-border top-0 sticky h-[63px] md:h-[72px] bg-haqq-black z-50',
+        'border-t border-b border-haqq-border top-0 sticky h-[63px] sm:h-[72px] bg-haqq-black z-50',
       )}
     >
-      <div className="lg:container w-full flex flex-row items-center h-full pr-[12px] md:pr-[64px] lg:pr-[58px] mx-auto">
-        <div className="w-[48px] md:w-[64px] lg:w-[80px] h-full flex items-center justify-center border-r border-haqq-border">
-          <div className="relative w-[26px] h-[26px] md:w-[32px] md:h-[32px]">
+      <div className="w-full flex flex-row items-center h-full pr-[12px] sm:pr-[64px] lg:pr-[58px] mx-auto">
+        <div className="w-[48px] sm:w-[64px] lg:w-[80px] h-full flex items-center justify-center border-r border-haqq-border">
+          <div className="relative w-[26px] h-[26px] sm:w-[32px] sm:h-[32px]">
             <Image src="/logo.svg" alt="HAQQ" fill />
           </div>
         </div>
-        <div className="ml-[12px] md:ml-[20px] lg:ml-[32px] font-serif font-medium text-[20px] md:text-[24px] leading-none">
+        <div className="ml-[12px] sm:ml-[20px] lg:ml-[32px] font-serif font-medium text-[20px] sm:text-[24px] leading-none">
           HAQQ
         </div>
         <div className="flex-1" />
@@ -44,7 +44,7 @@ export function Header() {
           <HeaderNavLink href="/404">404</HeaderNavLink>
         </nav>
         <div className="flex flex-row items-center">
-          <Button className="hidden md:block">Haqq wallet</Button>
+          <Button className="hidden sm:block">Haqq wallet</Button>
           <BurgerButton className="ml-[24px] block lg:hidden" />
         </div>
       </div>
