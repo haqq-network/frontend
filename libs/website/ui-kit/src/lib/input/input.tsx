e2ee34988f97615365ce47b19233ab36324ecdd1
Oleg Shilov <me@olegshilov.com>
2022-12-02 13:29:21 +0400

    fix(website-ui): add required flag for input component

--- libs/website/ui-kit/src/lib/input/input.tsx
+++ libs/website/ui-kit/src/lib/input/input.tsx
@@ -4,14 +4,38 @@ export interface InputProps {
   className?: string;
   placeholder?: string;
   type?: 'text' | 'email';
+  required?: boolean;
+  disabled?: boolean;
 }
 
-export function Input({ placeholder, type = 'text', className }: InputProps) {
+export function Input({
+  placeholder,
+  type = 'text',
+  className,
+  required = false,
+  disabled = false,
+}: InputProps) {
   const classNames = clsx(
     'inline-block pt-[14px] pb-[12px] px-[16px] text-[14px] text-white placeholder-white rounded-[6px] bg-[#252528] leading-[20px] outline-none border border-[#252528]',
     'focus:bg-transparent focus:border-white/50',
     'transition-color duration-150 ease-in',
     className,
   );
-  return <input className={classNames} type={type} placeholder={placeholder} />;
+
+  return (
+    <div className="inline relative">
+      <input
+        className={classNames}
+        type={type}
+        placeholder={placeholder}
+        required={required}
+        disabled={disabled}
+      />
+      {required && (
+        <span className="text-haqq-orange absolute right-[16px] top-0 select-none pointer-events-none">
+          *
+        </span>
+      )}
+    </div>
+  );
 }
