dcbb40f95206f9b7adeededaa86f2db2a071ef16
Oleg Shilov <me@olegshilov.com>
2022-12-02 05:43:34 +0400

    fix(website-ui): set input placeholder color

--- libs/website/ui-kit/src/lib/input/input.tsx
+++ libs/website/ui-kit/src/lib/input/input.tsx
@@ -8,7 +8,7 @@ export interface InputProps {
 
 export function Input({ placeholder, type = 'text', className }: InputProps) {
   const classNames = clsx(
-    'inline-block pt-[14px] pb-[12px] px-[16px] text-[14px] text-white rounded-[6px] bg-[#252528] leading-[20px] outline-none border border-[#252528]',
+    'inline-block pt-[14px] pb-[12px] px-[16px] text-[14px] text-white placeholder-white rounded-[6px] bg-[#252528] leading-[20px] outline-none border border-[#252528]',
     'focus:bg-transparent focus:border-white/50',
     'transition-color duration-150 ease-in',
     className,
