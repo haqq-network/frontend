86d46119983d59fa31a4e00958c3a08803404261
Oleg Shilov <me@olegshilov.com>
2022-12-02 04:18:29 +0400

    feat(website-ui): add input component

---
+++ libs/website/ui-kit/src/lib/input/input.tsx
@@ -0,0 +1,17 @@
+import clsx from 'clsx';
+
+export interface InputProps {
+  className?: string;
+  placeholder?: string;
+  type?: 'text' | 'email';
+}
+
+export function Input({ placeholder, type = 'text', className }: InputProps) {
+  const classNames = clsx(
+    'inline-block pt-[14px] pb-[12px] px-[16px] text-[14px] text-white rounded-[6px] bg-[#252528] leading-[20px] outline-none border border-[#252528]',
+    'focus:bg-transparent focus:border-white/50',
+    'transition-color duration-150 ease-in',
+    className,
+  );
+  return <input className={classNames} type={type} placeholder={placeholder} />;
+}
