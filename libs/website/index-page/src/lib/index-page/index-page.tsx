2adf724e460f5be4588a2489ebc9ac38a32abd37
Oleg Shilov <me@olegshilov.com>
2022-12-04 07:37:21 +0400

    feat(website): add mission block

--- libs/website/index-page/src/lib/index-page/index-page.tsx
+++ libs/website/index-page/src/lib/index-page/index-page.tsx
@@ -2,12 +2,14 @@ import { HeroBlock } from '../hero-block/hero-block';
 import { AboutBlock } from '../about-block/about-block';
 import { ContactBlock } from '../contact-block/contact-block';
 import { PartnershipBlock } from '../partnership-block/partnership-block';
+import { MissionBlock } from '../mission-block/mission-block';
 
 export function WebsiteIndexPage() {
   return (
     <section>
       <HeroBlock />
       <AboutBlock />
+      <MissionBlock />
       <PartnershipBlock />
       <ContactBlock />
     </section>
