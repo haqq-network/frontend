bc7faaeeb53d582f696b0f5eec4ba3436dcda13e
Jihad Hadj Husein <88910027+hadzhehsen@users.noreply.github.com>
2022-12-04 04:40:18 +0300

    fix(website): input error styles (#34)
    Co-authored-by: Oleg Shilov <me@olegshilov.com>

--- libs/website/ui-kit/src/lib/input/input.tsx
+++ libs/website/ui-kit/src/lib/input/input.tsx
@@ -1,40 +1,54 @@
 import clsx from 'clsx';
 
 export interface InputProps {
-  className?: string;
+  inputClassName?: string;
+  wrapperClassName?: string;
   placeholder?: string;
   type?: 'text' | 'email';
   required?: boolean;
   disabled?: boolean;
+  error?: string;
 }
 
 export function Input({
   placeholder,
   type = 'text',
-  className,
   required = false,
   disabled = false,
+  error,
+  inputClassName,
+  wrapperClassName,
 }: InputProps) {
-  const classNames = clsx(
-    'inline-block pt-[14px] pb-[12px] px-[16px] text-[14px] text-white placeholder-white rounded-[6px] bg-[#252528] leading-[20px] outline-none border border-[#252528]',
-    'focus:bg-transparent focus:border-white/50',
+  const inputClassNames = clsx(
+    'inline-block w-full pt-[14px] pb-[12px] px-[16px] text-[14px] text-white placeholder-white rounded-[6px] bg-[#252528] leading-[20px]',
+    'outline-none border border-[#252528]',
+    'focus:bg-transparent focus:border-white/50 focus:text-white',
     'transition-color duration-150 ease-in',
-    className,
+    error && 'text-[#FF5454] bg-[#360C0E] border-[#360C0E]',
+    inputClassName,
+  );
+  const wrapperClassNames = clsx('inline-block', wrapperClassName);
+  const requiredClassNames = clsx(
+    'absolute right-[16px] top-[22px] select-none pointer-events-none',
+    error ? 'text-[#FF5454]' : 'text-haqq-orange',
   );
 
   return (
-    <div className="inline relative">
-      <input
-        className={classNames}
-        type={type}
-        placeholder={placeholder}
-        required={required}
-        disabled={disabled}
-      />
-      {required && (
-        <span className="text-haqq-orange absolute right-[16px] top-0 select-none pointer-events-none">
-          *
-        </span>
+    <div className={wrapperClassNames}>
+      <div className="relative">
+        <input
+          className={inputClassNames}
+          type={type}
+          placeholder={placeholder}
+          required={required}
+          disabled={disabled}
+        />
+        {required && <span className={requiredClassNames}>*</span>}
+      </div>
+      {error && (
+        <div className="block mt-[8px] text-[#FF5454] text-[12px] leading-[16px]">
+          {error}
+        </div>
       )}
     </div>
   );
