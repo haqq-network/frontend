8367f079dd046856bf91293260f3d432200e5b25
Джихад Хадж Хусейн <skiptomylou999@gmail.com>
2022-12-05 18:01:12 +0300

    chore: remove default argument value

--- libs/website/ui-kit/src/lib/pagination-button/pagination-button.tsx
+++ libs/website/ui-kit/src/lib/pagination-button/pagination-button.tsx
@@ -4,14 +4,14 @@ interface PaginationButtonProps {
   className?: string;
   onClick?: () => void;
   active?: boolean;
-  page?: number;
+  page: number;
 }
 
 export function PaginationButton({
   className,
   onClick,
   active,
-  page = 1,
+  page,
 }: PaginationButtonProps) {
   const classNames = clsx(
     ' text-center align-text-bottom w-[140px] h-[30px] pt-[8px] pb-[0.75px] tracking-tighter border-t-[2px] bg-white',
