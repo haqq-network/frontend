eeb63e9c20425339e1d77026cfa1e57800f7e1d2
Джихад Хадж Хусейн <skiptomylou999@gmail.com>
2022-12-05 16:42:11 +0300

    fix: change props

--- libs/website/ui-kit/src/lib/scroll-button/scroll-button.tsx
+++ libs/website/ui-kit/src/lib/scroll-button/scroll-button.tsx
@@ -2,32 +2,35 @@ import clsx from 'clsx';
 
 export interface ArrowButtonProps {
   className?: string;
-  selected?: boolean;
+  disabled?: boolean;
   directionLeft?: boolean;
+  onClick: () => void;
 }
 
 export function ArrowButton({
   className,
-  selected,
+  disabled,
   directionLeft,
+  onClick,
 }: ArrowButtonProps) {
   const classNames = clsx(
-    'py-2 px-5 w-[68.5px] h-6',
+    'py-[8px] px-[20px] w-[68.5px] h-[24px]',
     'border rounded-[40px]',
     'bg-white flex items-center justify-center cursor-pointer',
-    selected ? 'text-black border-black' : 'text-black/30 border-black/30',
+    disabled ? 'text-black/30 border-black/30' : 'text-black border-black',
     directionLeft && 'rotate-180',
     className,
   );
 
   return (
-    <div className={classNames}>
+    <div className={classNames} onClick={onClick}>
       <svg
         width="30"
         height="10"
         viewBox="0 0 30 10"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
+        className="h-"
       >
         <path
           d="M0.183105 5L28.6831 5M28.6831 5L24.6831 8.99975M28.6831 5L24.6831 0.999997"
