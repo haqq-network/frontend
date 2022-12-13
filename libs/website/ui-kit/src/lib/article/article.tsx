e0250c5b5dfccc3e82a878aa65c3b36753c1bf96
Oleg Shilov <me@olegshilov.com>
2022-12-13 14:47:07 +0400

    fix(website): move styles to css module

--- libs/website/ui-kit/src/lib/article/article.tsx
+++ libs/website/ui-kit/src/lib/article/article.tsx
@@ -8,14 +8,7 @@ export interface ArticleProps {
 }
 
 export function Article({ children, className }: ArticleProps) {
-  const classNames = clsx(
-    'prose max-w-[50ch] lg:max-w-[80ch] m-auto',
-    'prose-p:text-[13px] prose-p:md:text-[15px] prose-p:lg:text-base',
-    'prose-a:text-haqq-orange prose-a:no-underline prose-a:cursor-default',
-    'prose-h1:font-serif prose-h1:font-medium prose-h1:leading-[130%] prose-h1:text-[18px] prose-h1:md:text-[24px] prose-h1:lg:text-[32px] prose-h1:mb-[20px] prose-h1:md:mb-[24px] prose-h1:mt-[48xp] prose-h1:md:mt-[64px]',
-    styles['container'],
-    className,
-  );
+  const classNames = clsx('prose mx-auto', styles['container'], className);
 
   return <article className={classNames}>{children}</article>;
 }
