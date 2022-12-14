59e8ef2011e5e903297dfb0476d269e777fe201a
Джихад Хадж Хусейн <skiptomylou999@gmail.com>
2022-12-14 18:09:37 +0300

    fix(website-ui): added classname prop

--- libs/website/ui-kit/src/lib/cookies-banner/cookies-banner.tsx
+++ libs/website/ui-kit/src/lib/cookies-banner/cookies-banner.tsx
@@ -1,9 +1,20 @@
+import clsx from 'clsx';
 import { Button } from '../button/button';
 import { Heading } from '../heading/heading';
 
-export function CookiesBanner({ onClose }: { onClose: () => void }) {
+interface CookiesBannerProps {
+  onClose: () => void;
+  className?: string;
+}
+
+export function CookiesBanner({ onClose, className }: CookiesBannerProps) {
   return (
-    <div className="fixed bg-white flex flex-col sm:flex-row items-center justify-center space-y-[16px] sm:space-x-[24px] sm:space-y-0 w-full mx-auto px-[20px] pb-[24px] pt-[32px] sm:px-[64px] sm:py-[30px]">
+    <div
+      className={clsx(
+        'fixed bg-white flex flex-col sm:flex-row items-center justify-center space-y-[16px] sm:space-x-[24px] sm:space-y-0 w-full px-[20px] pb-[24px] pt-[32px] sm:px-[64px] sm:py-[30px]',
+        className,
+      )}
+    >
       <Heading level={3} className="text-haqq-black text-center">
         Our website uses cookies to ensure maximum user experience.
       </Heading>
