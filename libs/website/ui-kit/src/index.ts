9f7141183e4e07785d5c5f834b30f710eb61aa26
Jihad Hadj Husein <88910027+hadzhehsen@users.noreply.github.com>
2022-12-14 18:17:39 +0300

    feat(website-ui): button loading state (#49)
    * feat(website-ui): added loader
    
    * feat(website-ui): added loading button prop
    
    * chore(website-ui): update button styles

--- libs/website/ui-kit/src/index.ts
+++ libs/website/ui-kit/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/loader/loader';
 export * from './lib/failure-message-modal/failure-message-modal';
 export * from './lib/success-message-modal/success-message-modal';
 export * from './lib/page-title/page-title';
