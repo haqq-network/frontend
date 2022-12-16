4d7a843b7bac37921772b0e85d28dc7d9d637089
Джихад Хадж Хусейн <skiptomylou999@gmail.com>
2022-12-16 22:24:05 +0300

    fix(website): change bg image to svg

--- apps/website/pages/wallet.tsx
+++ apps/website/pages/wallet.tsx
@@ -5,7 +5,7 @@ import Head from 'next/head';
 import clsx from 'clsx';
 import { DiscordIcon, GithubIcon, TwitterIcon } from '@haqq/website/ui-kit';
 import { ElMessiriFont, ManropeFont } from '../lib/fonts';
-import walletImageData from '../assets/images/wallet-bg.png';
+import walletImageData from '../assets/images/wallet-bg.svg';
 import iphoneWalletScreenshotData from '../assets/images/wallet-iphone-screenshot.png';
 
 function FeatureText({ children }: { children: ReactNode }) {
