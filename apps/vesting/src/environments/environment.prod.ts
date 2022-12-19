df2c3effc6af160973689401c93d7fd34837e836
Oleg Shilov <me@olegshilov.com>
2022-12-19 15:10:54 +0400

    fix(vesting): set devault production envs

--- apps/vesting/src/environments/environment.prod.ts
+++ apps/vesting/src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   isProduction: true,
-  chainName: process.env['NX_NETWORK'] ?? 'testedge2',
-  contractAddress: process.env['NX_VESTING_CONTRACT_ADDRESS'],
+  chainName: process.env['NX_NETWORK'] ?? 'mainnet',
+  contractAddress:
+    process.env['NX_VESTING_CONTRACT_ADDRESS'] ??
+    '0x1ba8624B418BFfaA93c369B0841204a9d50fA4D5',
   sentryDsn: process.env['NX_VESTING_SENTRY_DSN'],
 };
