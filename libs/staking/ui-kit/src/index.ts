2f3d7b5fa6b0af67a0f34ae768dfee74af9368bd
Джихад Хадж Хусейн <skiptomylou999@gmail.com>
2022-12-21 22:09:40 +0300

    fix(staking): add active delegated staking table

--- libs/staking/ui-kit/src/index.ts
+++ libs/staking/ui-kit/src/index.ts
@@ -1,1 +1,2 @@
 export * from './components/validator-status/validator-status';
+export * from './components/validators-list/validators-list';
--- libs/staking/validator-list/src/components/staking-validator-list/staking-validator-list.tsx
+++ libs/staking/validator-list/src/components/staking-validator-list/staking-validator-list.tsx
@@ -1,9 +1,79 @@
-import { Fragment } from 'react';
+import { Fragment, useCallback, useMemo } from 'react';
 import { Container, Heading } from '@haqq/ui-kit';
-import { ValidatorsList } from '../validators-list/validators-list';
+import { ValidatorsList } from '@haqq/staking/ui-kit';
 import { StakingInfo } from '@haqq/staking/validator-details';
+import { useQuery } from 'wagmi';
+import { useCosmosService } from '@haqq/providers';
+import { sortValidatorsByToken, splitValidators } from '@haqq/staking/utils';
+import { useAddress } from '@haqq/hooks';
 
 export function StakingValidatorList() {
+  const { getAllValidators, getRewardsInfo, getAccountDelegations } =
+    useCosmosService();
+
+  const {
+    data: validatorsList,
+    error,
+    status,
+  } = useQuery(['validators'], () => {
+    return getAllValidators(1000);
+  });
+
+  const { haqqAddress } = useAddress();
+  const { data: delegationInfo } = useQuery(['delegation', haqqAddress], () => {
+    if (!haqqAddress) {
+      return null;
+    }
+
+    return getAccountDelegations(haqqAddress);
+  });
+
+  const { data: rewardsInfo } = useQuery(['rewards', haqqAddress], () => {
+    if (!haqqAddress) {
+      return null;
+    }
+    console.log({ rewardsInfo });
+    return getRewardsInfo(haqqAddress);
+  });
+
+  const getValidatorRewards = useCallback(
+    (address: string) => {
+      const rewards = rewardsInfo?.rewards?.find((rewardsItem: any) => {
+        return rewardsItem.validator_address === address;
+      });
+
+      return rewards;
+    },
+    [rewardsInfo?.rewards],
+  );
+
+  const sortedAllValidators = useMemo(() => {
+    const { active, inactive, jailed } = splitValidators(validatorsList);
+    const sortedVals = [
+      ...sortValidatorsByToken(active),
+      ...sortValidatorsByToken(inactive),
+      ...sortValidatorsByToken(jailed),
+    ];
+
+    return sortedVals;
+  }, [validatorsList]);
+
+  const sortedDelegatedValidators = useMemo(() => {
+    const { active, inactive, jailed } = splitValidators(validatorsList);
+    const sortedVals = [
+      ...sortValidatorsByToken(active),
+      ...sortValidatorsByToken(inactive),
+      ...sortValidatorsByToken(jailed),
+    ];
+    const filteredVals = sortedVals.filter((el) => {
+      const rewardsInfo = getValidatorRewards(el.operator_address);
+      return rewardsInfo;
+    });
+    console.log({ filteredVals });
+
+    return filteredVals;
+  }, [validatorsList]);
+
   return (
     <Fragment>
       <Container>
@@ -13,12 +83,19 @@ export function StakingValidatorList() {
         <StakingInfo />
       </Container>
 
-      <Container className="flex flex-1 flex-col">
-        <Heading level={3} className="mb-4">
-          All validators
-        </Heading>
-
-        <ValidatorsList />
+      <Container className="flex flex-1 flex-col space-y-4">
+        <ValidatorsList
+          validators={sortedDelegatedValidators}
+          error={error}
+          status={status}
+          title="Active Delegation"
+        />
+        <ValidatorsList
+          validators={sortedAllValidators}
+          error={error}
+          status={status}
+          title="Validators"
+        />
       </Container>
     </Fragment>
   );
