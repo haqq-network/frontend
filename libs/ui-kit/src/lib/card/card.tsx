668fc411f814804753ca1fe30f6317212c812872
Oleg Shilov <me@olegshilov.com>
2022-12-26 08:08:17 +0400

    fix(ui-kit): disable card background blur, decrease border radius

--- libs/ui-kit/src/lib/card/card.tsx
+++ libs/ui-kit/src/lib/card/card.tsx
@@ -13,8 +13,8 @@ export function Card({
   return (
     <div
       className={clsx(
-        'relative p-6 shadow-md ring-1',
-        'backdrop-blur transform-gpu',
+        'relative p-6 shadow-sm ring-1',
+        // 'backdrop-blur-sm transform-gpu',
         'bg-white/50 ring-gray-300/10 shadow-slate-300/20',
         'dark:bg-slate-700/40 dark:ring-gray-500/10 dark:shadow-slate-600/10',
         { 'rounded-xl': rounded },
