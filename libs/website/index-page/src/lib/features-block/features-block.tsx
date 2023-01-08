37bfc02b9fcca1aad145de177a55a3920e984e80
Джихад Хадж Хусейн <skiptomylou999@gmail.com>
2023-01-08 05:43:38 +0300

    fix(website): feature block layout

--- libs/website/index-page/src/lib/features-block/features-block.tsx
+++ libs/website/index-page/src/lib/features-block/features-block.tsx
@@ -73,18 +73,18 @@ export function FeaturesBlock() {
   };
 
   return (
-    <div className="bg-white flex flex-col">
+    <div className="bg-white flex flex-col border-b">
       <div
-        className="text-black pl-[16px] pr-[32px] ml-[16px] sm:pl-0 sm:ml-[63px] sm:mr-0 sm:pr-0 lg:ml-[79px] border-l border-[#0D0D0E/24] h-[702px] sm:h-[850px] lg:h-[863px] pb-[60px] sm:pb-0"
+        className="text-black pl-[16px] pr-[32px] ml-[16px] sm:pl-0 sm:ml-[63px] sm:mr-0 sm:pr-0 lg:ml-[79px] border-l border-[#0D0D0E/24] pb-[60px] sm:pb-0"
         id="features"
       >
         <div className="flex flex-col">
-          <div className="flex flex-col sm:flex-row">
-            <div className="pt-[60px] sm:pt-[96px] lg:pt-[121px] sm:pl-[20px] lg:pl-[32px] sm:w-[241px] lg:w-[289px] sm:space-x-[40px] lg:space-x-[305px]">
+          <div className="flex flex-col sm:flex-row mb-[32px] sm:mb-[40px] sm:space-x-[40px] lg:space-x-[305px]">
+            <div className="pt-[60px] sm:pt-[96px] lg:pt-[121px] sm:pl-[20px] lg:pl-[32px] sm:w-[241px] lg:w-[289px]">
               <Heading level={2}>Features for ethical builders</Heading>
             </div>
-            <div className="pt-[16px] sm:pt-[100px] lg:pt-[132px] sm:pl-0 lg:pl-[32px] sm:pr-[64px] lg:pr-[199px] sm:border-l border-haqq-border flex-1 ">
-              <Text className="block mb-[32px] sm:mb-[40px]">
+            <div className="pt-[16px] sm:pt-[100px] lg:pt-[132px] sm:pr-[64px] xl:pr-[199px]  flex-1 ">
+              <Text className="block sm:ml-[20px] lg:ml-[32px]">
                 HAQQ is a fast Proof-of-stake network built with the Tendering
                 consensus engine, with fast finality. It means that transaction
                 effects become irreversible and can't be altered immediately
@@ -110,7 +110,7 @@ export function FeaturesBlock() {
                 </div>
               ))}
             </div>
-            <div className="flex sm:ml-[-100px] lg:ml-[-178px]">
+            <div className="flex sm:ml-[-100px] lg:ml-[-178px] mb-[60px] sm:mb-[80px] lg:mb-[100px]">
               {FeaturesData.map((el, index) => (
                 <PaginationButton
                   key={el.id}
