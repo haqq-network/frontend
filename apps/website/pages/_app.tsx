f4272b4e50cabf1869222e9a6f11cb9bf859814e
Oleg Shilov <me@olegshilov.com>
2023-01-09 21:42:49 +0700

    fix(website): use correct imports for fonts

--- apps/website/pages/_app.tsx
+++ apps/website/pages/_app.tsx
@@ -2,14 +2,11 @@ import { Fragment, ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import clsx from 'clsx';
-import {
-  ClashDisplayFont,
-  HKGuiseFont,
-} from '../../../libs/website/shared/src/lib/fonts';
 import '../styles/global.css';
 import Head from 'next/head';
 import { Header } from '../components/header/header';
 import { Footer } from '../components/footer/footer';
+import { ClashDisplayFont, HKGuiseFont } from '@haqq/website/shared';
 
 export type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
