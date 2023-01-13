4849e58f2b2d8348fc912d0121f292bb3dcb4937
Джихад Хадж Хусейн <skiptomylou999@gmail.com>
2023-01-13 04:59:55 +0300

    fix(website): add priority prop to feature img

--- libs/website/ui-kit/src/lib/feature-card/feature-card.tsx
+++ libs/website/ui-kit/src/lib/feature-card/feature-card.tsx
@@ -18,7 +18,7 @@ export function FeatureCard({
   return (
     <div className="flex flex-col w-[283px] sm:w-[332px] lg:w-[684px]">
       <div className="relative bg-[#262629] h-[140px] sm:h-[155px] lg:h-[200px]  overflow-hidden">
-        <Image src={img} alt="img" fill className={imgClassName} />
+        <Image src={img} alt="img" fill className={imgClassName} priority />
       </div>
       <div className="bg-black text-white items-start flex flex-col lg:flex-row h-[213px] sm:h-[245px] lg:h-[174px] p-[24px] sm:pr-[43px] lg:pt-[16px] lg:pr-[40px] lg:pb-[40px] lg:pl-[16px]">
         <div className="lg:w-2/5">
