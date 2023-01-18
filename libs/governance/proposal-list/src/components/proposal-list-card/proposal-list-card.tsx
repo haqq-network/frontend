e132e49d3997732d9b81958d18436786b1b5fb20
Джихад Хадж Хусейн <skiptomylou999@gmail.com>
2023-01-18 06:51:51 +0300

    feat(governance-ui): add new proposal list card

--- libs/governance/proposal-list/src/components/proposal-list-card/proposal-list-card.tsx
+++ libs/governance/proposal-list/src/components/proposal-list-card/proposal-list-card.tsx
@@ -1,11 +1,44 @@
-import { ReactElement, useMemo } from 'react';
+import { ReactElement, useEffect, useMemo, useState } from 'react';
 import { Proposal, ProposalStatus } from '@evmos/provider';
-import { Card, CardHeading } from '@haqq/ui-kit';
+import {
+  Card,
+  CardHeading,
+  NewCard,
+  NewCardHeading,
+  NewCardSubText,
+  NewCardText,
+  ProposalNumber,
+  TimerText,
+} from '@haqq/ui-kit';
 import clsx from 'clsx';
 
 export interface ProposalListCardProps {
   proposal: Proposal;
 }
+
+interface RemainingTime {
+  days: number;
+  hours: number;
+  minutes: number;
+}
+
+interface DateTimeFormatOptions {
+  year?: 'numeric' | undefined;
+  month?: 'short' | undefined;
+  day?: '2-digit' | undefined;
+  hour?: '2-digit' | undefined;
+  minute?: '2-digit' | undefined;
+  hour12?: boolean | undefined;
+}
+
+const dateOptions: DateTimeFormatOptions = {
+  year: 'numeric',
+  month: 'short',
+  day: '2-digit',
+  hour: '2-digit',
+  minute: '2-digit',
+  hour12: false,
+};
 
 export function ProposalStatusComponent({ status }: { status: string }) {
   const baseClassName =
@@ -111,7 +144,7 @@ export function NewProposalStatusComponent({
   };
 }): ReactElement {
   const baseClassName =
-    'inline-flex rounded-lg px-2 py-[11px] space-x-1 font-medium items-center uppercase text-sm';
+    'inline-flex rounded-lg px-2 py-[11px] space-x-1 items-center uppercase text-sm leading-[0.01em]';
   const { yes, abstain, no, veto, total } = useMemo(() => {
     const yes = Number.parseInt(results.yes) / 10 ** 18;
     const abstain = Number.parseInt(results.abstain) / 10 ** 18;
@@ -154,7 +187,7 @@ export function NewProposalStatusComponent({
             />
           </svg>
 
-          <div>Rejected</div>
+          <div>Reject</div>
         </div>
       );
     case ProposalStatus.Passed:
@@ -214,6 +247,153 @@ export function NewProposalStatusComponent({
   }
 }
 
+export function NewProposalVoteResults({
+  results,
+  voteOption = 'YES',
+}: {
+  results: {
+    yes: string;
+    abstain: string;
+    no: string;
+    no_with_veto: string;
+  };
+  voteOption?: string;
+}): ReactElement {
+  const { yes, abstain, no, veto, total } = useMemo(() => {
+    const yes = Number.parseInt(results.yes) / 10 ** 18;
+    const abstain = Number.parseInt(results.abstain) / 10 ** 18;
+    const no = Number.parseInt(results.no) / 10 ** 18;
+    const veto = Number.parseInt(results.no_with_veto) / 10 ** 18;
+
+    return {
+      yes,
+      abstain,
+      no,
+      veto,
+      total: yes + abstain + no + veto,
+    };
+  }, [results]);
+  const [yesPercents, abstainPercents, noPercents, vetoPercents] =
+    useMemo(() => {
+      return [
+        (yes / total) * 100,
+        (no / total) * 100,
+        (abstain / total) * 100,
+        (veto / total) * 100,
+      ];
+    }, [yes, abstain, no, veto, total]);
+  return (
+    <div className="flex flex-col space-y-2 w-full">
+      <div className="flex space-x-[12px] items-center">
+        <NewCardText className="text-[13px] leading-[20px] lg:text-[16px] lg:leading-[26px]">
+          Vote results
+        </NewCardText>
+        <div className="inline-flex space-x-[6px]">
+          <NewCardSubText className="text-white/50">You Voted:</NewCardSubText>
+          <NewCardSubText
+            className={clsx(
+              voteOption === 'YES' && 'text-[#01B26E]',
+              voteOption === 'NO' && 'text-white/50',
+            )}
+          >
+            {voteOption}
+          </NewCardSubText>
+        </div>
+      </div>
+
+      <div className="space-x-[4px] text-xs w-full h-2 relative flex flex-row overflow-hidden justify-between">
+        <div
+          className={clsx(
+            'h-full bg-[#01B26E] rounded-xl',
+            yesPercents === 0 && 'hidden',
+          )}
+          style={{
+            width: `${yesPercents}%`,
+          }}
+        />
+        <div
+          className={clsx(
+            'h-full bg-[#FF5454] rounded-xl',
+            noPercents === 0 && 'hidden',
+          )}
+          style={{ width: `${noPercents}%` }}
+        />
+        <div
+          className={clsx(
+            'h-full bg-[#AAABB2] rounded-xl',
+            abstainPercents === 0 && 'hidden',
+          )}
+          style={{ width: `${abstainPercents}%` }}
+        />
+        <div
+          className={clsx(
+            'h-full bg-[#E3A13F] rounded-xl',
+            vetoPercents === 0 && 'hidden',
+          )}
+          style={{ width: `${vetoPercents}%` }}
+        />
+      </div>
+      <div className="flex items-center space-x-[12px]">
+        <div className="flex items-center">
+          <div className="flex items-center">
+            <div className="h-2 w-2 rounded-full bg-[#01B26E] mr-1" />
+            <div className="mr-[2px]">
+              <NewCardText className="text-[12px] leading-[1.5em] lg:text-[14px] lg:leading-[22px]">
+                Yes
+              </NewCardText>
+            </div>
+            <NewCardText className="text-[12px] leading-[1.5em] lg:text-[14px] lg:leading-[22px]">
+              {yesPercents ? yesPercents.toFixed(0) : 0}%
+            </NewCardText>
+          </div>
+        </div>
+
+        <div className="flex items-center">
+          <div className="flex items-center">
+            <div className="h-2 w-2 rounded-full bg-[#FF5454] mr-1" />
+            <div className="mr-[2px]">
+              <NewCardText className="text-[12px] leading-[1.5em] lg:text-[14px] lg:leading-[22px]">
+                No
+              </NewCardText>
+            </div>
+          </div>
+          <NewCardText className="text-[12px] leading-[1.5em] lg:text-[14px] lg:leading-[22px]">
+            {noPercents ? noPercents.toFixed(0) : 0}%
+          </NewCardText>
+        </div>
+
+        <div className="flex items-center">
+          <div className="flex items-center">
+            <div className="h-2 w-2 rounded-full bg-[#AAABB2] mr-1" />
+            <div className="mr-[2px]">
+              <NewCardText className="text-[12px] leading-[1.5em] lg:text-[14px] lg:leading-[22px]">
+                Abstain
+              </NewCardText>
+            </div>
+            <NewCardText className="text-[12px] leading-[1.5em] lg:text-[14px] lg:leading-[22px]">
+              {abstainPercents ? abstainPercents.toFixed(0) : 0}%
+            </NewCardText>
+          </div>
+        </div>
+
+        <div className="flex items-center">
+          <div className="flex flex-row items-center">
+            <div className="h-2 w-2 rounded-full bg-yellow-500 mr-1" />
+            <div className="mr-[2px]">
+              <NewCardText className="text-[12px] leading-[1.5em] lg:text-[14px] lg:leading-[22px]">
+                Veto
+              </NewCardText>
+            </div>
+            <NewCardText className="text-[12px] leading-[1.5em] lg:text-[14px] lg:leading-[22px]">
+              {vetoPercents ? vetoPercents.toFixed(0) : 0}%
+            </NewCardText>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export function ProposalVoteResults({
   results,
 }: {
@@ -253,7 +433,7 @@ export function ProposalVoteResults({
       <div>
         <CardHeading>Vote results</CardHeading>
       </div>
-      <div className="w-full rounded-full h-4 relative flex flex-row overflow-hidden border bg-slate-400/10 dark:border-slate-400/10">
+      <div className="text-xs w-full rounded-full h-4 relative flex flex-row overflow-hidden border bg-slate-400/10 dark:border-slate-400/10">
         <div
           className="h-full bg-opacity-80 bg-green-500"
           style={{ width: `${yesPercents}%` }}
@@ -323,10 +503,6 @@ export function ProposalListCard({ proposal }: ProposalListCardProps) {
           #{proposal.proposal_id}
         </div>
         <ProposalStatusComponent status={proposal.status as string} />
-        <NewProposalStatusComponent
-          status={proposal.status as string}
-          results={proposal.final_tally_result}
-        />
       </div>
 
       <div className="font-semibold text-xl min-h-[48px] leading-[24px]">
@@ -346,5 +522,138 @@ export function ProposalListCard({ proposal }: ProposalListCardProps) {
 
       <ProposalVoteResults results={proposal.final_tally_result} />
     </Card>
+  );
+}
+
+export function NewProposalListCard({
+  proposal,
+}: ProposalListCardProps): ReactElement {
+  const startDate = useMemo(() => {
+    return new Date(proposal.voting_start_time).toLocaleString(
+      'en-US',
+      dateOptions,
+    );
+  }, [proposal]);
+  const endDate = useMemo(() => {
+    return new Date(proposal.voting_end_time).toLocaleString(
+      'en-US',
+      dateOptions,
+    );
+  }, [proposal]);
+
+  const [remainingTime, setRemainingTime] = useState<RemainingTime>({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+  });
+  const [currentTime, setCurrentTime] = useState<number>(Date.now());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      const end = new Date(endDate);
+      const diff = end.getTime() - now.getTime();
+      const remaining = {
+        days: Math.floor(diff / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((diff / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((diff / (1000 * 60)) % 60),
+      };
+      setRemainingTime(remaining);
+      setCurrentTime(Number(now));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [endDate]);
+
+  return (
+    <NewCard>
+      <div className="flex flex-col items-start">
+        <div className="space-y-[16px]">
+          <div className="flex space-x-[18px] font-clash items-center">
+            <ProposalNumber>#{proposal.proposal_id}</ProposalNumber>
+            <NewProposalStatusComponent
+              status={proposal.status as string}
+              results={proposal.final_tally_result}
+            />
+          </div>
+          <NewCardHeading>{proposal.content.title}</NewCardHeading>
+        </div>
+
+        <div className="my-3 lg:my-5 border-dashed border-y border-y-[#ffffff26] py-[13px] lg:py-[22px] w-full">
+          {currentTime <= new Date(proposal.deposit_end_time).getTime() ? (
+            <div className="flex space-x-[12px]">
+              <div
+                className={clsx(
+                  ProposalStatus.Deposit && 'text-[#0489D4]',
+                  ProposalStatus.Voting && 'text-[#01B26E]',
+                )}
+              >
+                <svg width="44" height="44" viewBox="0 0 44 44" fill="none">
+                  <g clip-path="url(#clip0_140_2817)">
+                    <circle
+                      cx="22"
+                      cy="22"
+                      r="21"
+                      stroke="white"
+                      stroke-opacity="0.24"
+                      stroke-width="2"
+                      stroke-dasharray="2 4"
+                    />
+                    <path
+                      d="M42.7716 19.6277C43.3716 19.5592 43.9163 19.9901 43.9549 20.5927C44.2202 24.7302 43.311 28.8669 41.3211 32.5211C39.1566 36.496 35.8189 39.7066 31.7629 41.7151C27.707 43.7236 23.1302 44.4323 18.6568 43.7445C14.1833 43.0567 10.0309 41.0059 6.76559 37.8718C3.50032 34.7376 1.28117 30.6726 0.410706 26.2311C-0.459755 21.7896 0.0608473 17.1877 1.90153 13.0529C3.74221 8.91803 6.81338 5.45154 10.6963 3.12605C14.2659 0.98817 18.3619 -0.0897354 22.5068 0.0058304C23.1105 0.0197487 23.5633 0.546388 23.5194 1.14863V1.14863C23.4756 1.75086 22.9516 2.20033 22.3478 2.18972C18.652 2.12478 15.0035 3.0953 11.8198 5.00201C8.32284 7.09637 5.55693 10.2183 3.8992 13.9422C2.24148 17.666 1.77262 21.8105 2.55656 25.8106C3.3405 29.8106 5.33909 33.4716 8.27981 36.2942C11.2205 39.1168 14.9603 40.9638 18.9891 41.5832C23.0179 42.2026 27.1397 41.5644 30.7925 39.7555C34.4454 37.9467 37.4514 35.0552 39.4007 31.4754C41.1754 28.2163 41.9957 24.5311 41.7794 20.841C41.7441 20.2382 42.1717 19.6962 42.7716 19.6277V19.6277Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M18 13.5H26V11.5H18V13.5ZM22 30.5C25.866 30.5 29 27.366 29 23.5C29 19.634 25.866 16.5 22 16.5C18.134 16.5 15 19.634 15 23.5C15 27.366 18.134 30.5 22 30.5ZM22 32.5C26.9706 32.5 31 28.4706 31 23.5C31 18.5294 26.9706 14.5 22 14.5C17.0294 14.5 13 18.5294 13 23.5C13 28.4706 17.0294 32.5 22 32.5ZM21 23V18H23V22.2792L26.8162 23.5513L26.1838 25.4487L21.6838 23.9487L21 23.7208V23Z"
+                      fill="currentColor"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_140_2817">
+                      <rect width="44" height="44" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+              </div>
+
+              <div className="flex flex-col items-start">
+                <NewCardSubText>Voting End</NewCardSubText>
+                <div className="inline-flex space-x-[8px]">
+                  <div className="inline-flex space-x-[4px]">
+                    <TimerText color="white">{remainingTime.days}</TimerText>
+                    <TimerText>days</TimerText>
+                  </div>
+                  <div className="inline-flex space-x-[4px]">
+                    <TimerText color="white">{remainingTime.minutes}</TimerText>
+                    <TimerText>hours</TimerText>
+                  </div>
+                  <div className="inline-flex space-x-[4px]">
+                    <TimerText color="white">{remainingTime.minutes}</TimerText>
+                    <TimerText>min</TimerText>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className="flex space-x-[32px]">
+              <div className="flex flex-col items-start">
+                <NewCardSubText>Voting start</NewCardSubText>
+                <NewCardText className="text-[12px] leading-[1.5em] lg:text-[14px] lg:leading-[22px]">
+                  {startDate}
+                </NewCardText>
+              </div>
+              <div className="flex flex-col items-start">
+                <NewCardSubText>Voting end</NewCardSubText>
+                <NewCardText className="text-[12px] leading-[1.5em] lg:text-[14px] lg:leading-[22px]">
+                  {endDate}
+                </NewCardText>
+              </div>
+            </div>
+          )}
+        </div>
+        <NewProposalVoteResults results={proposal.final_tally_result} />
+      </div>
+    </NewCard>
   );
 }
