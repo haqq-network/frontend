181b5b12e19d57dd8a4b39e8e8c39b62ff18da73
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-01-19 23:23:48 +0300

    fix(shell): use my account block

--- libs/shell/index-page/src/components/index-page/index-page.tsx
+++ libs/shell/index-page/src/components/index-page/index-page.tsx
@@ -3,13 +3,14 @@ import { ShellIndexPageAccountInfo } from '../account-info/account-info';
 import { ShellIndexPageChainStats } from '../chain-stats/chain-stats';
 import { ShellIndexPageDelegationList } from '../delegation-list/delegation-list';
 import { ShellIndexPageProposalList } from '../proposal-list/proposal-list';
-import { StatisticsBlock } from '@haqq/shell/index-page';
+import { MyAccountBlock, StatisticsBlock } from '@haqq/shell/index-page';
 
 export function ShellIndexPage() {
   return (
     <Container className="py-10">
       <div className="flex flex-col space-y-6">
         <StatisticsBlock />
+        <MyAccountBlock />
         <div className="flex flex-col md:flex-row md:space-x-6 md:space-y-0 space-y-6">
           <div className="md:flex-1 flex flex-col">
             <Heading level={3} className="mb-4">
