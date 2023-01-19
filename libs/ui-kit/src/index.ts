a67bdb2bdab09daf262988eb8bfb858eae37441f
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-01-19 07:13:31 +0300

    fix(ui-kit): add clipboard button

--- libs/ui-kit/src/index.ts
+++ libs/ui-kit/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/clipboard-button/clipboard-button';
 export * from './lib/new-card/new-card';
 export * from './lib/pending-page/pending-page';
 export * from './lib/modal/modal';
