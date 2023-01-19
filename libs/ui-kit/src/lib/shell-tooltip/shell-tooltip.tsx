80d3e0f702d91353a4763c27d1a91d6ae07cc96e
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-01-20 00:11:36 +0300

    fix(ui-kit): add tooltip bg color

--- libs/ui-kit/src/lib/shell-tooltip/shell-tooltip.tsx
+++ libs/ui-kit/src/lib/shell-tooltip/shell-tooltip.tsx
@@ -20,12 +20,12 @@ export function ShellTooltip({
       <div
         className={clsx(
           'flex flex-col invisible absolute w-max mb-2 opacity-0 max-w-[240px] break-words',
-          'bottom-full',
+          'bottom-full bg-black',
           'transition ease-out duration-100 delay-75',
           'group-hover:visible group-hover:z-50 group-hover:opacity-100 group-hover:translate-y-0 group-hover:scale-100',
-          'p-2 rounded-[4px] text-[12px] leading-[1.5em]',
+          'p-2 pb-1 rounded-[4px] text-[12px] leading-[1.5em]',
           'text-white border border-[#ffffff26]',
-          isCopied ? 'left-1/3' : '-left-1/3',
+          isCopied && 'left-1/3',
           className,
         )}
       >
