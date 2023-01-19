287d8597189c2d129481ea1501fa4520e0321529
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-01-19 05:01:41 +0300

    fix(shell): use statistics block

--- libs/shell/index-page/src/components/index-page/index-page.tsx
+++ libs/shell/index-page/src/components/index-page/index-page.tsx
@@ -3,11 +3,13 @@ import { ShellIndexPageAccountInfo } from '../account-info/account-info';
 import { ShellIndexPageChainStats } from '../chain-stats/chain-stats';
 import { ShellIndexPageDelegationList } from '../delegation-list/delegation-list';
 import { ShellIndexPageProposalList } from '../proposal-list/proposal-list';
+import { StatisticsBlock } from '../../lib/statistics-block/statistics-block';
 
 export function ShellIndexPage() {
   return (
     <Container className="py-10">
       <div className="flex flex-col space-y-6">
+        <StatisticsBlock />
         <div className="flex flex-col md:flex-row md:space-x-6 md:space-y-0 space-y-6">
           <div className="md:flex-1 flex flex-col">
             <Heading level={3} className="mb-4">
