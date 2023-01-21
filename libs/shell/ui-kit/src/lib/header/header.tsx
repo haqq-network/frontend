096fb51de6a0c47df6845825bdd081419f3b83fa
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-01-21 05:51:53 +0300

    fix(shell-ui): add balance button to header

--- libs/shell/ui-kit/src/lib/header/header.tsx
+++ libs/shell/ui-kit/src/lib/header/header.tsx
@@ -1,11 +1,17 @@
-import { Fragment, ReactNode, useCallback, useState } from 'react';
-import Image from 'next/image';
+import {
+  Fragment,
+  ReactElement,
+  ReactNode,
+  useCallback,
+  useMemo,
+  useState,
+} from 'react';
 import clsx from 'clsx';
-import Link from 'next/link';
-import logoImageData from '../../assets/images/logo.svg';
 import { Button, BurgerButton } from '@haqq/website/ui-kit';
 import ScrollLock from 'react-scrolllock';
-import { BurgerMenu } from '@haqq/shell/ui-kit';
+import { AccountButton, BurgerMenu } from '@haqq/shell/ui-kit';
+import { getFormattedAddress, useAddress, useMetamask } from '@haqq/shared';
+import { useAccount, useBalance } from 'wagmi';
 
 function HeaderNavLink({
   href,
@@ -23,17 +29,69 @@ function HeaderNavLink({
       }
     : {};
   return (
-    <Link
+    <a
       href={href}
       className="text-[13px] leading-[20px] sm:text-[15px] sm:leading-[24px]"
       {...additionalProps}
     >
       {children}
-    </Link>
+    </a>
   );
 }
 
+const BurgerMenuBalanceButton = ({
+  balance,
+  address,
+}: {
+  balance?: {
+    symbol: string;
+    value: number;
+  };
+  address: string | undefined;
+}): ReactElement => {
+  return (
+    <div className="flex flex-row rounded-[6px] max-w-fit outline outline-white">
+      {balance && (
+        <div className="font-clash tracking-[.01em] leading-[24px] pl-[12px] py-[8px] pr-[8px]">
+          {`${balance.value.toLocaleString()} ${balance.symbol.toLocaleUpperCase()}`}
+        </div>
+      )}
+      <div className="">
+        <button
+          className={clsx(
+            'flex flex-row items-center space-x-2 rounded-[6px] px-3 py-[9px]',
+            'cursor-pointer bg-white hover:bg-white/80 active:bg-white text-black',
+            'transition-all duration-150 ease-in',
+            'box-border appearance-none outline-none',
+          )}
+        >
+          <div className="text-sm leading-[24px]">
+            {getFormattedAddress(address, 3, 2)}
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+};
+
 export function Header() {
+  const { connect, disconnect } = useMetamask();
+  const { ethAddress } = useAddress();
+  const { isConnected } = useAccount();
+  const { data: balanceData } = useBalance({
+    address: ethAddress,
+    watch: true,
+  });
+  const balance = useMemo(() => {
+    if (!balanceData) {
+      return undefined;
+    }
+
+    return {
+      symbol: balanceData.symbol,
+      value: Number.parseFloat(balanceData.formatted),
+    };
+  }, [balanceData]);
   const [isBurgerMenuOpen, setBurgerMenuOpen] = useState(false);
 
   const handleMenuOpen = useCallback(() => {
@@ -52,28 +110,36 @@ export function Header() {
     >
       <div className="w-full flex flex-row items-center h-full pr-[16px] sm:pr-[64px] lg:pr-[80px] mx-auto">
         <div className="w-[48px] sm:w-[64px] lg:w-[80px] h-full flex items-center justify-center border-r border-[#464647]">
-          <div className="relative w-[26px] h-[26px] sm:w-[32px] sm:h-[32px]">
-            <Image src={logoImageData} alt="HAQQ" fill />
-          </div>
+          <HeaderNavLink href="/">
+            <div className="relative w-[26px] h-[26px] sm:w-[32px] sm:h-[32px]">
+              <img src="./assets/logo.svg" alt="HAQQ" />
+            </div>
+          </HeaderNavLink>
         </div>
-        <div className="ml-[12px] sm:ml-[20px] lg:ml-[32px] font-serif font-medium text-[20px] sm:text-[24px] leading-none">
+        <div className="ml-[12px] sm:ml-[20px] lg:ml-[32px] font-clash text-[20px] sm:text-[24px] leading-none">
           HAQQ
         </div>
         <div className="flex-1" />
-        <nav className="flex-row space-x-6 items-center mr-[80px] hidden lg:flex">
-          <HeaderNavLink href="/#about">About</HeaderNavLink>
-          {/* <HeaderNavLink href="/#technology">Technology</HeaderNavLink> */}
-          <HeaderNavLink href="/#developers">Developers</HeaderNavLink>
-          <HeaderNavLink href="https://docs.haqq.network" isOutLink>
-            Documentation
-          </HeaderNavLink>
-          {/* <HeaderNavLink href="/#community">Community</HeaderNavLink> */}
-          {/* <HeaderNavLink href="/404">404</HeaderNavLink> */}
+        <nav className="flex-row space-x-6 items-center mr-[80px] hidden lg:flex font-guise">
+          <HeaderNavLink href="/staking">Staking</HeaderNavLink>
+          <HeaderNavLink href="/governance">Governance</HeaderNavLink>
         </nav>
         <div className="flex flex-row items-center">
-          <Link href="/wallet">
-            <Button className="hidden sm:block">HAQQ Wallet</Button>
-          </Link>
+          {isConnected ? (
+            <AccountButton
+              balance={balance}
+              address={ethAddress}
+              onDisconnectClick={disconnect}
+              className="hidden"
+            />
+          ) : (
+            <Button
+              className={clsx('hidden lg:block !font-clash hover:text-black')}
+              onClick={connect}
+            >
+              Connect wallet
+            </Button>
+          )}
           <BurgerButton
             className="ml-[24px] block lg:hidden"
             isOpen={isBurgerMenuOpen}
@@ -85,7 +151,26 @@ export function Header() {
       {isBurgerMenuOpen && (
         <Fragment>
           <ScrollLock isActive />
-          <BurgerMenu className="fixed lg:hidden w-full sm:w-[468px] top-[62px] sm:top-[71px] h-[calc(100vh-62px)] sm:h-[calc(100vh-71px)] right-0 z-40" />
+
+          <BurgerMenu
+            connectButton={
+              isConnected && (
+                <BurgerMenuBalanceButton
+                  balance={balance}
+                  address={ethAddress}
+                />
+              )
+            }
+            disconnectButton={
+              <Button
+                className="!font-clash hover:text-black"
+                onClick={isConnected ? disconnect : connect}
+              >
+                {isConnected ? 'Disconnect' : 'Connect wallet'}
+              </Button>
+            }
+            className="fixed lg:hidden w-full top-[62px] sm:top-[71px] h-[calc(100vh-62px)] sm:h-[calc(100vh-71px)] right-0 z-40"
+          />
           <div
             onClick={handleMenuOpen}
             className="hidden sm:block lg:hidden absolute w-full right-0 -z-0 bg-black/50 top-[62px] sm:top-[71px] h-[calc(100vh-62px)] sm:h-[calc(100vh-71px)]"
