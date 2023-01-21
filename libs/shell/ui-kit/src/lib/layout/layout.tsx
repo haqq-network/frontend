a834539b5a34ae6c3cd6937029e93f29472f6295
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-01-22 01:43:10 +0300

    fix(shell-ui): style bg image

--- libs/shell/ui-kit/src/lib/layout/layout.tsx
+++ libs/shell/ui-kit/src/lib/layout/layout.tsx
@@ -13,7 +13,7 @@ export function Layout({
     <div className={clsx('min-h-screen relative flex flex-col', className)}>
       <div
         className={clsx(
-          'bg-center object-cover bg-no-repeat absolute inset-0 z-[-1] -translate-y-3/4 w-full',
+          'bg-center object-cover bg-no-repeat absolute inset-0 z-[-1] -translate-y-[50%] sm:-translate-y-[45%] xl:-translate-y-[40%] w-full',
           styles['bg-image'],
         )}
       />
