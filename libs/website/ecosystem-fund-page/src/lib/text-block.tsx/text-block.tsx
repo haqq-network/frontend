731308cd5c5eeab6f31fb94f5df6b326f9874863
Jihad HadjHusein <skiptomylou999@gmail.com>
2023-05-05 19:51:45 +0200

    fix: add lines images to title block

--- libs/website/ecosystem-fund-page/src/lib/text-block.tsx/text-block.tsx
+++ libs/website/ecosystem-fund-page/src/lib/text-block.tsx/text-block.tsx
@@ -3,59 +3,61 @@ import Link from 'next/link';
 
 export function TextBlock() {
   return (
-    <section className="flex flex-col gap-y-[20px] sm:gap-y-[24px] py-[60px] sm:py-[80px] lg:py-[100px] px-[16px] sm:px-[48px] lg:px-[110px] xl:px-[320px] border-b border-t border-white/20">
-      <Heading>
-        Introducing the Haqq Ecosystem Fund: An Opportunity to Grow with Haqq
-        Blockchain
-      </Heading>
-      <Text>
-        Haqq Association is proud to announce the launch of the Haqq Ecosystem
-        Fund, a grants and investment program designed to support the growth of
-        Haqq Network and Islamic Coin. With a total allocation of $40M ($20M
-        USDC and USDT + $20M Islamic Coin), the Haqq Ecosystem Fund represents
-        our commitment to promoting global adoption of Haqq Blockchain and
-        Islamic Coin.
-      </Text>
-      <Text>
-        The funds will be directed towards various initiatives, including
-        development, marketing, bug bounties, and other ventures aimed at
-        improving the Haqq Ecosystem's growth. Our grants and investments
-        undergo a stringent ethics and Shariah assessment to ensure that every
-        project funded aligns with our Islamic values and is ethical in its
-        general sense.
-      </Text>
-      <Text>
-        At Haqq Association, we prioritize projects that are building on Haqq
-        Network from scratch, mature projects willing to expand their services
-        to the Haqq Network, and individual developers willing to contribute to
-        the core haqq infrastructure products such as{' '}
-        <Link href={'/wallet'} className="text-haqq-orange">
-          Haqq Wallet
-        </Link>
-        ,{' '}
-        <Link href={'#'} className="text-haqq-orange">
-          Haqq Node
-        </Link>
-        ,{' '}
-        <Link
-          href={'https://app.haqq.network/'}
-          target="_blank"
-          rel={'noopener noreferrer'}
-          className="text-haqq-orange"
-        >
-          Haqq Shell
-        </Link>
-        . This focus on web3 projects will ensure that the Haqq Ecosystem
-        remains at the forefront of blockchain technology, serving as a hub for
-        innovative projects and technologies.
-      </Text>
-      <Text>
-        Our team is excited to work with like-minded individuals, whether you
-        are a fintech startup or a community benefit project. With the Haqq
-        Ecosystem Fund, we have the opportunity to create a positive impact,
-        driving growth and innovation that aligns with our shared Islamic
-        values.
-      </Text>
+    <section className="z-50 flex flex-col gap-y-[20px] sm:gap-y-[24px] py-[60px] sm:py-[80px] lg:py-[100px] px-[16px] sm:px-[48px] lg:px-[110px] xl:px-[320px] border-b border-t border-white/20">
+      <div className="z-10">
+        <Heading>
+          Introducing the Haqq Ecosystem Fund: An Opportunity to Grow with Haqq
+          Blockchain
+        </Heading>
+        <Text>
+          Haqq Association is proud to announce the launch of the Haqq Ecosystem
+          Fund, a grants and investment program designed to support the growth
+          of Haqq Network and Islamic Coin. With a total allocation of $40M
+          ($20M USDC and USDT + $20M Islamic Coin), the Haqq Ecosystem Fund
+          represents our commitment to promoting global adoption of Haqq
+          Blockchain and Islamic Coin.
+        </Text>
+        <Text>
+          The funds will be directed towards various initiatives, including
+          development, marketing, bug bounties, and other ventures aimed at
+          improving the Haqq Ecosystem's growth. Our grants and investments
+          undergo a stringent ethics and Shariah assessment to ensure that every
+          project funded aligns with our Islamic values and is ethical in its
+          general sense.
+        </Text>
+        <Text>
+          At Haqq Association, we prioritize projects that are building on Haqq
+          Network from scratch, mature projects willing to expand their services
+          to the Haqq Network, and individual developers willing to contribute
+          to the core haqq infrastructure products such as{' '}
+          <Link href={'/wallet'} className="text-haqq-orange">
+            Haqq Wallet
+          </Link>
+          ,{' '}
+          <Link href={'#'} className="text-haqq-orange">
+            Haqq Node
+          </Link>
+          ,{' '}
+          <Link
+            href={'https://app.haqq.network/'}
+            target="_blank"
+            rel={'noopener noreferrer'}
+            className="text-haqq-orange"
+          >
+            Haqq Shell
+          </Link>
+          . This focus on web3 projects will ensure that the Haqq Ecosystem
+          remains at the forefront of blockchain technology, serving as a hub
+          for innovative projects and technologies.
+        </Text>
+        <Text>
+          Our team is excited to work with like-minded individuals, whether you
+          are a fintech startup or a community benefit project. With the Haqq
+          Ecosystem Fund, we have the opportunity to create a positive impact,
+          driving growth and innovation that aligns with our shared Islamic
+          values.
+        </Text>
+      </div>
     </section>
   );
 }
--- libs/website/ecosystem-fund-page/src/lib/title-block/title-block.tsx
+++ libs/website/ecosystem-fund-page/src/lib/title-block/title-block.tsx
@@ -2,10 +2,12 @@ import { Button } from '@haqq/website/ui-kit';
 import Image from 'next/image';
 import Link from 'next/link';
 import sunshineBgData from '../../assets/sunshine.svg';
+import titleBlockLinesBgData from '../../assets/title-block-lines.svg';
+import titleBlockLinesSmallBgData from '../../assets/title-block-lines-small.svg';
 
 export function TitleBlock() {
   return (
-    <section className="relative pt-[80px] sm:pt-[120px] pl-[16px] sm:pl-[48px] lg:pl-[80px]">
+    <section className="overflow-hidden relative pt-[80px] sm:pt-[120px] px-[16px] sm:px-[48px] lg:px-[80px]">
       <div className="uppercase font-serif text-[46px] sm:text-[80px] lg:text-[140px] leading-none font-medium">
         Haqq <br /> Ecosys
         <br className="hidden lg:block xl:hidden" />
@@ -14,17 +16,32 @@ export function TitleBlock() {
       <Link href={'#apply'} className="scroll-smooth" scroll={false}>
         <Button
           variant={2}
-          className="mb-[80px] sm:mb-[120px] mt-[32px] sm:mt-[48px] lg:mt-[64px]"
+          className="z-10 mb-[80px] sm:mb-[120px] mt-[32px] sm:mt-[48px] lg:mt-[64px]"
         >
           Apply
         </Button>
       </Link>
+
       <Image
         src={sunshineBgData.src}
         alt=""
         height={sunshineBgData.height}
         width={sunshineBgData.width}
-        className="absolute bottom-0 left-1/2 z-[-1] transform -translate-x-1/2"
+        className="absolute bottom-0 left-1/2 z-[0] transform -translate-x-1/2"
+      />
+      <Image
+        src={titleBlockLinesBgData.src}
+        alt=""
+        height={titleBlockLinesBgData.height}
+        width={titleBlockLinesBgData.width}
+        className="hidden md:block absolute top-0 right-[-130px] md:right-[-210px] lg:right-[-370px] xl:right-[50px] z-[-1]"
+      />
+      <Image
+        src={titleBlockLinesSmallBgData.src}
+        alt=""
+        height={titleBlockLinesSmallBgData.height}
+        width={titleBlockLinesSmallBgData.width}
+        className="block md:hidden absolute top-0 right-0 z-[-1]"
       />
     </section>
   );
