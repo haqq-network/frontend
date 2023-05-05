7b1742a92ff181ed83e7667aa99a3accd2241a12
Oleg Shilov <me@olegshilov.com>
2023-05-05 19:04:15 +0700

    fix(website): update chain stats, get actual accounts number

--- apps/website/pages/index.tsx
+++ apps/website/pages/index.tsx
@@ -1,15 +1,20 @@
 export { WebsiteIndexPage as default } from '@haqq/website/index-page';
 
 export async function getServerSideProps() {
+  const accountsResponse = await fetch(
+    'https://rest.cosmos.haqq.network/cosmos/auth/v1beta1/accounts?pagination.count_total=true&pagination.limit=1',
+  );
+  const accounts = await accountsResponse.json();
+
   const stats = {
-    mainnetAccountsCreated: 2000,
-    transactionsInLast24Hours: 5000,
-    secondsToConsensusFinality: 1000,
-    averageCostPerTransaction: 200,
-    era: 5,
-    emissionRate: 2000,
-    emittedAlready: 2000,
-    willBeEmitted: 2000,
+    mainnetAccountsCreated: Number.parseInt(accounts.pagination.total, 10),
+    transactionsInLast24Hours: 10000,
+    secondsToConsensusFinality: 5.6,
+    averageCostPerTransaction: 147,
+    era: 1,
+    emissionRate: 0,
+    emittedAlready: 20000000000,
+    willBeEmitted: 80000000000,
   };
 
   return {
