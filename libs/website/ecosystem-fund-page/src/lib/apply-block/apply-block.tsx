aa4964709b5e19f9d5172b0bc97fededa87fe711
Jihad HadjHusein <skiptomylou999@gmail.com>
2023-05-06 03:41:41 +0200

    fix: side lines position

--- libs/website/ecosystem-fund-page/src/lib/apply-block/apply-block.tsx
+++ libs/website/ecosystem-fund-page/src/lib/apply-block/apply-block.tsx
@@ -4,25 +4,36 @@ import Image from 'next/image';
 import bottomLinesImgData from '../../assets/apply-block-bottom-lines.svg';
 import sideLinesImgData from '../../assets/apply-block-side-lines.svg';
 
-//px-[16px] sm:px-[48px] lg:px-[112px] xl:px-[135px]
+// px-[16px] sm:px-[48px] lg:px-[112px] xl:px-[135px]
+// px-[64px]
 export function ApplyBlock() {
   return (
-    <section className="flex flex-col relative text-center" id="apply">
-      <div className="flex">
+    <section
+      className={'flex items-center flex-col relative text-center'}
+      id="apply"
+    >
+      <div className="flex w-full">
         <Image
           src={sideLinesImgData.src}
           alt=""
-          height={sideLinesImgData.height}
           width={sideLinesImgData.width}
-          // className="absolute top-0 right-0 z-[0]"
+          height={sideLinesImgData.height}
+          className="left-[64px] hidden absolute top-0 xl:block"
         />
-        <div className="flex-1 py-[48px] sm:py-[60px] lg:py-[80px] px-[16px] sm:px-[48px] lg:px-[112px] xl:px-[135px]">
+        <Image
+          src={sideLinesImgData.src}
+          alt=""
+          width={sideLinesImgData.width}
+          height={sideLinesImgData.height}
+          className="left-[171px] hidden absolute top-0 xl:block"
+        />
+        <div className="flex-1 my-[48px] sm:my-[60px] lg:my-[80px] xl:mx-[134px]">
           <Text className="text-haqq-bigfoot-feet">
             Grants and Investment Program
           </Text>
           <Heading className="mt-[8px]">
             Apply now for the Haqq Ecosystem Fund, and let's{' '}
-            <br className="hidden lg:block" />
+            <br className="hidden absolute top-0 lg:block" />
             make a difference together!
           </Heading>
           <Link
@@ -38,20 +49,26 @@ export function ApplyBlock() {
         <Image
           src={sideLinesImgData.src}
           alt=""
-          height={sideLinesImgData.height}
           width={sideLinesImgData.width}
-          // className="absolute top-0 left-0 z-[0]"
+          height={sideLinesImgData.height}
+          className="right-[64px] hidden absolute top-0 xl:block"
         />
-      </div>
-      <div>
         <Image
-          src={bottomLinesImgData.src}
+          src={sideLinesImgData.src}
           alt=""
-          height={bottomLinesImgData.height}
-          width={bottomLinesImgData.width}
-          // className=" bottom-0 left-1/2 z-[0] transform -translate-x-1/2"
+          width={sideLinesImgData.width}
+          height={sideLinesImgData.height}
+          className="right-[171px] hidden absolute xl:block"
         />
       </div>
+
+      {/* <Image
+        src={bottomLinesImgData.src}
+        alt=""
+        height={bottomLinesImgData.height}
+        width={bottomLinesImgData.width}
+        className="absolute"
+      /> */}
     </section>
   );
 }
