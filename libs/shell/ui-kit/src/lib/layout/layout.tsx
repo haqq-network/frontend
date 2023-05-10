3e25316f41aad50f2d841586a408869e9904ccd8
Oleg Shilov <me@olegshilov.com>
2023-05-10 22:12:17 +0700

    fix(ui-kit): fix layout background

--- libs/shell/ui-kit/src/lib/layout/layout.tsx
+++ libs/shell/ui-kit/src/lib/layout/layout.tsx
@@ -11,13 +11,7 @@ export function Layout({
 }) {
   return (
     <div className={clsx('min-h-screen relative flex flex-col', className)}>
-      <div
-        className={clsx(
-          'bg-center object-cover bg-no-repeat absolute inset-0 z-[-1] -translate-y-[50%] sm:-translate-y-[45%] xl:-translate-y-[40%] w-full',
-          styles['bg-image'],
-        )}
-      />
-      <div className="">{children}</div>
+      <div className={styles['bg-image']}>{children}</div>
     </div>
   );
 }
