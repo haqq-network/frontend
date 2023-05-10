67c92cb037f4e2b88e0275fa2356bbc0fcce78b2
Oleg Shilov <me@olegshilov.com>
2023-05-10 22:03:29 +0700

    fix(governance): use page from shell ui-kit

--- apps/governance/src/app/app-wrapper.tsx
+++ apps/governance/src/app/app-wrapper.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from 'react';
-import { Page } from '@haqq/ui-kit';
-import { Header } from '@haqq/shell/ui-kit';
+import { Header, Page } from '@haqq/shell/ui-kit';
 
 export function AppWrapper({ children }: { children: ReactNode }) {
   return <Page header={<Header />}>{children}</Page>;
