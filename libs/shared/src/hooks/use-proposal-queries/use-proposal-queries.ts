b907dd108ad46eb10adae75e766cea5f3d62c2f9
Oleg Shilov <me@olegshilov.com>
2023-05-11 10:23:03 +0700

    feat(cosmos): governance module parameters query and hooks

--- libs/shared/src/hooks/use-proposal-queries/use-proposal-queries.ts
+++ libs/shared/src/hooks/use-proposal-queries/use-proposal-queries.ts
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { useCosmosService } from '../../providers/cosmos-provider';
+import { GovParamsType } from '@cosmjs/stargate';
 
 export function useProposalListQuery() {
   const { getProposals } = useCosmosService();
@@ -20,6 +21,30 @@ export function useProposalDetailsQuery(proposalId: string | undefined) {
       }
 
       return await getProposalDetails(proposalId);
+    },
+    {
+      refetchOnWindowFocus: false,
+    },
+  );
+}
+
+export function useGovernanceParamsQuery() {
+  const { getGovernanceParams } = useCosmosService();
+
+  return useQuery(
+    ['governance-params'],
+    async () => {
+      const [deposit_params, voting_params, tally_params] = await Promise.all([
+        getGovernanceParams('deposit').then((res) => res.deposit_params),
+        getGovernanceParams('voting').then((res) => res.voting_params),
+        getGovernanceParams('tallying').then((res) => res.tally_params),
+      ]);
+
+      return {
+        deposit_params,
+        voting_params,
+        tally_params,
+      };
     },
     {
       refetchOnWindowFocus: false,
