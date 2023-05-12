eaa661f32bafee0ed401eca2d8e4269b86415acf
Jihad HadjHusein <skiptomylou999@gmail.com>
2023-05-12 13:31:40 +0200

    fix(shell): change type&props val list item mobile

--- libs/shell/ui-kit/src/lib/validator-list-item-mobile/validator-list-item-mobile.stories.tsx
+++ libs/shell/ui-kit/src/lib/validator-list-item-mobile/validator-list-item-mobile.stories.tsx
@@ -7,64 +7,47 @@ export default {
   },
 };
 
-export const Default = () => {
-  const stakingPool = 53397150014.54143;
-  const validator = {
-    operator_address: 'haqqvaloper18xa2e2z4ndmgef9a7c7lj00etutw2dafyyhdkx',
-    consensus_pubkey: {
-      '@type': '/cosmos.crypto.ed25519.PubKey',
-      key: 'oddw5eWjHnP8ymfMM4nkb+cB0vebA3STKIPUADXCyx0=',
-    },
-    jailed: false,
-    status: 'BOND_STATUS_BONDED',
-    tokens: '12209530634048454401992834211',
-    delegator_shares: '14484395474234476265703469022.452914368020086907',
-    description: {
-      moniker: 'val01',
-      identity: '',
-      website: '',
-      security_contact: '',
-      details: '',
-    },
-    unbonding_height: '1816048',
-    unbonding_time: '2023-02-02T09:57:23.086714574Z',
-    commission: {
-      commission_rates: {
-        rate: '0.100000000000000000',
-        max_rate: '0.200000000000000000',
-        max_change_rate: '0.010000000000000000',
-      },
-      update_time: '2022-09-09T13:00:00Z',
-    },
-    min_self_delegation: '1',
-  };
-  const delegation = {
-    delegation: {
-      delegator_address: 'haqq1vn0ludw6clzwxg6tdqxnzkxcwj6t34gj0vg43g',
-      validator_address: 'haqqvaloper18xa2e2z4ndmgef9a7c7lj00etutw2dafyyhdkx',
-      shares: '9557487464119541945.986652783152507000',
-    },
-    balance: {
-      denom: 'aISLM',
-      amount: '8056424321282832799',
-    },
-  };
-  const reward = {
-    validator_address: 'haqqvaloper18xa2e2z4ndmgef9a7c7lj00etutw2dafyyhdkx',
-    reward: [
-      {
-        denom: 'aISLM',
-        amount: '2700031527588484633.412799778322221285',
-      },
-    ],
-  };
-
+export const Active = () => {
   return (
     <ValidatorListItemMobile
-      stakingPool={stakingPool}
-      validator={validator}
-      delegation={delegation}
-      reward={reward}
+      address="haqqvaloper1tag22x87340up4xkazt6f5dk8z8x23nmq2d06q"
+      fee={10}
+      name="ActiveVal"
+      reward={100}
+      staked={1000}
+      status="BOND_STATUS_BONDED"
+      votingPower={120000000}
+      votingPowerPercent={25}
+    />
+  );
+};
+
+export const Jailed = () => {
+  return (
+    <ValidatorListItemMobile
+      address="haqqvaloper1tag22x87340up4xkazt6f5dk8z8x23nmq2d06q"
+      fee={12}
+      name="JailedVal"
+      reward={100}
+      staked={1000}
+      status="BOND_STATUS_BONDED"
+      votingPower={120000000}
+      votingPowerPercent={25}
+    />
+  );
+};
+
+export const Inactive = () => {
+  return (
+    <ValidatorListItemMobile
+      address="haqqvaloper1tag22x87340up4xkazt6f5dk8z8x23nmq2d06q"
+      fee={11}
+      name="InactiveVal"
+      reward={100}
+      staked={1000}
+      status="BOND_STATUS_BONDED"
+      votingPower={120000000}
+      votingPowerPercent={25}
     />
   );
 };
--- libs/shell/ui-kit/src/lib/validator-list-item-mobile/validator-list-item-mobile.tsx
+++ libs/shell/ui-kit/src/lib/validator-list-item-mobile/validator-list-item-mobile.tsx
@@ -1,16 +1,18 @@
-import { PropsWithChildren, useMemo } from 'react';
-import { formatUnits } from 'ethers/lib/utils';
+import { PropsWithChildren } from 'react';
 import { bondStatusFromJSON } from 'cosmjs-types/cosmos/staking/v1beta1/staking';
-import type { Validator, DelegationResponse, Reward } from '@evmos/provider';
 import { useNavigate } from 'react-router-dom';
-
 import clsx from 'clsx';
 
 interface ValidatorListItemProps {
-  validator: Validator;
-  delegation?: DelegationResponse;
-  reward?: Reward;
-  stakingPool: number;
+  name: string;
+  status: string;
+  fee: number;
+  votingPower: number;
+  votingPowerPercent: number;
+  staked: number;
+  reward: number;
+  address: string;
+  jailed?: boolean;
 }
 
 function ColumnLine({
@@ -26,79 +28,53 @@ function ColumnLine({
 }
 
 export function ValidatorListItemMobile({
-  validator,
+  fee,
+  name,
   reward,
-  delegation,
-  stakingPool,
+  staked,
+  status,
+  votingPower,
+  votingPowerPercent,
+  address,
+  jailed,
 }: ValidatorListItemProps) {
   const navigate = useNavigate();
-
-  const validatorCommission = useMemo(() => {
-    return (
-      Number.parseFloat(validator.commission?.commission_rates?.rate ?? '0') *
-      100
-    ).toFixed(0);
-  }, [validator.commission?.commission_rates]);
-
-  const votingPower = useMemo(() => {
-    return Number.parseInt(validator.tokens ?? '0') / 10 ** 18;
-  }, [validator.tokens]);
-
-  const userDelegate = useMemo(() => {
-    if (delegation?.balance) {
-      return Number.parseFloat(formatUnits(delegation.balance.amount));
-    }
-
-    return 0;
-  }, [delegation]);
-
-  const userRewards = useMemo(() => {
-    if (reward?.reward.length) {
-      return Number.parseFloat(reward?.reward[0].amount) / 10 ** 18;
-    }
-
-    return 0;
-  }, [reward]);
-
-  const votingPowerInPercents = useMemo(() => {
-    return ((votingPower / stakingPool) * 100).toFixed(2);
-  }, [votingPower, stakingPool]);
 
   return (
     <div
       className="flex flex-col"
-      onClick={() => navigate(`validator/${validator.operator_address}`)}
+      onClick={() => navigate(`validator/${address}`)}
     >
       <ColumnLine columnName="Name">
-        <span className="text-white">{validator.description?.moniker}</span>
+        <span className="text-white">{name}</span>
       </ColumnLine>
       <ColumnLine columnName="Status">
         <span
           className={clsx(
-            validator.jailed && 'text-[#FF5454]',
-            bondStatusFromJSON(validator.status) === 3
+            jailed && 'text-[#FF5454]',
+            bondStatusFromJSON(status) === 3
               ? 'text-[#01B26E]'
               : 'text-[#E3A13F]',
           )}
         >
-          {validator.jailed && 'Jailed'}
-          {bondStatusFromJSON(validator.status) === 3 ? 'Active' : 'Inactive'}
+          {jailed && 'Jailed'}
+          {bondStatusFromJSON(status) === 3 ? 'Active' : 'Inactive'}
         </span>
       </ColumnLine>
       <ColumnLine columnName="Fee">
-        <span className="text-white">{validatorCommission}%</span>
+        <span className="text-white">{fee}%</span>
       </ColumnLine>
       <ColumnLine columnName="Voting power">
-        <span className="text-white">{votingPower.toLocaleString()}</span>
+        <span className="text-white">{votingPower}</span>
       </ColumnLine>
       <ColumnLine columnName="Voting power %">
-        <span className="text-white">{votingPowerInPercents}%</span>
+        <span className="text-white">{votingPowerPercent}%</span>
       </ColumnLine>
       <ColumnLine columnName="Staked">
-        <span className="text-white">{userDelegate.toLocaleString()}%</span>
+        <span className="text-white">{staked}%</span>
       </ColumnLine>
       <ColumnLine columnName="Reward">
-        <span className="text-white">{userRewards.toLocaleString()}%</span>
+        <span className="text-white">{reward}%</span>
       </ColumnLine>
     </div>
   );
