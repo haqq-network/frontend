abf04929c0810e48cdbd4dbd0128d9b0389e112d
Oleg Shilov <me@olegshilov.com>
2023-05-12 18:49:18 +0700

    fix(shell-ui): update validator mobile card props

--- libs/shell/ui-kit/src/lib/validator-list-item-mobile/validator-list-item-mobile.stories.tsx
+++ libs/shell/ui-kit/src/lib/validator-list-item-mobile/validator-list-item-mobile.stories.tsx
@@ -1,4 +1,3 @@
-import { Router } from 'react-router-dom';
 import { ValidatorListItemMobile } from './validator-list-item-mobile';
 
 export default {
@@ -10,51 +9,42 @@ export default {
 
 export const Active = () => {
   return (
-    <Router location={''} navigator={''}>
-      <ValidatorListItemMobile
-        address="haqqvaloper1tag22x87340up4xkazt6f5dk8z8x23nmq2d06q"
-        fee={10}
-        name="ActiveVal"
-        reward={100}
-        staked={1000}
-        status="BOND_STATUS_BONDED"
-        votingPower={120000000}
-        votingPowerPercent={25}
-      />
-    </Router>
+    <ValidatorListItemMobile
+      fee={'10%'}
+      name="ActiveVal"
+      reward={100}
+      staked={1000}
+      status="active"
+      votingPower={120000000}
+      votingPowerPercent={25}
+    />
   );
 };
 
 export const Jailed = () => {
   return (
-    <Router location={''} navigator={''}>
-      <ValidatorListItemMobile
-        address="haqqvaloper1tag22x87340up4xkazt6f5dk8z8x23nmq2d06q"
-        fee={12}
-        jailed
-        name="JailedVal"
-        reward={100}
-        staked={1000}
-        votingPower={120000000}
-        votingPowerPercent={25}
-      />
-    </Router>
+    <ValidatorListItemMobile
+      fee={'12%'}
+      name="JailedVal"
+      reward={100}
+      staked={1000}
+      status="jailed"
+      votingPower={120000000}
+      votingPowerPercent={25}
+    />
   );
 };
 
 export const Inactive = () => {
   return (
-    <Router location={''} navigator={''}>
-      <ValidatorListItemMobile
-        address="haqqvaloper1tag22x87340up4xkazt6f5dk8z8x23nmq2d06q"
-        fee={11}
-        name="InactiveVal"
-        reward={100}
-        staked={1000}
-        status="BOND_STATUS_UNBONDED"
-        votingPower={120000000}
-        votingPowerPercent={25}
-      />
-    </Router>
+    <ValidatorListItemMobile
+      fee={'11%'}
+      name="InactiveVal"
+      reward={100}
+      staked={1000}
+      status="inactive"
+      votingPower={120000000}
+      votingPowerPercent={25}
+    />
   );
 };
--- libs/shell/ui-kit/src/lib/validator-list-item-mobile/validator-list-item-mobile.tsx
+++ libs/shell/ui-kit/src/lib/validator-list-item-mobile/validator-list-item-mobile.tsx
@@ -1,19 +1,4 @@
 import { PropsWithChildren } from 'react';
-import { bondStatusFromJSON } from 'cosmjs-types/cosmos/staking/v1beta1/staking';
-import { useNavigate } from 'react-router-dom';
-import clsx from 'clsx';
-
-interface ValidatorListItemProps {
-  name: string;
-  status?: string;
-  fee: number;
-  votingPower: number;
-  votingPowerPercent: number;
-  staked: number;
-  reward: number;
-  address: string;
-  jailed?: boolean;
-}
 
 function ColumnLine({
   children,
@@ -35,49 +20,41 @@ export function ValidatorListItemMobile({
   status,
   votingPower,
   votingPowerPercent,
-  address,
-  jailed,
-}: ValidatorListItemProps) {
-  const navigate = useNavigate();
-
+}: {
+  name: string;
+  status: 'jailed' | 'active' | 'inactive';
+  fee: string | string | number;
+  votingPower: string | number;
+  votingPowerPercent: string | number;
+  staked: string | number;
+  reward: string | number;
+}) {
   return (
-    <div
-      className="flex w-full flex-col px-[8px] py-[12px]"
-      onClick={() => navigate(`validator/${address}`)}
-    >
+    <div className="flex flex-col">
       <ColumnLine columnName="Name">
         <span className="text-white">{name}</span>
       </ColumnLine>
       <ColumnLine columnName="Status">
-        <span
-          className={clsx(
-            jailed && 'text-[#FF5454]',
-            bondStatusFromJSON(status) === 3
-              ? 'text-[#01B26E]'
-              : 'text-[#E3A13F]',
-          )}
-        >
-          {bondStatusFromJSON(status) === 3
-            ? 'Active'
-            : jailed
-            ? 'Jailed'
-            : 'Inactive'}
-        </span>
+        {status === 'jailed' && <span className="text-[#FF5454]">Jailed</span>}
+        {status === 'active' && <span className="text-[#01B26E]">Active</span>}
+        {status === 'inactive' && (
+          <span className="text-[#E3A13F]">Inactive</span>
+        )}
       </ColumnLine>
       <ColumnLine columnName="Fee">
-        <span className="text-white">{fee}%</span>
+        <span className="text-white">{fee}</span>
       </ColumnLine>
       <ColumnLine columnName="Voting power">
         <span className="text-white">{votingPower}</span>
       </ColumnLine>
       <ColumnLine columnName="Voting power %">
-        <span className="text-white">{votingPowerPercent}%</span>
+        <span className="text-white">{votingPowerPercent}</span>
       </ColumnLine>
       <ColumnLine columnName="Staked">
-        <span className="text-white">{staked}%</span>
+        <span className="text-white">{staked}</span>
       </ColumnLine>
       <ColumnLine columnName="Reward">
-        <span className="text-white">{reward}%</span>
+        <span className="text-white">{reward}</span>
       </ColumnLine>
     </div>
   );
