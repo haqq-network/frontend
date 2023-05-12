5b5d18b9005d98e82319e4706aa5149209635ba4
Jihad HadjHusein <skiptomylou999@gmail.com>
2023-05-12 18:01:49 +0200

    feat(shell-ui): add orange link story

---
+++ libs/shell/ui-kit/src/lib/orange-link/orange-link.stories.tsx
@@ -0,0 +1,23 @@
+import { withRouter } from 'storybook-addon-react-router-v6';
+import { OrangeLink as OrangeLinkComponent } from './orange-link';
+
+export default {
+  title: 'shell/ui-kit/orange-link',
+  decorators: [withRouter],
+};
+
+export const Default = () => {
+  return <OrangeLinkComponent href="/">Orange link</OrangeLinkComponent>;
+};
+
+export const WithTarget = () => {
+  return (
+    <OrangeLinkComponent
+      href="https://haqq.network/"
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      Orange link
+    </OrangeLinkComponent>
+  );
+};
