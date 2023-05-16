e6f96372997302655aad959865640deb4244fb98
Oleg Shilov <me@olegshilov.com>
2023-05-17 00:31:00 +0700

    feat(ui-kit): add more components and stories

--- libs/shell/ui-kit/src/lib/header/header.stories.tsx
+++ libs/shell/ui-kit/src/lib/header/header.stories.tsx
@@ -1,15 +1,12 @@
 import { withRouter } from 'storybook-addon-react-router-v6';
 import { Header as HeaderComponent } from './header';
 import { withoutPadding } from '../../../.storybook/decorators';
-import { Meta, StoryObj } from '@storybook/react';
 
-const meta: Meta<typeof HeaderComponent> = {
-  component: HeaderComponent,
-  title: 'shell/ui-kit',
+export default {
+  title: 'shell/ui-kit/header',
   decorators: [withRouter, withoutPadding],
 };
 
-export default meta;
-type Story = StoryObj<typeof HeaderComponent>;
-
-export const Header: Story = {};
+export const Default = () => {
+  return <HeaderComponent />;
+};
