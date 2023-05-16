910b0169d6311e803701473f5e2975b9a1d2f736
Jihad HadjHusein <skiptomylou999@gmail.com>
2023-05-17 00:49:51 +0200

    fix(shell-ui): header story

--- libs/shell/ui-kit/src/lib/header/header.stories.tsx
+++ libs/shell/ui-kit/src/lib/header/header.stories.tsx
@@ -1,12 +1,15 @@
 import { withRouter } from 'storybook-addon-react-router-v6';
 import { Header as HeaderComponent } from './header';
 import { withoutPadding } from '../../../.storybook/decorators';
+import { Meta, StoryObj } from '@storybook/react';
 
-export default {
-  title: 'shell/ui-kit/header',
+const meta: Meta<typeof HeaderComponent> = {
+  component: HeaderComponent,
+  title: 'shell/ui-kit',
   decorators: [withRouter, withoutPadding],
 };
 
-export const Default = () => {
-  return <HeaderComponent />;
-};
+export default meta;
+type Story = StoryObj<typeof HeaderComponent>;
+
+export const Header: Story = {};
