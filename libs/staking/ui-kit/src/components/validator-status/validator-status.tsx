8d0d1b1f383d364569aade46a4e2ffc98408a029
Oleg Shilov <me@olegshilov.com>
2023-05-17 21:38:16 +0700

    fix(ui-kit): add className prop to validator status

--- libs/staking/ui-kit/src/components/validator-status/validator-status.tsx
+++ libs/staking/ui-kit/src/components/validator-status/validator-status.tsx
@@ -3,24 +3,38 @@ import clsx from 'clsx';
 export function ValidatorListStatus({
   jailed,
   status,
+  className,
 }: {
   jailed: boolean;
   status: number;
+  className?: string;
 }) {
   if (jailed) {
     return (
-      <div className="text-[16px] leading-[26px] text-[#FF5454]">Jailed</div>
+      <div
+        className={clsx('text-[16px] leading-[26px] text-[#FF5454]', className)}
+      >
+        Jailed
+      </div>
     );
   }
 
   if (status === 3) {
     return (
-      <div className="text-[16px] leading-[26px] text-[#01B26E]">Active</div>
+      <div
+        className={clsx('text-[16px] leading-[26px] text-[#01B26E]', className)}
+      >
+        Active
+      </div>
     );
   }
 
   return (
-    <div className="text-[16px] leading-[26px] text-[#E3A13F]">Inactive</div>
+    <div
+      className={clsx('text-[16px] leading-[26px] text-[#E3A13F]', className)}
+    >
+      Inactive
+    </div>
   );
 }
 
