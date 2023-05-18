e5a166e710e84fa9575fa131e2d62893f3eaf7f2
Jihad HadjHusein <skiptomylou999@gmail.com>
2023-05-18 17:31:14 +0200

    fix(ui-kit): revert tooltip classnames

--- libs/ui-kit/src/lib/tooltip/tooltip.tsx
+++ libs/ui-kit/src/lib/tooltip/tooltip.tsx
@@ -16,12 +16,12 @@ export function Tooltip({
       {children}
       <div
         className={clsx(
-          'invisible absolute mb-1 max-w-[240px] scale-90 opacity-0',
+          'invisible absolute mb-1 w-max scale-90 opacity-0',
           'bottom-full left-1/2 -translate-x-1/2 -translate-y-2',
           'transition delay-75 duration-100 ease-out',
           'group-hover:visible group-hover:z-50 group-hover:translate-y-0 group-hover:scale-100 group-hover:opacity-100',
           'rounded-[4px] px-[8px] py-[4px] text-xs font-medium leading-snug shadow-lg',
-          'bg-haqq-black break-words border border-[#ffffff3d] text-white',
+          'bg-slate-500 text-white',
           className,
         )}
       >
