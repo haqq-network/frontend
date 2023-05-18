a2d5317fc46a4a899cf39fcd0aae311f6c17a22c
Jihad HadjHusein <skiptomylou999@gmail.com>
2023-05-18 16:18:20 +0200

    feat(ui-kit): tooltip classnames & tooltip story

---
+++ libs/shell/ui-kit/src/lib/tooltip/tooltip.stories.tsx
@@ -0,0 +1,20 @@
+import { Tooltip as TooltipComponent } from '@haqq/ui-kit';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof TooltipComponent> = {
+  component: TooltipComponent,
+  title: 'shell/ui-kit',
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof TooltipComponent>;
+
+export const Tooltip: Story = {
+  args: {
+    children: "I'm a tooltip children!",
+    text: 'Click to copy 0x5D62DB16Ee4DA021f2644017380E1D4Cb02Bc78D',
+  },
+};
--- libs/ui-kit/src/lib/tooltip/tooltip.tsx
+++ libs/ui-kit/src/lib/tooltip/tooltip.tsx
@@ -1,9 +1,8 @@
-import { ReactElement, ReactNode } from 'react';
+import { PropsWithChildren, ReactElement } from 'react';
 import clsx from 'clsx';
 
 export interface TooltipProps {
   text: string;
-  children: ReactNode;
   className?: string;
 }
 
@@ -11,18 +10,18 @@ export function Tooltip({
   text,
   children,
   className,
-}: TooltipProps): ReactElement {
+}: PropsWithChildren<TooltipProps>): ReactElement {
   return (
     <span className="group relative inline-block leading-none">
       {children}
       <div
         className={clsx(
-          'invisible absolute mb-1 w-max scale-90 opacity-0',
+          'invisible absolute mb-1 max-w-[240px] scale-90 opacity-0',
           'bottom-full left-1/2 -translate-x-1/2 -translate-y-2',
           'transition delay-75 duration-100 ease-out',
           'group-hover:visible group-hover:z-50 group-hover:translate-y-0 group-hover:scale-100 group-hover:opacity-100',
           'rounded-[4px] px-[8px] py-[4px] text-xs font-medium leading-snug shadow-lg',
-          'bg-slate-500 text-white',
+          'bg-haqq-black break-words border border-[#ffffff3d] text-white',
           className,
         )}
       >
