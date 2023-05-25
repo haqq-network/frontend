948064b39f782b5eadef1a22b1dbe930fbae1f33
Jihad HadjHusein <skiptomylou999@gmail.com>
2023-05-25 12:57:41 +0200

    fix(website): change icon height, link width

--- libs/website/ecosystem-page/src/lib/partners-block/partners-block.tsx
+++ libs/website/ecosystem-page/src/lib/partners-block/partners-block.tsx
@@ -113,6 +113,7 @@ function PartnerCard({
           alt={name}
           width={logoWidth}
           height={logoHeight}
+          className="h-[36px]"
         />
         <div className="text-[16px] leading-[1.2em] mt-[12px]">{name}</div>
       </div>
@@ -122,32 +123,34 @@ function PartnerCard({
           <PartnerTypeOrStatus status={status} />
           <PartnerTypeOrStatus type={type} />
         </div>
-        <Link
-          href={link}
-          className={clsx(
-            'text-[14px] font-[600] leading-[1.2em] text-haqq-orange hover:text-haqq-light-orange',
-            'cursor-pointer transition-colors duration-100 ease-out',
-            'inline-flex items-center gap-x-[4px]',
-          )}
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Website
-          <svg
-            width="20"
-            height="20"
-            viewBox="0 0 20 20"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+        <div>
+          <Link
+            href={link}
+            className={clsx(
+              'text-[14px] font-[600] leading-[1.2em] text-haqq-orange hover:text-haqq-light-orange',
+              'cursor-pointer transition-colors duration-100 ease-out',
+              'inline-flex items-center gap-x-[4px]',
+            )}
+            rel="noopener noreferrer"
+            target="_blank"
           >
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M12.2874 8.30374L12.2874 13.363L13.954 13.363L13.954 5.45857H6.04965V7.12523L11.1089 7.12523L4.28188 13.9522L5.46039 15.1307L12.2874 8.30374Z"
-              fill="currentColor"
-            />
-          </svg>
-        </Link>
+            Website
+            <svg
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M12.2874 8.30374L12.2874 13.363L13.954 13.363L13.954 5.45857H6.04965V7.12523L11.1089 7.12523L4.28188 13.9522L5.46039 15.1307L12.2874 8.30374Z"
+                fill="currentColor"
+              />
+            </svg>
+          </Link>
+        </div>
       </div>
     </div>
   );
