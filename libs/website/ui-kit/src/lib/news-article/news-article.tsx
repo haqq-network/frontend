e68abd0a0a166ef1cbdbd2e6934b796ac89ddeac
Jihad HadjHusein <skiptomylou999@gmail.com>
2023-05-30 15:14:41 +0200

    fix(website-ui):article link, type, date, story

--- libs/website/ui-kit/src/lib/news-article/news-article.tsx
+++ libs/website/ui-kit/src/lib/news-article/news-article.tsx
@@ -5,14 +5,17 @@ import { MarkdownText } from '../markdown-text/markdown-text';
 import { TelegramIcon, TwitterIcon } from '../icons';
 import { CopyIcon } from '../icons/copy-icon';
 
-type NewsArticleProps = Omit<NewsCardProps, 'isFeatured' | 'className'>;
+type NewsArticleProps = Omit<NewsCardProps, 'isFeatured' | 'className'> & {
+  onLinkCopy?: () => void;
+};
 
 export function NewsArticle({
   imageUrl,
-  description,
+  content,
   title,
   date,
   category,
+  onLinkCopy,
 }: NewsArticleProps) {
   return (
     <section className="flex flex-col items-start px-[16px] py-[48px] md:py-[68px] md:px-[48px] lg:px-[110px] lg:py-[100px] 2xl:px-[320px]">
@@ -20,10 +23,16 @@ export function NewsArticle({
         <Image src={imageUrl} alt="" fill />
       </div>
       <div className="text-white/50 text-[12px] leading-[1.5em] mt-[28px] md:text-[13px] md:leading-[22px] md:mt-[32px] lg:text-[14px] lg:mt-[36px]">
-        {date}
+        {new Intl.DateTimeFormat('en-US', {
+          day: 'numeric',
+          month: 'long',
+          year: 'numeric',
+          hour: 'numeric',
+          minute: 'numeric',
+        }).format(date)}
       </div>
       <Heading className="mt-[8px]">{title}</Heading>
-      <MarkdownText>{description}</MarkdownText>
+      <MarkdownText>{content}</MarkdownText>
       <div className="mt-[20px] md:mt-[24px] lg:mt-[32px] px-[10px] py-[6px] rounded-[2px] border border-haqq-border text-[11px] leading-[1.55em] md:leading-[18px] lg:text-[12px] text-center max-w-fit">
         {category === 'news' && 'News'}
         {category === 'technology' && 'Technology'}
@@ -33,9 +42,13 @@ export function NewsArticle({
           Share the article
         </span>
         <div className="flex items-center gap-x-[16px] text-white/50">
-          <TwitterIcon className="hover:text-white cursor-pointer transition-colors duration-100 ease-out" />
-          <TelegramIcon className="hover:text-white cursor-pointer transition-colors duration-100 ease-out" />
-          <CopyIcon className="hover:text-white cursor-pointer transition-colors duration-100 ease-out" />
+          {/* <TwitterIcon className="hover:text-white cursor-pointer transition-colors duration-100 ease-out" /> */}
+          {/* <TelegramIcon className="hover:text-white cursor-pointer transition-colors duration-100 ease-out" /> */}
+          <div>
+            <button aria-label="Copy" onClick={onLinkCopy}>
+              <CopyIcon className="hover:text-white cursor-pointer transition-colors duration-100 ease-out" />
+            </button>
+          </div>
         </div>
       </div>
     </section>
