95dc23b9afc74e835d1992159a0df33c940a6059
Jihad HadjHusein <skiptomylou999@gmail.com>
2023-05-30 15:15:49 +0200

    fix(website): recent posts block types

--- libs/website/news-page/src/lib/recent-posts-block/recent-posts-block.tsx
+++ libs/website/news-page/src/lib/recent-posts-block/recent-posts-block.tsx
@@ -1,17 +1,18 @@
 import { Heading, NewsCard } from '@haqq/website/ui-kit';
+import { Post } from '../post-page/post-page';
 
-export function RecentPostsBlock({ recentPosts }: { recentPosts: any[] }) {
+export function RecentPostsBlock({ recentPosts }: { recentPosts: Post[] }) {
   return (
     <section className="flex flex-col items-start px-[16px] pb-[48px] md:pb-[68px] md:px-[48px] lg:px-[110px] lg:pb-[100px] 2xl:px-[320px]">
       <Heading level={3}>Recent posts</Heading>
       <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-[28px] md:gap-[38px] mt-[28px] lg:mt-[32px]">
-        {recentPosts.map((post) => (
+        {recentPosts?.map((post) => (
           <NewsCard
             key={post.id}
             title={post.title}
             imageUrl={post.imageUrl}
             id={post.id}
-            description={post.description}
+            content={post.content}
             date={post.date}
             category={post.category}
           />
