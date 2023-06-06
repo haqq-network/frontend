291273f6aadca1e46a5ac30cee926e155b4bc0d3
Oleg Shilov <me@olegshilov.com>
2023-06-06 10:01:48 +0700

    fix(website): make blog breadcrumb sticky

--- libs/website/news-page/src/lib/post-page/post-page.tsx
+++ libs/website/news-page/src/lib/post-page/post-page.tsx
@@ -1,9 +1,10 @@
 import { NewsArticle, Breadcrumb } from '@haqq/website/ui-kit';
 import Head from 'next/head';
-import { Fragment, useCallback, useState } from 'react';
+import { Fragment, useCallback } from 'react';
 // import { RecentPostsBlock } from '../recent-posts-block/recent-posts-block';
 import { useRouter } from 'next/router';
 import toast from 'react-hot-toast';
+import clsx from 'clsx';
 
 type PostPageProps = {
   post: any;
@@ -12,7 +13,6 @@ type PostPageProps = {
 
 export function PostPage({ post, recentPosts }: PostPageProps) {
   const { push } = useRouter();
-  // const [isLinkCopied, setIsLinkCopied] = useState(false);
 
   const copyLink = useCallback(async () => {
     const copyPromise = navigator.clipboard.writeText(window.location.href);
@@ -34,7 +34,13 @@ export function PostPage({ post, recentPosts }: PostPageProps) {
         <title>HAQQ | Blog | {post.title}</title>
       </Head>
 
-      <div className="border-b border-[#2A2A2B] px-[16px] sm:px-[63px] lg:px-[79px] overflow-clip">
+      <div
+        className={clsx(
+          'border-b border-[#2A2A2B] px-[16px] sm:px-[63px] lg:px-[79px] overflow-clip',
+          'bg-haqq-black backdrop-blur transform-gpu',
+          'sticky z-50 top-[63px] sm:top-[72px]',
+        )}
+      >
         <Breadcrumb
           title={post.title}
           onBackClick={() => {
