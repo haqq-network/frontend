d603b75b78a1a4d4a6deb9b218b89fb2b748942d
Jihad HadjHusein <skiptomylou999@gmail.com>
2023-06-09 14:27:12 +0200

    fix(faucet): update faucet styles

--- apps/faucet/src/components/Faucet.tsx
+++ apps/faucet/src/components/Faucet.tsx
@@ -10,8 +10,9 @@ import { PulseLoader } from 'react-spinners';
 import { useAccount, useConnect, useNetwork } from 'wagmi';
 import { hexValue } from 'ethers/lib/utils';
 import { environment } from '../environments/environment';
-import { getChainParams, useConfig, useTheme } from '@haqq/shared';
-import { Button2 } from '@haqq/ui-kit';
+import { getChainParams, useConfig } from '@haqq/shared';
+import { Button, CardText } from '@haqq/shell/ui-kit';
+import clsx from 'clsx';
 
 interface ClaimInfo {
   available: boolean;
@@ -19,6 +20,22 @@ interface ClaimInfo {
 }
 
 const { serviceConfig, reCaptchaConfig } = environment;
+
+function CardHeading({
+  children,
+  className,
+}: PropsWithChildren<{ className?: string }>) {
+  return (
+    <h3
+      className={clsx(
+        'font-serif text-[20px] font-[500] uppercase leading-[26px] text-white',
+        className,
+      )}
+    >
+      {children}
+    </h3>
+  );
+}
 
 export function Faucet(): ReactElement {
   const { chainName } = useConfig();
@@ -36,7 +53,6 @@ export function Faucet(): ReactElement {
   const [isTokensClaimed, setTokensClaimed] = useState<boolean>(false);
   const [claimInfo, setClaimInfo] = useState<ClaimInfo>();
   const [claimIsLoading, setClaimIsLoading] = useState<boolean>(false);
-  const { isDark } = useTheme();
   const { isConnected, address } = useAccount();
   const { connect, connectors, error, isLoading, pendingConnector } =
     useConnect();
@@ -179,149 +195,146 @@ export function Faucet(): ReactElement {
 
   return (
     <div className="container mx-auto px-2 sm:px-6 lg:px-8">
-      <div className="mx-auto my-20 max-w-md transform-gpu rounded-xl bg-white/50 shadow-lg backdrop-blur dark:bg-slate-700/40">
-        <div className="flex flex-col space-y-5 py-6">
-          <div className="px-5">
-            <div className="mb-4 flex flex-row  space-x-4">
-              <h2 className="text-md inline-flex flex-1 font-semibold uppercase text-[#0c0c0c] dark:text-gray-100">
-                Connect wallet
-              </h2>
+      <div className="mx-auto my-20 max-w-lg transform-gpu overflow-hidden rounded-[8px] bg-[#ffffff14]">
+        <div className="flex flex-col space-y-5 px-5 py-6">
+          <div className="flex flex-row justify-between">
+            <CardHeading>Connect wallet</CardHeading>
 
-              {currentChain?.unsupported && (
-                <div className="flex items-center">
-                  <span
-                    className="cursor-pointer text-sm leading-snug text-[#5baacd] hover:text-[#5baacd]/80"
-                    onClick={handleNetworkSwitch}
+            {currentChain?.unsupported && (
+              <div className="flex items-center">
+                <span
+                  className="text-haqq-orange hover:text-haqq-light-orange cursor-pointer text-sm leading-snug transition-colors duration-300"
+                  onClick={handleNetworkSwitch}
+                >
+                  Switch to {chain.name}
+                </span>
+              </div>
+            )}
+          </div>
+
+          {!isConnected && (
+            <div className="flex flex-col space-y-2">
+              {connectors.map((connector) => {
+                if (!connector.ready) {
+                  return null;
+                }
+
+                return (
+                  <Button
+                    key={connector.id}
+                    onClick={() => {
+                      connect({ connector });
+                    }}
+                    variant={2}
                   >
-                    Switch to {chain.name}
-                  </span>
+                    {connector.name}
+                    {isLoading &&
+                      connector.id === pendingConnector?.id &&
+                      '(connecting)'}
+                  </Button>
+                );
+              })}
+
+              {error && <div>{error.message}</div>}
+            </div>
+          )}
+
+          {isConnected && <AccountInfo />}
+
+          <CardHeading>Github Auth</CardHeading>
+
+          {isAuthenticated ? (
+            <div className="mt-[16px] flex flex-row gap-[16px]">
+              {user && (
+                <div className="flex flex-1 flex-row items-center space-x-4">
+                  {user.picture && (
+                    <img
+                      src={user.picture}
+                      alt={user.nickname}
+                      className="h-10 w-10 rounded-full"
+                    />
+                  )}
+                  <p>{user.nickname}</p>
                 </div>
               )}
             </div>
+          ) : (
+            <Button
+              className="mt-[16px] w-full"
+              onClick={handleLogin}
+              disabled={isAuth0Loading}
+              variant={2}
+            >
+              Login with github
+            </Button>
+          )}
+        </div>
 
-            {!isConnected && (
-              <div className="flex flex-col space-y-2">
-                {connectors.map((connector) => {
-                  if (!connector.ready) {
-                    return null;
-                  }
+        {isAuthenticated && address && (
+          <div className="flex flex-col gap-[16px] bg-white bg-opacity-[15%] px-[20px] py-[24px]">
+            <CardHeading>
+              {isCountDownVisible
+                ? 'Next request tokens available after'
+                : 'Claim tokens'}
+            </CardHeading>
 
-                  return (
-                    <Button2
-                      fill
-                      key={connector.id}
-                      onClick={() => {
-                        connect({ connector });
-                      }}
-                    >
-                      {connector.name}
-                      {isLoading &&
-                        connector.id === pendingConnector?.id &&
-                        ' (connecting)'}
-                    </Button2>
-                  );
-                })}
-
-                {error && <div>{error.message}</div>}
+            {claimIsLoading && (
+              <div className="my-2 flex flex-row items-center space-x-4">
+                <PulseLoader color="#FF8D69" speedMultiplier={0.7} size={16} />
               </div>
             )}
 
-            {isConnected && <AccountInfo />}
-          </div>
-
-          <div className="px-5">
-            <h2 className="text-md mb-4 font-semibold uppercase text-[#0c0c0c] dark:text-gray-100">
-              Github Auth
-            </h2>
-
-            {isAuthenticated ? (
-              <div className="flex flex-row space-x-4">
-                {user && (
-                  <div className="flex flex-1 flex-row items-center space-x-4">
-                    {user.picture && (
-                      <img
-                        src={user.picture}
-                        alt={user.nickname}
-                        className="h-10 w-10 rounded-full"
-                      />
-                    )}
-                    <p>{user.nickname}</p>
+            {isRequestTokensAvailable && (
+              <div>
+                {isRecaptchaVerified ? (
+                  <div>
+                    <Button
+                      variant={2}
+                      className="w-full"
+                      onClick={handleRequestTokens}
+                    >
+                      Request tokens
+                    </Button>
+                  </div>
+                ) : (
+                  <div className="flex flex-row items-center space-x-4">
+                    <Reaptcha
+                      sitekey={reCaptchaConfig.siteKey}
+                      onVerify={handleRecapthcaVerify}
+                      theme={'dark'}
+                    />
                   </div>
                 )}
               </div>
-            ) : (
-              <Button2 fill onClick={handleLogin} disabled={isAuth0Loading}>
-                Login with github
-              </Button2>
+            )}
+
+            {isTokensClaimed && (
+              <div className="flex flex-row items-center space-x-4">
+                <SuccessIndicator size="36px" color="green" />
+                <CardText>Tokens claimed!</CardText>
+              </div>
+            )}
+
+            {isCountDownVisible && (
+              <div className="flex flex-row items-center space-x-4">
+                <Countdown
+                  date={Date.now() + claimInfo.next_claim_sec * 1000}
+                  onComplete={requestClaimInfo}
+                  renderer={({ hours, minutes, seconds }) => {
+                    // Render a countdown
+                    return (
+                      <div className="text-4xl font-semibold leading-tight">
+                        {hours < 10 ? '0' + hours : hours}:
+                        {minutes < 10 ? '0' + minutes : minutes}:
+                        {seconds < 10 ? '0' + seconds : seconds}
+                      </div>
+                    );
+                  }}
+                />
+              </div>
             )}
           </div>
-
-          {isAuthenticated && address && (
-            <div className="px-5">
-              <h2 className="text-md mb-5 font-semibold uppercase text-[#0c0c0c] dark:text-gray-100">
-                {isCountDownVisible
-                  ? 'Next request tokens available after'
-                  : 'Claim tokens'}
-              </h2>
-
-              {claimIsLoading && (
-                <div className="my-2 flex flex-row items-center space-x-4">
-                  <PulseLoader
-                    color="#5baacd"
-                    speedMultiplier={0.7}
-                    size={16}
-                  />
-                </div>
-              )}
-
-              {isRequestTokensAvailable && (
-                <div>
-                  {isRecaptchaVerified ? (
-                    <div>
-                      <Button2 fill onClick={handleRequestTokens}>
-                        Request tokens
-                      </Button2>
-                    </div>
-                  ) : (
-                    <div className="flex flex-row items-center space-x-4">
-                      <Reaptcha
-                        sitekey={reCaptchaConfig.siteKey}
-                        onVerify={handleRecapthcaVerify}
-                        theme={isDark ? 'dark' : 'light'}
-                      />
-                    </div>
-                  )}
-                </div>
-              )}
-
-              {isTokensClaimed && (
-                <div className="flex flex-row items-center space-x-4">
-                  <SuccessIndicator size="36px" color="green" />
-                  <p>Tokens claimed!</p>
-                </div>
-              )}
-
-              {isCountDownVisible && (
-                <div className="flex flex-row items-center space-x-4">
-                  <Countdown
-                    date={Date.now() + claimInfo.next_claim_sec * 1000}
-                    onComplete={requestClaimInfo}
-                    renderer={({ hours, minutes, seconds }) => {
-                      // Render a countdown
-                      return (
-                        <div className="text-4xl font-semibold leading-tight">
-                          {hours < 10 ? '0' + hours : hours}:
-                          {minutes < 10 ? '0' + minutes : minutes}:
-                          {seconds < 10 ? '0' + seconds : seconds}
-                        </div>
-                      );
-                    }}
-                  />
-                </div>
-              )}
-            </div>
-          )}
-        </div>
+        )}
       </div>
     </div>
   );
