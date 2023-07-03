dfca3da3167427ca365d9033b5ce5574a514b36c
Oleg Shilov <me@olegshilov.com>
2023-07-04 00:05:25 +0400

    fix(shell): set different module federation remotes for preview builds
    - cleanup;

--- apps/shell/src/environments/environment.ts
+++ apps/shell/src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   isProduction: false,
-  chainName: process.env['NX_NETWORK'] ?? 'testedge2',
   sentryDsn: process.env['NX_SHELL_SENTRY_DSN'],
   walletConnectProjectId: process.env['NX_WALLETCONNECT_PROJECT_ID'],
 };
