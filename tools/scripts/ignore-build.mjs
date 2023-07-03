bfd1cf41a0667e725670c285dc4f79f280af565c
Oleg Shilov <me@olegshilov.com>
2023-07-04 00:12:32 +0400

    chore(deploy): force build all apps for dev and prod branches

--- tools/scripts/ignore-build.mjs
+++ tools/scripts/ignore-build.mjs
@@ -3,16 +3,18 @@
 import { exec } from 'child_process';
 
 const APP = process.argv.slice(2)[0];
-const FORCE_BUILD_BRANCH = 'dev';
 const GIT_BRANCH_VERCEL = process.env['VERCEL_GIT_COMMIT_REF'];
 
 console.log(`current branch: "${GIT_BRANCH_VERCEL}"`);
 console.log(`check build need for "${APP}"`);
-if (FORCE_BUILD_BRANCH === GIT_BRANCH_VERCEL) {
-  console.log('current branch is dev, force build');
+
+if (GIT_BRANCH_VERCEL === 'dev' || GIT_BRANCH_VERCEL === 'prod') {
+  console.log(`current branch is 'dev' or 'prod', force build`);
   process.exit(1);
 } else {
-  console.log(`current branch is not dev, check build need for "${APP}"`);
+  console.log(
+    `current branch is not 'dev' or 'prod', check build need for "${APP}"`,
+  );
   const IGNORE_CMD = `yarn dlx nx-ignore ${APP}`;
 
   exec(IGNORE_CMD).on('close', (code) => {
