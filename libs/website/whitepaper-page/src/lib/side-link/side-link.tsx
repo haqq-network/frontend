10e4e9cb18bf3ff8148eae9fcd65e08b07567b0c
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-07-04 18:04:15 +0300

    feat(website): add side link component

---
+++ libs/website/whitepaper-page/src/lib/side-link/side-link.tsx
@@ -0,0 +1,31 @@
+import clsx from 'clsx';
+
+interface SideLinkProps {
+  isActive?: boolean;
+  children: string;
+}
+
+function BarAndSquare() {
+  return (
+    <div className="flex items-center">
+      <div className="h-[1px] w-full min-w-[30px]  bg-[#0d0d0e3d] xl:min-w-[50px]" />
+      <div className="bg-haqq-black h-[12px] w-[13px] xl:h-[16px] xl:w-[17px]" />
+    </div>
+  );
+}
+
+export function SideLink({ isActive, children }: SideLinkProps) {
+  return (
+    <div className="grid w-full grid-cols-2 gap-x-[16px]">
+      {isActive ? <BarAndSquare /> : <div />}
+      <div
+        className={clsx(
+          'text-[13px] xl:text-[14px] xl:leading-[22px]',
+          isActive ? 'text-haqq-black' : 'text-[#0d0d0e3d]',
+        )}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
