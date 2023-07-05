9432ae7d939b14bbcac219e1d9260f4b8e850f11
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-07-05 20:09:08 +0300

    fix(website): detect headings in markdown text

--- libs/website/whitepaper-page/src/lib/whitepaper-page/whitepaper-page.tsx
+++ libs/website/whitepaper-page/src/lib/whitepaper-page/whitepaper-page.tsx
@@ -1,12 +1,24 @@
 import Head from 'next/head';
-import { Fragment } from 'react';
+import { Fragment, useMemo } from 'react';
 import { WhitepaperMarkdownText } from '../whitepaper-markdown-text/whitepaper-markdown-text';
 import { SideLink } from '../side-link/side-link';
-
 import { CoinomicsModal } from '../coinomics-modal/coinomics-modal';
 import { PageTitle } from '../hero-block/hero-block';
 
 export function WhitepaperPage({ whitepaper }: { whitepaper: string }) {
+  function findH2Elements(markdownString: string): string[] {
+    const h2Regex = /## (.+?)(?:\n\n|$)/g;
+    const matches = markdownString.match(h2Regex) || [];
+    const h2Elements = matches.map((match) => {
+      return match.replace('## ', '').replace(/\n\n$/, '');
+    });
+    return h2Elements;
+  }
+
+  const h2Elements = useMemo(() => {
+    return findH2Elements(whitepaper);
+  }, [whitepaper]);
+
   return (
     <Fragment>
       <Head>
@@ -15,13 +27,9 @@ export function WhitepaperPage({ whitepaper }: { whitepaper: string }) {
       <PageTitle>HAQQ Whitepaper</PageTitle>
       <section className="flex bg-white">
         <div className="hidden items-end gap-y-[16px] border-r border-r-[#0d0d0e3d] pr-[34px] pt-[56px] md:flex md:w-1/3 md:flex-col xl:w-1/5">
-          <SideLink isActive>Lorem ipsum dolor</SideLink>
-          <SideLink>Islam</SideLink>
-          <SideLink>Crypto</SideLink>
-          <SideLink isActive>Blockchain</SideLink>
-          <SideLink>Synthesis</SideLink>
-          <SideLink isActive>Founders Reward</SideLink>
-          <SideLink isActive>Private Sale and Partners Allocations</SideLink>
+          {h2Elements.map((h2Element) => {
+            return <SideLink key={h2Element}>{h2Element}</SideLink>;
+          })}
         </div>
         <WhitepaperMarkdownText className="w-full px-[16px] py-[60px] md:py-[80px] md:pl-[20px] md:pr-[64px] xl:pl-[150px] xl:pr-[200px]">
           {whitepaper}
