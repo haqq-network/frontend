08ac1ef7cfbd77dec9191b681189abcce919116c
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-07-14 14:50:25 +0300

    fix(vesting): add icon to address link, remove "block" inscription

--- apps/vesting/src/components/DepositWithdrawalList/DepositWithdrawalList.tsx
+++ apps/vesting/src/components/DepositWithdrawalList/DepositWithdrawalList.tsx
@@ -304,8 +304,8 @@ function DepositWithdrawalListItem({
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
+              fillRule="evenodd"
+              clipRule="evenodd"
               d="M14.224 8C14.224 10.9455 11.8361 13.3333 8.89063 13.3333C5.94511 13.3333 3.55729 10.9455 3.55729 8C3.55729 5.05448 5.94511 2.66667 8.89063 2.66667C11.8361 2.66667 14.224 5.05448 14.224 8ZM15.5573 8C15.5573 11.6819 12.5725 14.6667 8.89063 14.6667C5.20873 14.6667 2.22396 11.6819 2.22396 8C2.22396 4.3181 5.20873 1.33333 8.89063 1.33333C12.5725 1.33333 15.5573 4.3181 15.5573 8ZM9.55729 4C9.55729 3.63181 9.25882 3.33333 8.89063 3.33333C8.52244 3.33333 8.22396 3.63181 8.22396 4V7.64321L6.52083 8.77863C6.21447 8.98287 6.13169 9.39678 6.33593 9.70313C6.54016 10.0095 6.95407 10.0923 7.26043 9.88803L9.08231 8.67345C9.37905 8.47562 9.55729 8.14257 9.55729 7.78593V4Z"
               fill="currentColor"
             />
@@ -315,8 +315,8 @@ function DepositWithdrawalListItem({
       </div>
 
       <div className="mt-[8px] flex items-center justify-between">
-        <div className="flex flex-row items-center gap-[8px]">
-          <div className="flex flex-row items-center gap-[4px]">
+        <div className="flex items-center gap-[16px]">
+          <div className="flex items-center gap-[4px]">
             <svg
               width="16"
               height="16"
@@ -329,10 +329,6 @@ function DepositWithdrawalListItem({
                 fill="currentColor"
               />
             </svg>
-            <div>Block:</div>
-          </div>
-
-          <div className="flex flex-row items-center gap-[12px]">
             <a
               href={`${EXPLORER_LINK}/block/${withdrawal.blockNumber}`}
               target="_blank"
@@ -341,7 +337,22 @@ function DepositWithdrawalListItem({
             >
               {withdrawal.blockNumber}
             </a>
-
+          </div>
+          <div className="flex items-center gap-[4px]">
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M13 5.5H6C4.89543 5.5 4 6.39543 4 7.5V11.5C4 12.6046 4.89543 13.5 6 13.5H13C13.7571 13.5 14.4159 13.0793 14.7555 12.459C15.0207 11.9745 15.4477 11.5 16 11.5C16.5523 11.5 17.0128 11.9547 16.8766 12.4899C16.4361 14.2202 14.8675 15.5 13 15.5H6C3.79086 15.5 2 13.7091 2 11.5V7.5C2 5.29086 3.79086 3.5 6 3.5H13C14.8675 3.5 16.4361 4.77976 16.8766 6.51012C17.0128 7.04533 16.5523 7.5 16 7.5C15.4477 7.5 15.0207 7.02548 14.7555 6.54103C14.4159 5.92067 13.7571 5.5 13 5.5ZM18 10.5H11C10.2429 10.5 9.58407 10.9207 9.24447 11.541C8.97928 12.0255 8.55228 12.5 8 12.5C7.44772 12.5 6.98717 12.0453 7.12343 11.5101C7.56394 9.77976 9.13252 8.5 11 8.5H18C20.2091 8.5 22 10.2909 22 12.5V16.5C22 18.7091 20.2091 20.5 18 20.5H11C9.13252 20.5 7.56394 19.2202 7.12343 17.4899C6.98717 16.9547 7.44772 16.5 8 16.5C8.55228 16.5 8.97928 16.9745 9.24447 17.459C9.58406 18.0793 10.2429 18.5 11 18.5H18C19.1046 18.5 20 17.6046 20 16.5V12.5C20 11.3954 19.1046 10.5 18 10.5Z"
+                fill="#2F2F2F"
+              />
+            </svg>
             <a
               href={`${EXPLORER_LINK}/tx/${withdrawal.transactionHash}`}
               target="_blank"
