1205ebb08865324ae4d2d06bab51c3d75eafbce0
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-07-19 13:02:55 +0300

    fix(staking): prevent anchor save in browser history

--- libs/staking/validator-details-page/src/components/validator-info/validator-info.tsx
+++ libs/staking/validator-details-page/src/components/validator-info/validator-info.tsx
@@ -1,6 +1,13 @@
 import { Fragment, useCallback, useEffect, useMemo, useState } from 'react';
 import { useAccount, useBalance, useNetwork } from 'wagmi';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import {
+  Link,
+  NavigateFunction,
+  RelativeRoutingType,
+  To,
+  useLocation,
+  useNavigate,
+} from 'react-router-dom';
 import {
   useAddress,
   useStakingValidatorInfoQuery,
@@ -585,7 +592,6 @@ export function ValidatorInfo({
     </Fragment>
   );
 }
-
 export function ValidatorBlockDesktop({
   validatorInfo,
   delegation,
@@ -639,7 +645,7 @@ export function ValidatorBlockDesktop({
               disabled={balance < 1}
               className="w-full"
               onClick={() => {
-                navigate(`#delegate`);
+                navigate(`#delegate`, { replace: true });
               }}
             >
               Delegate
@@ -651,7 +657,7 @@ export function ValidatorBlockDesktop({
               className="w-full"
               disabled={delegation === 0}
               onClick={() => {
-                navigate(`#undelegate`);
+                navigate(`#undelegate`, { replace: true });
               }}
             >
               Undelegate
@@ -701,10 +707,10 @@ function ValidatorBlockMobile({
     <ValidatorBlockMobileComponent
       onGetRewardClick={onGetRewardsClick}
       onDelegateClick={() => {
-        navigate(`#delegate`);
+        navigate(`#delegate`, { replace: true });
       }}
       onUndelegateClick={() => {
-        navigate(`#undelegate`);
+        navigate(`#undelegate`, { replace: true });
       }}
       isDelegateDisabled={balance < 1}
       isUndelegateDisabled={delegation === 0}
