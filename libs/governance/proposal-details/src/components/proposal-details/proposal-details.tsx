a8b8bc7e78f5d1b55f708033ed0a9abb90e93843
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-07-19 13:48:46 +0300

    fix(governance): prevent anchor save in browser history

--- libs/governance/proposal-details/src/components/proposal-details/proposal-details.tsx
+++ libs/governance/proposal-details/src/components/proposal-details/proposal-details.tsx
@@ -594,7 +594,7 @@ export function ProposalDetailsComponent({
             onConnectWalletClick={openSelectWallet}
             isDepositAvailable={isDepositAvailable}
             onDepositWalletClick={() => {
-              navigate(`#deposit`);
+              navigate(`#deposit`, { replace: true });
             }}
           />
           {/* <ProposalDepositModal
