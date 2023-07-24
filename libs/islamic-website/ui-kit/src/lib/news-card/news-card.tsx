0c96be0517daeacf902beefc9e92a5f3fa372d70
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-07-24 19:09:33 +0300

    feat(islamic-website-ui-kit): add news card component & story

---
+++ libs/islamic-website/ui-kit/src/lib/news-card/news-card.tsx
@@ -0,0 +1,58 @@
+import clsx from 'clsx';
+import Image from 'next/image';
+
+interface NewsCardProps {
+  image: {
+    src: string;
+    width: number;
+    height: number;
+  } | null;
+  title: string;
+  description: string;
+  date: Date;
+  source: string;
+  className?: string;
+}
+
+export function NewsCard({
+  date,
+  description,
+  image,
+  title,
+  source,
+  className,
+}: NewsCardProps) {
+  return (
+    <div
+      className={clsx('flex max-w-[400px] flex-col bg-transparent', className)}
+    >
+      <div className="relative h-[250px] w-full">
+        {image && (
+          <Image
+            src={image.src}
+            width={image.width}
+            height={image.height}
+            alt=""
+            className="h-full w-full rounded-xl object-cover"
+          />
+        )}
+      </div>
+      <div className="mt-[28px] text-[#838383]">{source}</div>
+      <div className="mt-[8px] line-clamp-2 text-[24px] font-[700] leading-[34px] text-[#272727]">
+        {title}
+      </div>
+      <div className="mt-[12px] line-clamp-3 text-[16px] font-[400] leading-[1.5em] text-[#555555]">
+        {description}
+      </div>
+      <div className="mt-[12px] text-[12px] leading-[1.5em] text-[#838383]">
+        {new Intl.DateTimeFormat('en-US', {
+          day: 'numeric',
+          month: 'long',
+          year: 'numeric',
+          hour: 'numeric',
+          minute: 'numeric',
+        }).format(date)}
+      </div>
+    </div>
+  );
+}
