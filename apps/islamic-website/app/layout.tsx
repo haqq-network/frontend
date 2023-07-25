7a62fced109281908f2a1e99bf9e43284bed38e2
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-07-25 18:02:59 +0300

    fix(islamic-website): add header & footer to layout

--- apps/islamic-website/app/layout.tsx
+++ apps/islamic-website/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Footer } from '../components/footer/footer';
+import { Header } from '../components/header/header';
 import './global.css';
 
 export const metadata = {
@@ -12,7 +14,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
