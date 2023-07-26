99eabb419ec8e191095469e96808aa52aef990d6
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-07-26 12:05:20 +0300

    feat(haqq-website): add google analytics script

--- libs/website/index-page/src/lib/index-page/index-page.tsx
+++ libs/website/index-page/src/lib/index-page/index-page.tsx
@@ -7,6 +7,7 @@ import { MissionBlock } from '../mission-block/mission-block';
 import Head from 'next/head';
 import { VisionBlock } from '../vision-block/vision-block';
 import { StatisticsBlock } from '../statistics-block/statistics-block';
+import Script from 'next/script';
 
 export function WebsiteIndexPage() {
   return (
@@ -17,6 +18,18 @@ export function WebsiteIndexPage() {
           name="description"
           content="Ethics-first network that welcomes sustainability-centered developers, validators and open source contributors as well as Muslim innovators in sustainable Finance"
         />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env['GOOGLE_TAG_ID']}`}
+        />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${process.env['GOOGLE_TAG_ID']}');
+        `}
+        </Script>
       </Head>
       <section>
         <HeroBlock />
