006bfd49805520e9109f4a4d46429bfdb090f4da
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-08-01 23:38:27 +0300

    feat(islamic-website): add heroblock
    closes: HQW-46

---
+++ libs/islamic-website/index-page/src/lib/hero-block/hero-block.tsx
@@ -0,0 +1,23 @@
+import { Button, GradientText, Text } from '@haqq/islamic-ui-kit';
+
+export function HeroBlock() {
+  return (
+    <div className="mt-[150px] flex flex-col text-white">
+      <div className="text-[28px] font-[600] leading-[32px] md:text-[44px] md:leading-[48px] lg:text-[64px] lg:leading-[70px]">
+        Your Gateway <br className="block md:hidden" /> to{' '}
+        <br className="hidden md:block" /> Shariah{' '}
+        <GradientText>
+          Compliant <br className="block xl:hidden" /> Web3
+        </GradientText>
+      </div>
+      <Text size="small" className="mt-[24px] text-white/50 md:mt-[40px]">
+        We are on a mission to onboard over 1.8+ billion Muslims into digital
+        finance without compromising the values or faith. Islamic Coin is the
+        native currency of HAQQ Network, an ethics first blockchain
+      </Text>
+      <div className="mt-[36px] md:mt-[48px]">
+        <Button>Get started</Button>
+      </div>
+    </div>
+  );
+}
