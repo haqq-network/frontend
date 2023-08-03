c06f266584bc0a9b52c50c1552b9eae6a2dee903
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-08-03 14:08:43 +0300

    fix(islamic-website): add assets & use block on main page

--- libs/islamic-website/index-page/src/lib/index-page/index-page.tsx
+++ libs/islamic-website/index-page/src/lib/index-page/index-page.tsx
@@ -1,9 +1,69 @@
+import Image from 'next/image';
+import { HeroBlock } from '../hero-block/hero-block';
 import { JoinCommunityBlock } from '../join-community-block/join-community-block';
 import { LearnAndGrowBlock } from '../learn-and-grow-block/learn-and-grow-block';
 import { NewsBlock } from '../news-block/news-block';
+import { PortfolioBlock } from '../portfolio-block/portfolio-block';
 import { WhyBlock } from '../why-block/why-block';
+import moonBgData from '../../assets/images/moon.png';
+import clsx from 'clsx';
+import styles from './index-page.module.css';
+import { Fragment } from 'react';
+import { Marquee } from '../marquee/marquee';
 
 const mockNews = [
+  {
+    date: new Date(),
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title:
+      'News Title News TitleNews TitleNews TitleNews TitleNews TitleNews TitleNews Title',
+    image: {
+      height: 300,
+      width: 200,
+      src: 'https://picsum.photos/200/300',
+    },
+    source: 'mockwebsite.com',
+  },
+  {
+    date: new Date(),
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title:
+      'News Title News TitleNews TitleNews TitleNews TitleNews TitleNews TitleNews Title',
+    image: {
+      height: 300,
+      width: 200,
+      src: 'https://picsum.photos/200/300',
+    },
+    source: 'mockwebsite.com',
+  },
+  {
+    date: new Date(),
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title:
+      'News Title News TitleNews TitleNews TitleNews TitleNews TitleNews TitleNews Title',
+    image: {
+      height: 300,
+      width: 200,
+      src: 'https://picsum.photos/200/300',
+    },
+    source: 'mockwebsite.com',
+  },
+  {
+    date: new Date(),
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title:
+      'News Title News TitleNews TitleNews TitleNews TitleNews TitleNews TitleNews Title',
+    image: {
+      height: 300,
+      width: 200,
+      src: 'https://picsum.photos/200/300',
+    },
+    source: 'mockwebsite.com',
+  },
   {
     date: new Date(),
     description:
@@ -60,12 +120,18 @@ const mockNews = [
 
 export function IndexPage() {
   return (
-    <div>
-      <h1>Welcome to IslamicWebsiteIndexPage!</h1>
+    <Fragment>
+      <HeroBlock />
+      <Marquee className="mt-[160px]">
+        Our mission is to empower the world's Muslim community with a financial
+        instrument for the Digital Age, that enables seamless transactions and
+        interaction, while supporting innovation and philanthropy.
+      </Marquee>
       <WhyBlock />
-      <JoinCommunityBlock />
-      {/* <NewsBlock news={mockNews} /> */}
+      <PortfolioBlock />
       <LearnAndGrowBlock />
-    </div>
+      <NewsBlock news={mockNews} />
+      <JoinCommunityBlock />
+    </Fragment>
   );
 }
