de55b5a0ab2e27856479b8ee9bc03fed084a7197
Oleg Shilov <me@olegshilov.com>
2023-08-10 14:39:22 +0700

    fix(islamic-website): wrap lists into Text component

--- libs/islamic-website/shariah-page/src/lib/fatwa-block/fatwa-block.tsx
+++ libs/islamic-website/shariah-page/src/lib/fatwa-block/fatwa-block.tsx
@@ -116,32 +116,35 @@ function EnFatwa() {
         its administrators, and their answers to the Authority’s questions on
         this matter, that included the following:
       </Text>
-      <ol className="list-decimal pl-[20px]">
-        <li>
-          HAQQ Chain platform will be built using a technical framework that is
-          well-known in the blockchain field, which is the Cosmos technical
-          framework.
-        </li>
-        <li>
-          The platform will operate on a “Proof of ownership” basis for mining,
-          so that users freeze several their currencies to have Authority mining
-          according to specific criteria. This method is more energy-efficient
-          than the traditional mining method based on “Proof of work.”
-        </li>
-        <li>
-          That the main currency used in “HAQQ Chain” platform is the “Islamic
-          Coin” currency, and a certain percentage of these currencies will be
-          issued exclusively for a limited number of buyers.
-        </li>
-        <li>
-          That 10% of the currencies issued on the platform will be
-          automatically converted to the Evergreen DAO Endowment Fund (An
-          independent decentralized entity), which is an endowment
-          cryptocurrency fund that invests in projects that serve the
-          International Islamic community and is subject to the decisions voted
-          on by the miners of HAQQ Chain platform.
-        </li>
-      </ol>
+      <Text size="small">
+        <ol className="list-decimal pl-[20px]">
+          <li>
+            HAQQ Chain platform will be built using a technical framework that
+            is well-known in the blockchain field, which is the Cosmos technical
+            framework.
+          </li>
+          <li>
+            The platform will operate on a “Proof of ownership” basis for
+            mining, so that users freeze several their currencies to have
+            Authority mining according to specific criteria. This method is more
+            energy-efficient than the traditional mining method based on “Proof
+            of work.”
+          </li>
+          <li>
+            That the main currency used in “HAQQ Chain” platform is the “Islamic
+            Coin” currency, and a certain percentage of these currencies will be
+            issued exclusively for a limited number of buyers.
+          </li>
+          <li>
+            That 10% of the currencies issued on the platform will be
+            automatically converted to the Evergreen DAO Endowment Fund (An
+            independent decentralized entity), which is an endowment
+            cryptocurrency fund that invests in projects that serve the
+            International Islamic community and is subject to the decisions
+            voted on by the miners of HAQQ Chain platform.
+          </li>
+        </ol>
+      </Text>
       <Text size="small">
         After thorough review of the structure of the platform and the white
         paper, and what has been shown to the Authority from the blockchain
@@ -151,35 +154,37 @@ function EnFatwa() {
         reflecting on the types of currencies currently available, which are
         summarized in the following types:
       </Text>
-      <ol className="list-decimal pl-[20px]">
-        <li>
-          Currencies with no specific purpose behind their issuance other than
-          being a store of value and having a price, where the customer aims to
-          save, use in purchases and reservations, get wages through and pay
-          taxes using them in a number of countries.
-        </li>
-        <li>
-          Currencies issued with the purpose of using them for a particular
-          product or on a specific platform, so that it is the official currency
-          through which evaluation and purchase of services and products
-          provided on those platforms can be made. Some of these currencies can
-          become famous and can have an increase in demand that it can have the
-          characteristics of type 1 as well.
-        </li>
+      <Text size="small">
+        <ol className="list-decimal pl-[20px]">
+          <li>
+            Currencies with no specific purpose behind their issuance other than
+            being a store of value and having a price, where the customer aims
+            to save, use in purchases and reservations, get wages through and
+            pay taxes using them in a number of countries.
+          </li>
+          <li>
+            Currencies issued with the purpose of using them for a particular
+            product or on a specific platform, so that it is the official
+            currency through which evaluation and purchase of services and
+            products provided on those platforms can be made. Some of these
+            currencies can become famous and can have an increase in demand that
+            it can have the characteristics of type 1 as well.
+          </li>
 
-        <li>
-          Currencies issued against another financial asset, such as gold,
-          dollars, euros, etc. so that the value of the currency is like the
-          value of the financial asset against which it is issued.
-        </li>
+          <li>
+            Currencies issued against another financial asset, such as gold,
+            dollars, euros, etc. so that the value of the currency is like the
+            value of the financial asset against which it is issued.
+          </li>
 
-        <li>
-          Non-Fungible Tokens, usually symbolized by NFT. These are currencies
-          symbolizing the ownership of NFTs for certain assets, images or
-          specific property, where the currency owner shall be the owner of the
-          asset, image or associated property.
-        </li>
-      </ol>
+          <li>
+            Non-Fungible Tokens, usually symbolized by NFT. These are currencies
+            symbolizing the ownership of NFTs for certain assets, images or
+            specific property, where the currency owner shall be the owner of
+            the asset, image or associated property.
+          </li>
+        </ol>
+      </Text>
       <Text size="small">
         The Authority has seen that these four types of currencies are
         considered significant financial assets whose value changes as supply
@@ -198,24 +203,27 @@ function EnFatwa() {
         First: There is no objection to establish the “HAQQ Chain” Platform and
         issuing the “Islamic Coin” based on the following measures:
       </Text>
-      <ol className="list-decimal pl-[20px]">
-        <li>
-          Islamic Coin is a financial asset that may be traded by sale and
-          purchase and replaced with legitimate services and goods and is
-          considered money of its owners that may not be taken away, and Zakat
-          must be paid by its owners after purchase or after mining and
-          possession according to the Zakat conditions and measures.
-        </li>
-        <li>
-          To check the software code used to build the “HAQQ chain” platform at
-          an accredited software audit company, to ensure the integrity of the
-          platform and its ability to protect the funds of its customers.
-        </li>
-        <li>
-          Appoint an Authority committee for the Evergreen DAO Endowment Fund to
-          ensure the safety of the Fund’s activities.
-        </li>
-      </ol>
+      <Text size="small">
+        <ol className="list-decimal pl-[20px]">
+          <li>
+            Islamic Coin is a financial asset that may be traded by sale and
+            purchase and replaced with legitimate services and goods and is
+            considered money of its owners that may not be taken away, and Zakat
+            must be paid by its owners after purchase or after mining and
+            possession according to the Zakat conditions and measures.
+          </li>
+          <li>
+            To check the software code used to build the “HAQQ chain” platform
+            at an accredited software audit company, to ensure the integrity of
+            the platform and its ability to protect the funds of its customers.
+          </li>
+          <li>
+            Appoint an Authority committee for the Evergreen DAO Endowment Fund
+            to ensure the safety of the Fund’s activities.
+          </li>
+        </ol>{' '}
+      </Text>
+
       <Text size="small">
         Second: The Authority confirms that this view is specific to the white
         paper, which includes details of establishing the “Haqq Chain” Platform,
@@ -278,60 +286,64 @@ function ArFatwa() {
         وبعد الاطلاع على عرض مفصل لعمل المنصة من القائمين عليها، وإجاباتهم عن
         أسئلة الهيئة بهذا الشأن، وقد تضمن العرض النقاط التالية:
       </Text>
-      <ol className="list-decimal pl-[20px]">
-        <li>
-          أن منصة ’الحق تشين’ سيتم بناؤها باستخدام إطار عمل فني معروف في مجال
-          ’البلوك تشين’ وهو الإطار الفني المتعلق بلغة ’كوزموس’.
-        </li>
-        <li>
-          أن المنصة ستعمل على أساس ’تأكيد الملكية’ للتعدين، بحيث يُجمّد عدداً من
-          عملاتهم لتكون لهم صلاحية التعدين وفق معايير معينة، وتعد هذه الطريقة
-          أوفر في استهلاك الطاقة من طريقة التعدين التقليدية التي تقوم على أساس
-          ’تأكيد العمل’.
-        </li>
-        <li>
-          أن العملة الرئيسية المستخدمة في منصة ’الحق تشين’ هي عملة ’إسلاميك
-          كوين’، وستُطرح نسبة معينة من هذه العملات طرحاً خاصاً على عدد محدود من
-          المشترين.
-        </li>
-        <li>
-          أن 10% من العملات المصدرة في المنصة سيتم تحويلها تلقائياً لصندوق
-          ’إيفرجين داو’ الوقفي (مؤسسة مستقلة لا مركزية) وهو صندوق عملات مشفرة
-          وقفي يستثمر في المشروعات التي تخدم المجتمع الإسلامي دولياً، ويخضع
-          الصندوق للقرارات التي يصوت عليها المُعدّنون في منصة ’الحق تشين’.
-        </li>
-      </ol>
-      <p>
+      <Text size="small">
+        <ol className="list-decimal pl-[20px]">
+          <li>
+            أن منصة ’الحق تشين’ سيتم بناؤها باستخدام إطار عمل فني معروف في مجال
+            ’البلوك تشين’ وهو الإطار الفني المتعلق بلغة ’كوزموس’.
+          </li>
+          <li>
+            أن المنصة ستعمل على أساس ’تأكيد الملكية’ للتعدين، بحيث يُجمّد عدداً
+            من عملاتهم لتكون لهم صلاحية التعدين وفق معايير معينة، وتعد هذه
+            الطريقة أوفر في استهلاك الطاقة من طريقة التعدين التقليدية التي تقوم
+            على أساس ’تأكيد العمل’.
+          </li>
+          <li>
+            أن العملة الرئيسية المستخدمة في منصة ’الحق تشين’ هي عملة ’إسلاميك
+            كوين’، وستُطرح نسبة معينة من هذه العملات طرحاً خاصاً على عدد محدود
+            من المشترين.
+          </li>
+          <li>
+            أن 10% من العملات المصدرة في المنصة سيتم تحويلها تلقائياً لصندوق
+            ’إيفرجين داو’ الوقفي (مؤسسة مستقلة لا مركزية) وهو صندوق عملات مشفرة
+            وقفي يستثمر في المشروعات التي تخدم المجتمع الإسلامي دولياً، ويخضع
+            الصندوق للقرارات التي يصوت عليها المُعدّنون في منصة ’الحق تشين’.
+          </li>
+        </ol>
+      </Text>
+      <Text size="small">
         وبعد التأمل في هيكل المنصة وما تضمنته الورقة البيضاء، وما ظهر للهيئة من
         أن تقنية ’البلوك تشين’ وإطار العمل الفني المستخدم في برمجة منصة ’الحق
         تشين’ تُعد تقنية آمنة مُجرّبة يتم من خلالها إجراء التعاملات المالية وحفظ
         أموال المستخدمين المتعاملين فيها. وبعد التأمل في أنواع العملات المتوفرة
         حالياً، والتي تتلخص في الأنواع الآتية:
-      </p>
-      <ol className="list-decimal pl-[20px]">
-        <li>
-          عملات ليس لها هدف معين من إصدارها سوى كونها أثماناً ومخزناً للقيمة،
-          ويهدف المتعاملون فيها من الاحتفاظ بها أو استخدامها في مشترياتهم
-          وحجوزاتهم والحصول على أجورهم من خلالها ودفع ضرائبهم بها في عدد من
-          الدول.
-        </li>
-        <li>
-          عملات يتم إصدارها بهدف التعامل بها في منتج معين أو منصة معينة، بحيث
-          تكون هي العملة الرئيسية التي يتم من خلالها تقييم وشراء الخدمات
-          والمنتجات المقدمة في تلك المنصة أو البرنامج، وقد تشتهر بعض هذه العملات
-          ويزيد الطلب عليها لتحمل خصائص النوع الأول أيضاً.
-        </li>
-        <li>
-          عملات يتم إصدارها بمقابل أصل مالي آخر، كالدولار أو الذهب أو اليورو
-          وغير ذلك، بحيث تكون قيمة العملة مماثلة لقيمة الأصل المالي الذي أصدرت
-          بناء عليه.
-        </li>
-        <li>
-          عملات الرموز غير قابلة للاستبدال، ويرمز لها عادة ب (Non Fungible
-          Tokens)NFT ، هي عملات ترمز إلى ملكية لأصول أو صور أو ممتلكات معينة،
-          ويكون مالك العملة مالكاً للأصل أو الصورة أو الممتلكات المرتبطة بها.
-        </li>
-      </ol>
+      </Text>
+      <Text size="small">
+        <ol className="list-decimal pl-[20px]">
+          <li>
+            عملات ليس لها هدف معين من إصدارها سوى كونها أثماناً ومخزناً للقيمة،
+            ويهدف المتعاملون فيها من الاحتفاظ بها أو استخدامها في مشترياتهم
+            وحجوزاتهم والحصول على أجورهم من خلالها ودفع ضرائبهم بها في عدد من
+            الدول.
+          </li>
+          <li>
+            عملات يتم إصدارها بهدف التعامل بها في منتج معين أو منصة معينة، بحيث
+            تكون هي العملة الرئيسية التي يتم من خلالها تقييم وشراء الخدمات
+            والمنتجات المقدمة في تلك المنصة أو البرنامج، وقد تشتهر بعض هذه
+            العملات ويزيد الطلب عليها لتحمل خصائص النوع الأول أيضاً.
+          </li>
+          <li>
+            عملات يتم إصدارها بمقابل أصل مالي آخر، كالدولار أو الذهب أو اليورو
+            وغير ذلك، بحيث تكون قيمة العملة مماثلة لقيمة الأصل المالي الذي أصدرت
+            بناء عليه.
+          </li>
+          <li>
+            عملات الرموز غير قابلة للاستبدال، ويرمز لها عادة ب (Non Fungible
+            Tokens)NFT ، هي عملات ترمز إلى ملكية لأصول أو صور أو ممتلكات معينة،
+            ويكون مالك العملة مالكاً للأصل أو الصورة أو الممتلكات المرتبطة بها.
+          </li>
+        </ol>
+      </Text>
       <Text size="small">
         وما ظهر للهيئة من أن هذه الأنواع الأربعة من العملات تُعد أصولا مالياً
         معتبرة تتغير قيمتها بتغيير العرض والطلب، وأن لكل نوع من تلك العملات
@@ -346,23 +358,25 @@ function ArFatwa() {
         أولاً: لا مانع من تأسيس ’منصةالحق شين’، وإصدار ’عملة إسلاميك كوين’
         بالضوابط الآتية:
       </Text>
-      <ol className="list-decimal pl-[20px]">
-        <li>
-          أن عملة ’إسلاميك كوين’ تعتبر أصلاً مالياً يجوز تداوله بالبيع والشراء
-          واستبدالها بالخدمات والسلع المشروعة، كما أنها تعتبر أموالاً لأصحابها
-          لا يجوز الاعتداء عليها، وتجب الزكاة بقيمتها على ماليكها بعد شرائها أو
-          بعد تعدينها وحيازتها بشروط وضوابط الزكاة.
-        </li>
-        <li>
-          تدقيق الأكواد البرمجية المستخدمة في بناء منصة ’الحق تشين’ لدى شركة
-          تدقيق برمجي معتمدة، وذلك للتأكد من سلامة المنصة وقدرتها على حماية
-          أموال المتعاملين فيها.
-        </li>
-        <li>
-          تعيين لجنة نظارة على صندوق ’إيفرجين داو’ الوقفي للتأكد من سلامة أنشطة
-          الصندوق.
-        </li>
-      </ol>
+      <Text size="small">
+        <ol className="list-decimal pl-[20px]">
+          <li>
+            أن عملة ’إسلاميك كوين’ تعتبر أصلاً مالياً يجوز تداوله بالبيع والشراء
+            واستبدالها بالخدمات والسلع المشروعة، كما أنها تعتبر أموالاً لأصحابها
+            لا يجوز الاعتداء عليها، وتجب الزكاة بقيمتها على ماليكها بعد شرائها
+            أو بعد تعدينها وحيازتها بشروط وضوابط الزكاة.
+          </li>
+          <li>
+            تدقيق الأكواد البرمجية المستخدمة في بناء منصة ’الحق تشين’ لدى شركة
+            تدقيق برمجي معتمدة، وذلك للتأكد من سلامة المنصة وقدرتها على حماية
+            أموال المتعاملين فيها.
+          </li>
+          <li>
+            تعيين لجنة نظارة على صندوق ’إيفرجين داو’ الوقفي للتأكد من سلامة
+            أنشطة الصندوق.
+          </li>
+        </ol>
+      </Text>
       <Text size="small">
         ثانياً: تؤكد الهيئة أن هذا الرأي خاص بالورقة البيضاء التي تتضمن تفاصيل
         إنشاء ’منصة الحق تشين’ وإصدار عملة ’إسلاميك كوين’ وتأسيس صندوق ’إيفرجين
