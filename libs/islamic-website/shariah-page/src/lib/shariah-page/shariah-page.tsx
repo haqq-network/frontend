7821de8c94b91b5d8bbac09d9528063728bc0d19
Oleg Shilov <me@olegshilov.com>
2023-08-10 14:52:06 +0700

    chore(islamic-website): change sharia page gb image styles

--- libs/islamic-website/shariah-page/src/lib/shariah-page/shariah-page.tsx
+++ libs/islamic-website/shariah-page/src/lib/shariah-page/shariah-page.tsx
@@ -1,4 +1,3 @@
-import { Fragment } from 'react';
 import { HeroBlock } from '../hero-block/hero-block';
 import { FatwaBlock } from '../fatwa-block/fatwa-block';
 import fatwaStarsImgData from '../../assets/images/fatwa-stars.webp';
@@ -6,14 +5,13 @@ import Image from 'next/image';
 
 export function ShariahPage() {
   return (
-    <Fragment>
-      <div className="relative">
-        <div className="absolute right-0 top-[-104px] z-[-1] h-[400px] w-[235px] md:h-[742px] md:w-[437px] lg:top-[-132px] lg:h-[1483px] lg:w-[874px]">
-          <Image src={fatwaStarsImgData} alt="" fill />
-        </div>
-      </div>
+    <div className="relative">
       <HeroBlock />
       <FatwaBlock />
-    </Fragment>
+
+      <div className="absolute right-0 top-[-104px] z-[-1] h-[400px] w-[235px] md:h-[742px] md:w-[437px] lg:top-[-132px] lg:h-[1483px] lg:w-[874px]">
+        <Image src={fatwaStarsImgData} alt="" fill />
+      </div>
+    </div>
   );
 }
