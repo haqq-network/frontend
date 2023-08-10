b846aead877a368cbe4de397263f4d7a2732b11e
Oleg Shilov <me@olegshilov.com>
2023-08-10 13:24:09 +0700

    fix(islamic-website): fix mission page background position
    - refactor some styles

--- libs/islamic-website/mission-page/src/lib/mission-page/mission-page.tsx
+++ libs/islamic-website/mission-page/src/lib/mission-page/mission-page.tsx
@@ -5,49 +5,50 @@ import Image from 'next/image';
 
 export function MissionPage() {
   return (
-    <div>
-      <Container className="mt-[32px] flex flex-col gap-y-[32px] text-white md:mt-[52px] lg:mt-[68px]">
-        <div className="text-[46px] font-[600] leading-[52px] md:text-[60px] md:leading-none lg:text-[80px]">
-          Our Mission & Vision
-        </div>
-        <div className="h-[240px] w-[234px] self-center">
-          <Image src={missionStarImgData} alt="" />
-        </div>
-        <div className="flex flex-col gap-y-[28px]">
-          <div className="flex flex-col gap-y-[16px]">
-            <Text
-              isMono
-              className="bg-gradient-to-r from-[#36FFF3] to-[#18FFAC] bg-clip-text text-transparent"
-            >
-              Vision
-            </Text>
-            <Text>
-              Our vision is to create a global, inclusive, and ethical financial
-              ecosystem that aligns with Islamic principles, empowering
-              individuals and businesses with accessible and transparent
-              financial solutions.
-            </Text>
+    <div className="relative overflow-clip pb-[150px] pt-[32px] md:pt-[52px] lg:pt-[68px]">
+      <Container>
+        <div className="flex flex-col gap-y-[32px]">
+          <div className="text-[46px] font-[600] leading-[52px] md:text-[60px] md:leading-none lg:text-[80px]">
+            Our Mission & Vision
           </div>
-          <div className="flex flex-col gap-y-[16px]">
-            <Text
-              isMono
-              className="bg-gradient-to-r from-[#36FFF3] to-[#18FFAC] bg-clip-text text-transparent"
-            >
-              Mission
-            </Text>
-            <Text>
-              Our mission is to empower the world's Muslim community with a
-              financial instrument for the Digital Age, that enables seamless
-              transactions and interaction, while supporting innovation and
-              philanthropy.
-            </Text>
+          <div className="h-[240px] w-[234px] self-center">
+            <Image src={missionStarImgData} alt="" />
+          </div>
+          <div className="flex flex-col gap-y-[28px]">
+            <div className="flex flex-col gap-y-[16px]">
+              <Text
+                isMono
+                className="bg-gradient-to-r from-[#36FFF3] to-[#18FFAC] bg-clip-text text-transparent"
+              >
+                Vision
+              </Text>
+              <Text>
+                Our vision is to create a global, inclusive, and ethical
+                financial ecosystem that aligns with Islamic principles,
+                empowering individuals and businesses with accessible and
+                transparent financial solutions.
+              </Text>
+            </div>
+            <div className="flex flex-col gap-y-[16px]">
+              <Text
+                isMono
+                className="bg-gradient-to-r from-[#36FFF3] to-[#18FFAC] bg-clip-text text-transparent"
+              >
+                Mission
+              </Text>
+              <Text>
+                Our mission is to empower the world's Muslim community with a
+                financial instrument for the Digital Age, that enables seamless
+                transactions and interaction, while supporting innovation and
+                philanthropy.
+              </Text>
+            </div>
           </div>
         </div>
       </Container>
-      <div className="relative overflow-clip pb-[250px] opacity-50">
-        <div className="absolute bottom-0 left-1/2 z-[-1] h-[262px] w-[781px] translate-x-[-50%] transform">
-          <Image src={missionRockImgData} alt="" fill />
-        </div>
+
+      <div className="absolute bottom-0 left-1/2 z-[-1] h-[262px] w-[781px] translate-x-[-50%]">
+        <Image src={missionRockImgData} alt="" fill className="opacity-50" />
       </div>
     </div>
   );
