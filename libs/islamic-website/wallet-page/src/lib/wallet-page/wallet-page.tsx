4f3341108c24a91f36ef2b8a656b673258139916
Mila Abramova <milajs@pm.me>
2023-08-11 20:13:23 +0400

    chore(islamic-website): add images and clean up wallets page

--- libs/islamic-website/wallet-page/src/lib/wallet-page/wallet-page.tsx
+++ libs/islamic-website/wallet-page/src/lib/wallet-page/wallet-page.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import {
   Container,
   Text,
@@ -5,20 +6,23 @@ import {
   WalletDownloadButton,
 } from '@haqq/islamic-ui-kit';
 import Link from 'next/link';
+import Image from 'next/image';
+import phoneImgData from '../assets/images/wallet_phone.webp';
+import bgImgData from '../assets/images/wallet_bg.webp';
 
 export function WalletPage() {
-  return (
-    <Container className="mt-[32px] flex flex-col pb-[60px] text-white md:mt-[52px] lg:mt-[68px]">
-      <Text className="text-[46px] font-[600] leading-[52px] md:text-[80px] md:leading-none lg:text-[80px] lg:leading-none">
+  const content = (
+    <Fragment>
+      <h1 className="text-[46px] font-[600] leading-[52px] md:text-[80px] md:leading-none lg:text-[80px] lg:leading-none">
         <span className="bg-gradient-to-r from-[#36FFF3] to-[#18FFAC] bg-clip-text text-transparent">
           Wallets
         </span>{' '}
         for iOS and Android
-      </Text>
+      </h1>
       <Text isMono className="mt-[24px] md:mt-[40px]">
         Ethics-First, Non-Custodial Wallet
       </Text>
-      <div className="mt-[20px] text-[13px] md:mt-[24px] md:text-[16px] md:text-[#F5F5F580]">
+      <div className="mt-[20px] max-w-[600px] text-[13px] md:mt-[24px] md:text-[16px] lg:text-[#F5F5F580]">
         Seamlessly navigate Web3 with HAQQ Wallet, your trusted partner for a
         principled DeFi journey. With mnemonicless security and a user-friendly
         interface, managing your digital Shariah-compliant assets has never been
@@ -28,7 +32,7 @@ export function WalletPage() {
         <RatingBadge market="app-store" rating={4.5} />
         <RatingBadge market="google-play" rating={5.0} />
       </div>
-      <div className="mt-[28px] flex flex-col gap-x-[16px] gap-y-[20px] lg:mt-[24px] lg:flex-row">
+      <div className="mt-[28px] flex flex-col gap-x-[16px] gap-y-[20px] md:flex-row lg:mt-[24px]">
         <div className="w-fit">
           <Link
             href="https://apps.apple.com/app/haqq-wallet-by-bored-gen/id6443843352"
@@ -46,6 +50,32 @@ export function WalletPage() {
           >
             <WalletDownloadButton type="google" />
           </Link>
+        </div>
+      </div>
+    </Fragment>
+  );
+
+  const image = (
+    <Fragment>
+      <div className="absolute left-[-140px] top-[-60px] z-[-1] h-[600px] w-[600px]">
+        <Image src={bgImgData} alt="" />
+      </div>
+      <Image src={phoneImgData} alt="" fill />
+    </Fragment>
+  );
+
+  return (
+    <Container className="relative mt-[32px] overflow-hidden pb-[60px] text-white md:mt-[52px] lg:mt-[68px] lg:overflow-visible">
+      <div className="hidden items-center justify-between lg:flex">
+        <div className="mr-[60px] flex max-w-[660px] flex-col">{content}</div>
+        <div className="flex grow-0">
+          <div className="relative mr-[44px] h-[600px] w-[350px]">{image}</div>
+        </div>
+      </div>
+      <div className="flex flex-col lg:hidden">
+        {content}
+        <div className="relative mt-[48px] h-[600px] w-[350px] self-center">
+          {image}
         </div>
       </div>
     </Container>
