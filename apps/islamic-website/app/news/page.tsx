8153c741b8f95464253c07fb7678ecf2d585f564
Oleg Shilov <me@olegshilov.com>
2023-08-14 18:13:45 +0700

    fix(islamic-website): fix news page container, move mock news to app

--- apps/islamic-website/app/news/page.tsx
+++ apps/islamic-website/app/news/page.tsx
@@ -1,1 +1,123 @@
-export { NewsPage as default } from '@haqq/islamic-website/news-page';
+import { NewsPage as NewsPageComponent } from '@haqq/islamic-website/news-page';
+import { NewsPost } from '@haqq/islamic-ui-kit';
+
+const mockNews: NewsPost[] = [
+  {
+    image: {
+      height: 300,
+      width: 200,
+      src: 'https://picsum.photos/id/1/200/300',
+    },
+    title:
+      'News Title News TitleNews TitleNews TitleNews TitleNews TitleNews TitleNews Title',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    date: new Date(),
+    source: 'mockwebsite.com',
+    type: 'events',
+  },
+  {
+    image: {
+      height: 300,
+      width: 200,
+      src: 'https://picsum.photos/id/2/200/300',
+    },
+    title:
+      'News Title News TitleNews TitleNews TitleNews TitleNews TitleNews TitleNews Title',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    date: new Date(),
+    source: 'mockwebsite.com',
+    type: 'press',
+  },
+  {
+    date: new Date(),
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title:
+      'News Title News TitleNews TitleNews TitleNews TitleNews TitleNews TitleNews Title',
+    image: {
+      height: 300,
+      width: 200,
+      src: 'https://picsum.photos/id/3/200/300',
+    },
+    source: 'mockwebsite.com',
+    type: 'press',
+  },
+  {
+    date: new Date(),
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title:
+      'News Title News TitleNews TitleNews TitleNews TitleNews TitleNews TitleNews Title',
+    image: {
+      height: 300,
+      width: 200,
+      src: 'https://picsum.photos/id/4/200/300',
+    },
+    source: 'mockwebsite.com',
+    type: 'press',
+  },
+  {
+    date: new Date(),
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title:
+      'News Title News TitleNews TitleNews TitleNews TitleNews TitleNews TitleNews Title',
+    image: {
+      height: 300,
+      width: 200,
+      src: 'https://picsum.photos/id/5/200/300',
+    },
+    source: 'mockwebsite.com',
+    type: 'press',
+  },
+  {
+    date: new Date(),
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title:
+      'News Title News TitleNews TitleNews TitleNews TitleNews TitleNews TitleNews Title',
+    image: {
+      height: 300,
+      width: 200,
+      src: 'https://picsum.photos/id/6/200/300',
+    },
+    source: 'mockwebsite.com',
+    type: 'press',
+  },
+  {
+    date: new Date(),
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title:
+      'News Title News TitleNews TitleNews TitleNews TitleNews TitleNews TitleNews Title',
+    image: {
+      height: 300,
+      width: 200,
+      src: 'https://picsum.photos/id/7/200/300',
+    },
+    source: 'mockwebsite.com',
+    type: 'press',
+    isFeatured: true,
+  },
+  {
+    date: new Date(),
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title:
+      'News Title News TitleNews TitleNews TitleNews TitleNews TitleNews TitleNews Title',
+    image: {
+      height: 300,
+      width: 200,
+      src: 'https://picsum.photos/id/8/200/300',
+    },
+    source: 'mockwebsite.com',
+    type: 'press',
+    isFeatured: true,
+  },
+];
+
+export default function NewsPage() {
+  return <NewsPageComponent news={mockNews} />;
+}
--- libs/islamic-website/news-page/src/lib/news-page/news-page.tsx
+++ libs/islamic-website/news-page/src/lib/news-page/news-page.tsx
@@ -1,132 +1,14 @@
-import { Container, NewsPost, Text } from '@haqq/islamic-ui-kit';
-import { FeaturedPostBlock } from '../featured-post-block/featured-post-block';
+import { Container, NewsCard, NewsPost, Text } from '@haqq/islamic-ui-kit';
 import { useMemo } from 'react';
 import { PostsBlock } from '../posts-block/posts-block';
 import { SubscribeForm } from '@haqq/islamic-website/forms';
 
-const mockNews: NewsPost[] = [
-  {
-    image: {
-      height: 300,
-      width: 200,
-      src: 'https://picsum.photos/200/300',
-    },
-    title:
-      'News Title News TitleNews TitleNews TitleNews TitleNews TitleNews TitleNews Title',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    date: new Date(),
-    source: 'mockwebsite.com',
-    type: 'events',
-  },
-  {
-    image: {
-      height: 300,
-      width: 200,
-      src: 'https://picsum.photos/200/300',
-    },
-    title:
-      'News Title News TitleNews TitleNews TitleNews TitleNews TitleNews TitleNews Title',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    date: new Date(),
-    source: 'mockwebsite.com',
-    type: 'press',
-  },
-  {
-    date: new Date(),
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    title:
-      'News Title News TitleNews TitleNews TitleNews TitleNews TitleNews TitleNews Title',
-    image: {
-      height: 300,
-      width: 200,
-      src: 'https://picsum.photos/200/300',
-    },
-    source: 'mockwebsite.com',
-    type: 'press',
-  },
-  {
-    date: new Date(),
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    title:
-      'News Title News TitleNews TitleNews TitleNews TitleNews TitleNews TitleNews Title',
-    image: {
-      height: 300,
-      width: 200,
-      src: 'https://picsum.photos/200/300',
-    },
-    source: 'mockwebsite.com',
-    type: 'press',
-  },
-  {
-    date: new Date(),
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    title:
-      'News Title News TitleNews TitleNews TitleNews TitleNews TitleNews TitleNews Title',
-    image: {
-      height: 300,
-      width: 200,
-      src: 'https://picsum.photos/200/300',
-    },
-    source: 'mockwebsite.com',
-    type: 'press',
-  },
-  {
-    date: new Date(),
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    title:
-      'News Title News TitleNews TitleNews TitleNews TitleNews TitleNews TitleNews Title',
-    image: {
-      height: 300,
-      width: 200,
-      src: 'https://picsum.photos/200/300',
-    },
-    source: 'mockwebsite.com',
-    type: 'press',
-  },
-  {
-    date: new Date(),
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    title:
-      'News Title News TitleNews TitleNews TitleNews TitleNews TitleNews TitleNews Title',
-    image: {
-      height: 300,
-      width: 200,
-      src: 'https://picsum.photos/200/300',
-    },
-    source: 'mockwebsite.com',
-    type: 'press',
-    isFeatured: true,
-  },
-  {
-    date: new Date(),
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    title:
-      'News Title News TitleNews TitleNews TitleNews TitleNews TitleNews TitleNews Title',
-    image: {
-      height: 300,
-      width: 200,
-      src: 'https://picsum.photos/200/300',
-    },
-    source: 'mockwebsite.com',
-    type: 'press',
-    isFeatured: true,
-  },
-];
-
-export function NewsPage() {
+export function NewsPage({ news }: { news: NewsPost[] }) {
   const { featuredPost, postsToRender } = useMemo(() => {
     let featuredPost = undefined;
     const postsToRender = [];
 
-    for (const post of mockNews) {
+    for (const post of news) {
       if (!featuredPost) {
         if (post.isFeatured) {
           featuredPost = post;
@@ -139,32 +21,42 @@ export function NewsPage() {
     }
 
     return { featuredPost, postsToRender };
-  }, []);
+  }, [news]);
 
   return (
-    <Container className="mt-[32px] flex flex-col pb-[60px] text-white md:mt-[52px] lg:mt-[68px]">
-      <div className="text-[46px] font-[600] leading-[52px] md:text-[60px] md:leading-none lg:text-[80px]">
-        Islamic Coin Press & News
-      </div>
-      <div className="mt-[32px] flex flex-col gap-[24px] lg:flex-row xl:w-3/4">
-        <Text size="small" className="lg:w-1/2">
-          Keep up to date with our upcoming Public Launch and Exchange listing -
-          Subscribe to our newsletter
-        </Text>
-        <SubscribeForm
-          className="flex flex-col gap-[16px] md:flex-row"
-          inputClassName="md:min-w-[240px]"
-        />
-      </div>
-      {featuredPost && (
-        <FeaturedPostBlock
-          post={featuredPost}
-          className="mt-[60px] lg:mt-[140px]"
-        />
-      )}
-      {postsToRender.length > 0 && (
-        <PostsBlock posts={postsToRender} className="mt-[60px] lg:mt-[140px]" />
-      )}
-    </Container>
+    <div className="flex flex-col pb-[60px] pt-[32px] text-white md:pt-[52px] lg:pt-[68px]">
+      <Container>
+        <h1 className="text-[46px] font-[600] leading-[52px] md:text-[60px] md:leading-none lg:text-[80px]">
+          Islamic Coin Press & News
+        </h1>
+
+        <div className="mt-[32px] flex flex-col gap-[24px] lg:flex-row xl:w-3/4">
+          <Text size="small" className="lg:w-1/2">
+            Keep up to date with our upcoming Public Launch and Exchange listing
+            - Subscribe to our newsletter
+          </Text>
+          <SubscribeForm
+            className="flex flex-col gap-[16px] md:flex-row"
+            inputClassName="md:min-w-[240px]"
+          />
+        </div>
+
+        {featuredPost && (
+          <div className="flex flex-col gap-y-[28px] pt-[60px] md:gap-y-[48px] lg:pt-[140px]">
+            <h2 className="text-[22px] font-[600] leading-[24px] md:font-[48px] md:leading-[54px]">
+              Featured Post
+            </h2>
+            <NewsCard post={featuredPost} isFeatured />
+          </div>
+        )}
+
+        {postsToRender.length > 0 && (
+          <PostsBlock
+            posts={postsToRender}
+            className="mt-[60px] lg:mt-[140px]"
+          />
+        )}
+      </Container>
+    </div>
   );
 }
