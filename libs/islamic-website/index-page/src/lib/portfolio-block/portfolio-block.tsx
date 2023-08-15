b39b1763fc9c4623c68c296e1135f65d4f9c1a17
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-08-16 02:19:01 +0300

    fix(islamic-website): portfolio block text margin

--- libs/islamic-website/index-page/src/lib/portfolio-block/portfolio-block.tsx
+++ libs/islamic-website/index-page/src/lib/portfolio-block/portfolio-block.tsx
@@ -32,7 +32,9 @@ export function PortfolioBlock() {
                 Manage Your <GradientText>Shariah Portfolio </GradientText>
                 with HAQQ Wallet
               </div>
-              <Text isMono>Ethics-First, Non-Custodial Wallet</Text>
+              <Text isMono className="lg:mt-[40px]">
+                Ethics-First, Non-Custodial Wallet
+              </Text>
               <Text
                 size="small"
                 className="mt-[20px] text-white/50 md:mt-[24px]"
