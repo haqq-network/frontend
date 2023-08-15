68bc4fb013003132233c2dcc08b6bba177fba4e6
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-08-16 02:23:08 +0300

    fix(islamic-website): hero block heading size

--- libs/islamic-website/index-page/src/lib/hero-block/hero-block.tsx
+++ libs/islamic-website/index-page/src/lib/hero-block/hero-block.tsx
@@ -4,13 +4,13 @@ export function HeroBlock() {
   return (
     <Container>
       <div className="flex flex-col pt-[60px] text-white md:pt-[120px] xl:pt-[150px]">
-        <div className="text-[28px] font-[600] leading-[32px] md:text-[44px] md:leading-[48px] lg:text-[64px] lg:leading-[70px]">
+        <h1 className="text-[46px] font-[600] leading-[52px] md:text-[60px] md:leading-none lg:text-[80px]">
           Your Gateway <br className="block md:hidden" /> to{' '}
           <br className="hidden md:block" /> Shariah{' '}
           <GradientText>
             Compliant <br className="block xl:hidden" /> Web3
           </GradientText>
-        </div>
+        </h1>
         <div className="mt-[24px] md:max-w-xl">
           <Text size="small" className="text-white/50 md:mt-[40px]">
             We are on a mission to onboard over 1.8+ billion Muslims into
