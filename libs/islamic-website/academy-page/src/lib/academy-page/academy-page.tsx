0e47580a35ed52c726c1a5c94475a525e4430ce4
Oleg Shilov <me@olegshilov.com>
2023-08-16 21:05:39 +0700

    fix(islamic-website): set correct text gap in academy page

--- libs/islamic-website/academy-page/src/lib/academy-page/academy-page.tsx
+++ libs/islamic-website/academy-page/src/lib/academy-page/academy-page.tsx
@@ -95,7 +95,7 @@ export function AcademyPage() {
     <section className="overflow-clip">
       <Container>
         <div className="relative pb-[60px] pt-[32px] md:pt-[52px] lg:pt-[68px]">
-          <div className="flex flex-col gap-[32px]">
+          <div className="flex flex-col gap-[32px] md:gap-[60px]">
             <div className="md:max-w-[880px]">
               <div className="text-[46px] font-[600] leading-[52px] md:text-[60px] md:leading-none lg:text-[80px]">
                 Embark on a journey of learning through our&nbsp;
