63b6813867fb722043cc41c5e4690ca5d527dcb2
Oleg Shilov <me@olegshilov.com>
2023-08-16 18:32:01 +0700

    fix(islamic-ui-kit): set select component height

--- libs/islamic-website/ui-kit/src/lib/select/select.tsx
+++ libs/islamic-website/ui-kit/src/lib/select/select.tsx
@@ -46,8 +46,8 @@ export function Select({
               className="mr-[-4px]"
             >
               <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M13.172 3.52063C13.4981 3.80018 13.5359 4.29119 13.2563 4.61733L6.58967 12.3951C6.44191 12.5675 6.22619 12.6667 5.99914 12.6667C5.77209 12.6667 5.55637 12.5675 5.40861 12.3951L2.74195 9.28399C2.4624 8.95785 2.50017 8.46684 2.82631 8.18729C3.15245 7.90774 3.64346 7.94551 3.92301 8.27165L5.99914 10.6938L12.0753 3.60499C12.3548 3.27885 12.8458 3.24108 13.172 3.52063Z"
                 fill="#F5F5F5"
               />
@@ -68,7 +68,7 @@ export function Select({
           return (
             <button
               className={clsx(
-                'flex flex-row items-center justify-between gap-[6px] rounded-[8px] px-[16px] py-[12px]',
+                'flex h-[48px] flex-row items-center justify-between gap-[6px] rounded-[8px] px-[16px] py-[12px]',
                 'font-mono text-base',
                 'transition-colors duration-300 ease-out',
                 'box-border appearance-none outline-none',
@@ -119,7 +119,7 @@ export function Select({
         <Menu.Items
           className={clsx(
             'absolute left-[0px] z-10 mt-[4px] w-fit min-w-full origin-top rounded-[8px] bg-[#2f2f2f] py-[8px] shadow-lg focus:outline-none',
-            'max-h-[240px] overflow-y-scroll',
+            'max-h-[256px] overflow-y-scroll',
           )}
         >
           {optionsToRender}
