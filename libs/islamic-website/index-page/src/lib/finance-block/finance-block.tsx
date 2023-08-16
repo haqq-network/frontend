ebe5b80a982c6f13277ffa9c93cfb459f26b1275
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-08-16 14:55:39 +0300

    fix(islamic-website): finance block

--- libs/islamic-website/index-page/src/lib/finance-block/finance-block.tsx
+++ libs/islamic-website/index-page/src/lib/finance-block/finance-block.tsx
@@ -1,10 +1,8 @@
-import { Container, GradientText, Text } from '@haqq/islamic-ui-kit';
-import Image from 'next/image';
-import cubeOnRockImgData from '../../assets/images/cube-on-rock.webp';
+import { GradientText, Text } from '@haqq/islamic-ui-kit';
 
 export function FinanceBlock() {
   return (
-    <Container className="mt-[80px] md:mt-0">
+    <div className="mt-[80px] md:mt-0">
       <div className="relative flex text-white">
         <div className="flex flex-col md:w-4/5 lg:w-3/5 lg:py-[12px] xl:py-[60px]">
           <div className="text-[28px] font-[600] leading-[32px] md:text-[44px] md:leading-[48px] lg:text-[64px] lg:leading-[70px]">
@@ -31,10 +29,7 @@ export function FinanceBlock() {
             </Text>
           </div>
         </div>
-        {/* <div className="absolute right-[22%] top-[-315px] z-[-1] w-[656px] translate-x-[24%] md:right-0 md:top-[-12%] md:translate-x-[250px] lg:right-0 lg:top-0 lg:h-[550px] lg:translate-x-0">
-          <Image src={cubeOnRockImgData} alt="" />
-        </div> */}
       </div>
-    </Container>
+    </div>
   );
 }
