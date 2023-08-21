b346142eda34dfaf220566705eda294cdf3a2d52
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-08-21 15:28:10 +0300

    fix(islamic-website): add opengraph & twitter image to values page

--- apps/islamic-website/app/values/page.tsx
+++ apps/islamic-website/app/values/page.tsx
@@ -1,1 +1,17 @@
 export { ValuesPage as default } from '@haqq/islamic-website/values-page';
+
+const VERCEL_URL = process.env['NEXT_PUBLIC_VERCEL_URL'];
+
+export const metadata = {
+  title: 'IslamicCoin | Our Values',
+  description:
+    'Our values are the foundation of our company and our products. They guide our decisions, actions and the way we work with each other and our clients.',
+  openGraph: {
+    images: [{ url: '/opengraph-image.png' }],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    images: [{ url: '/opengraph-image.png' }],
+  },
+  metadataBase: new URL(VERCEL_URL ?? ''),
+};
