bb0c45ae6fd81c1f04693ab8b165ba2aac82857a
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-08-22 15:13:50 +0300

    fix(islamic-ui-kit): add isOutLink prop to dropdown link

--- libs/islamic-website/ui-kit/src/lib/dropdown-link/dropdown-link.tsx
+++ libs/islamic-website/ui-kit/src/lib/dropdown-link/dropdown-link.tsx
@@ -5,14 +5,18 @@ export function DropdownLink({
   icon,
   title,
   href,
+  isOutLink = false,
 }: {
   icon?: ReactNode;
   title: string;
   href: string;
+  isOutLink?: boolean;
 }) {
   return (
     <Link
       href={href}
+      target={isOutLink ? '_blank' : undefined}
+      rel={isOutLink ? 'noopener noreferrer' : undefined}
       className="hover:text-islamic-primary-green w-fit cursor-pointer px-[16px] py-[12px] text-base font-[500] text-white transition-colors duration-200"
     >
       <div className="flex items-center gap-x-[10px]">
