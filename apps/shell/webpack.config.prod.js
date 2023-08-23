ec2369c0bdd9d37e61d2096e00c095fa993605df
Oleg Shilov <me@olegshilov.com>
2023-08-23 14:20:13 +0700

    chore(shell): use preview deploys endpoints module federation

--- apps/shell/webpack.config.prod.js
+++ apps/shell/webpack.config.prod.js
@@ -5,16 +5,20 @@ const { ProvidePlugin } = require('webpack');
 const { merge } = require('webpack-merge');
 const baseConfig = require('./module-federation.config');
 
-const moduleFederationRemote =
-  process.env['VERCEL_ENV'] === 'production'
-    ? [
-        ['staking', '//staking.haqq.network'],
-        ['governance', '//governance.haqq.network'],
-      ]
-    : [
-        ['staking', '//staking.haqq.sh'],
-        ['governance', '//governance.haqq.sh'],
-      ];
+const moduleFederationRemote = [
+  ['staking', '//staking-app-git-feat-authz-haqq-network.vercel.app'],
+  ['governance', '//governance-app-git-feat-authz-haqq-network.vercel.app'],
+];
+// const moduleFederationRemote =
+//   process.env['VERCEL_ENV'] === 'production'
+//     ? [
+//         ['staking', '//staking.haqq.network'],
+//         ['governance', '//governance.haqq.network'],
+//       ]
+//     : [
+//         ['staking', '//staking.haqq.sh'],
+//         ['governance', '//governance.haqq.sh'],
+//       ];
 
 /**
  * @type {import('@nrwl/devkit').ModuleFederationConfig}
