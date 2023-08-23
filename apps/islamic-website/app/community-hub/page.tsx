5b62ec0b4e7fbd7533b8c210bd68716ff1d321cd
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-08-23 16:38:20 +0300

    feat(islamic-website): community hub page markup

---
+++ apps/islamic-website/app/community-hub/page.tsx
@@ -0,0 +1,1 @@
+export { CommunityHubPage as default } from '@haqq/islamic-website/community-hub-page';
---
+++ libs/islamic-website/community-hub-page/src/lib/community-hub-page/community-hub-page.tsx
@@ -0,0 +1,7 @@
+export function CommunityHubPage() {
+  return (
+    <div className={''}>
+      <h1>Welcome to IslamicWebsiteCommunityHubPage!</h1>
+    </div>
+  );
+}
