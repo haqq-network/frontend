dc9c3e380a6bc1d50c9e54ada8dfb447549282f1
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-08-23 18:56:58 +0300

    fix(islamic-website): add meta tag for community hub page

--- apps/islamic-website/app/community-hub/page.tsx
+++ apps/islamic-website/app/community-hub/page.tsx
@@ -1,1 +1,7 @@
+import { Metadata } from 'next';
+
 export { CommunityHubPage as default } from '@haqq/islamic-website/community-hub-page';
+
+export const metadata: Metadata = {
+  title: 'IslamicCoin | Community hub',
+};
