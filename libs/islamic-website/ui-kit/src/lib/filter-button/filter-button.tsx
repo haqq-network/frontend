dd060798b8ce2e71720f1314d96e50511ef47b3a
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-08-28 19:31:35 +0300

    feat(islamic-ui-kit): add filter button & new member card components

---
+++ libs/islamic-website/ui-kit/src/lib/filter-button/filter-button.tsx
@@ -0,0 +1,25 @@
+import clsx from 'clsx';
+import { PropsWithChildren } from 'react';
+
+export function FilterButton({
+  children,
+  active,
+  onClick,
+}: PropsWithChildren<{
+  active: boolean;
+  onClick: () => void;
+}>) {
+  return (
+    <div
+      className={clsx(
+        ' cursor-pointer rounded-[8px] px-[12px] py-[8px] font-mono text-[14px] font-[400] uppercase leading-[20px] text-white transition-colors duration-300',
+        active
+          ? 'bg-islamic-primary-green'
+          : 'hover:bg-islamic-primary-green/50 bg-transparent',
+      )}
+      onClick={onClick}
+    >
+      {children}
+    </div>
+  );
+}
