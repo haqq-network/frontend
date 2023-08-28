df94c4ed6b0a7bd0da204a3ecede39a85065ba6a
Oleg Shilov <me@olegshilov.com>
2023-08-29 01:01:44 +0700

    chore: fix exports

--- apps/islamic-website/constants.ts
+++ apps/islamic-website/constants.ts
@@ -1,6 +1,5 @@
-const DEPLOY_URL = `https://${process.env['NEXT_PUBLIC_VERCEL_URL']}`;
-const STORYBLOK_ACCESS_TOKEN = process.env['STORYBLOK_ACCESS_TOKEN'];
-const VERCEL_ENV = process.env['VERCEL_ENV'];
-const REVALIDATE_TIME = 3600;
-
-export { DEPLOY_URL, STORYBLOK_ACCESS_TOKEN, VERCEL_ENV, REVALIDATE_TIME };
+export const DEPLOY_URL = `https://${process.env['NEXT_PUBLIC_VERCEL_URL']}`;
+export const STORYBLOK_ACCESS_TOKEN = process.env['STORYBLOK_ACCESS_TOKEN'];
+export const VERCEL_ENV = process.env['VERCEL_ENV'];
+export const REVALIDATE_TIME = 3600;
+export const SERVICE_ENDPOINT = process.env['SERVICE_ENDPOINT'];
