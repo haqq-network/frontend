887b40fd45b5455fd07b8bd4e5d044d92f9ed47e
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-08-31 13:02:54 +0300

    fix(islamic-website): community hub & fraud pages meta description

--- apps/islamic-website/app/community-hub/page.tsx
+++ apps/islamic-website/app/community-hub/page.tsx
@@ -2,13 +2,15 @@ import { DEPLOY_URL } from '../../constants';
 import type { Metadata } from 'next';
 
 const title = 'IslamicCoin | Community hub';
+const description =
+  'Join a vibrant community committed to ethical finance. Engage in conversations, share knowledge, and foster connections within the Islamic Coin network.';
 
 export const metadata: Metadata = {
   title,
-
+  description,
   openGraph: {
     title,
-
+    description,
     images: [{ url: '/opengraph-image.png' }],
     locale: 'en-US',
     url: `${new URL(DEPLOY_URL)}build`,
