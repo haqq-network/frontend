f9a44150afd73c2bfeb00ab5a65d2b29b8ab15aa
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-09-13 02:02:29 +0300

    chore(islamic-website): try new matcher in middleware

--- apps/islamic-website/middleware.ts
+++ apps/islamic-website/middleware.ts
@@ -11,5 +11,5 @@ export default createMiddleware({
 export const config = {
   // Skip all paths that should not be internationalized. This example skips the
   // folders "api", "_next" and all files with an extension (e.g. favicon.ico)
-  matcher: ['/((?!api|_next|.*\\..*).*)'],
+  matcher: ['/((?!_next).*)'],
 };
