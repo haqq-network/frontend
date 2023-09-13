44a0c14672beab8165c92866ac1f4d1b2aae231a
Oleg Shilov <me@olegshilov.com>
2023-09-13 18:26:01 +0700

    feat(shell-app): improve grant interface
    - add select for type
    - add select for period
    - put back `valid thru` column to grant table
    
    closes: HQW-129, HQW-128

---
+++ libs/shell/authz-page/src/lib/select/select.tsx
@@ -0,0 +1,129 @@
+import ReactSelect, {
+  components as selectComponents,
+  OptionProps,
+  ClassNamesConfig,
+  OptionsOrGroups,
+  GroupBase,
+} from 'react-select';
+import clsx from 'clsx';
+import { useCallback, useMemo } from 'react';
+
+interface SelectOption {
+  label: string | undefined;
+  value: string;
+}
+
+export function SelectOption({
+  children: _,
+  ...rest
+}: OptionProps<SelectOption, false>) {
+  return (
+    <selectComponents.Option {...rest}>
+      <div>{rest.data.label}</div>
+    </selectComponents.Option>
+  );
+}
+
+export function Select({
+  options,
+  placeholder,
+  className,
+  selectContainerClassName,
+  label,
+  id,
+  defaultValue,
+  onChange,
+}: {
+  options: OptionsOrGroups<SelectOption, GroupBase<SelectOption>>;
+  defaultValue?: SelectOption;
+  placeholder?: string;
+  selectContainerClassName?: string;
+  className?: string;
+  label?: string;
+  id?: string;
+  onChange: (option?: string) => void;
+}) {
+  const formatGroupLabel = useCallback((data: GroupBase<SelectOption>) => {
+    return (
+      <div className="px-[16px] py-[6px] text-left text-[10px] font-[500] uppercase leading-[18px] text-white/50">
+        {data.label}
+      </div>
+    );
+  }, []);
+
+  const classNames = useMemo<ClassNamesConfig<SelectOption>>(() => {
+    return {
+      control: () => {
+        return clsx(
+          'inline-block w-full rounded-[6px] border border-[#252528] bg-transparent outline-none',
+          'transition-colors duration-150 ease-in will-change-[color,background]',
+          'focus:border-white/50 focus:bg-transparent focus:text-white',
+          'hover:border-white/20',
+        );
+      },
+      container: () => {
+        return clsx(selectContainerClassName);
+      },
+      placeholder: () => {
+        return 'text-white/25';
+      },
+      valueContainer: () => {
+        return 'text-white text-[14px] font-[500] leading-[22px] min-h-[46px] px-[16px]';
+      },
+      indicatorsContainer: () => {
+        return 'pr-[10px] text-white/25';
+      },
+      menu: () => {
+        return clsx(
+          'border-[1px] border-[#ffffff26] bg-haqq-black rounded-[6px] mt-[4px] shadow-lg',
+          'text-white text-[13px] leading-[20px]',
+          'overflow-hiddenz                                                                                                                                                                                                                                                                                                                                                                                         ',
+        );
+      },
+      option: ({ isFocused, isSelected }) => {
+        return clsx(
+          'px-[16px] py-[10px] text-left',
+          'transition-colors duration-150 ease-out',
+          {
+            'bg-[#ffffff14]': isFocused || isSelected,
+          },
+        );
+      },
+      noOptionsMessage: () => {
+        return 'px-[16px] py-[10px]';
+      },
+    };
+  }, [selectContainerClassName]);
+
+  return (
+    <div className={clsx('flex flex-col gap-[8px]', className)}>
+      {label && (
+        <div>
+          <label
+            htmlFor={id}
+            className="cursor-pointer text-[12px] font-[500] uppercase leading-[24px] text-white/50"
+          >
+            {label}
+          </label>
+        </div>
+      )}
+      <ReactSelect
+        id={id}
+        placeholder={placeholder}
+        options={options}
+        onChange={(validator) => {
+          onChange(validator?.value);
+        }}
+        components={{
+          Option: SelectOption,
+        }}
+        unstyled
+        classNames={classNames}
+        isSearchable={false}
+        formatGroupLabel={formatGroupLabel}
+        menuPlacement="auto"
+        defaultValue={defaultValue}
+      />
+    </div>
+  );
+}
