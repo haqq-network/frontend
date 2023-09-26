ed9bcf121ea59177bf9648d2e616fe8e14b4b52c
Oleg Shilov <me@olegshilov.com>
2023-09-26 22:06:11 +0700

    fix(islamic-website): update markup for background image on contact us page

--- libs/islamic-website/alerts-pages/src/lib/contact-us-page/contact-us-page.tsx
+++ libs/islamic-website/alerts-pages/src/lib/contact-us-page/contact-us-page.tsx
@@ -1,54 +1,71 @@
 import { Container } from '@haqq/islamic-website-ui-kit';
 import Image from 'next/image';
 import Link from 'next/link';
-import mobileStarsImgData from '../../assets/images/contact-stars-mobile.webp';
-import desktopStarsImgData from '../../assets/images/contact-stars-desktop.webp';
+import contactPageImageData from '../../assets/images/contact-page-image.webp';
 
 const phone = '+9715855474242';
 export function ContactUsPage() {
   return (
     <section className="">
-      <Container className="relative">
-        <div className="flex flex-col items-start pb-[36px] pt-[32px] md:pb-[46px] md:pt-[60px] lg:py-[244px] xl:pb-[324px] xl:pt-[284px]">
-          <h1 className="text-[46px] font-[600] leading-[52px] md:text-[60px] md:leading-none lg:text-[80px]">
-            Contact us
-          </h1>
-          <div className="flex flex-col lg:flex-row">
-            <div className="mt-[24px] flex max-w-[500px] flex-col gap-[16px] md:mt-[28px] md:gap-[20px] lg:mt-[36px] lg:gap-[24px] xl:max-w-[850px] xl:flex-row xl:gap-[32px]">
-              <p>
-                For questions related to anti-scam, please write to us at{' '}
-                <Link
-                  href="mailto:scamalert@islmamicoin.net"
-                  className="text-islamic-primary-green hover:text-islamic-primary-green-hover cursor-pointer transition-colors duration-300"
-                >
-                  scamalert@islmamicoin.net
-                </Link>
-              </p>
-              <p>
-                For other legal and privacy questions, please write to us at{' '}
-                <Link
-                  href="mailto:complience@islmamicoin.net"
-                  className="text-islamic-primary-green hover:text-islamic-primary-green-hover cursor-pointer transition-colors duration-300"
-                >
-                  complience@islmamicoin.net
-                </Link>
-              </p>
-              <p>
-                In case of an emergency, please contact us by phone{' '}
-                <Link
-                  href={`tel:${phone}`}
-                  className="text-islamic-primary-green hover:text-islamic-primary-green-hover cursor-pointer transition-colors duration-300"
-                >
-                  {phone}
-                </Link>
-              </p>
+      <Container>
+        <div className="pb-[36px] pt-[32px] md:pb-[46px] md:pt-[60px] lg:py-[244px] xl:pb-[324px] xl:pt-[284px]">
+          <div className="flex min-h-[200px] flex-col items-start lg:flex-row">
+            <div className="lg:w-2/3">
+              <div className="flex flex-col gap-[24px] md:gap-[28px] lg:gap-[36px] xl:gap-[32px]">
+                <h1 className="text-[46px] font-[600] leading-[52px] md:text-[60px] md:leading-none lg:text-[80px]">
+                  Contact us
+                </h1>
+                <div className="flex flex-col gap-[16px] md:gap-[20px] lg:gap-[24px] xl:flex-row xl:gap-[32px]">
+                  <div>
+                    <p>
+                      For questions related to anti-scam, please write to us at{' '}
+                      <Link
+                        href="mailto:scamalert@islmamicoin.net"
+                        className="text-islamic-primary-green hover:text-islamic-primary-green-hover cursor-pointer transition-colors duration-300"
+                      >
+                        scamalert@islmamicoin.net
+                      </Link>
+                    </p>
+                  </div>
+                  <div>
+                    <p>
+                      For other legal and privacy questions, please write to us
+                      at{' '}
+                      <Link
+                        href="mailto:complience@islmamicoin.net"
+                        className="text-islamic-primary-green hover:text-islamic-primary-green-hover cursor-pointer transition-colors duration-300"
+                      >
+                        complience@islmamicoin.net
+                      </Link>
+                    </p>
+                  </div>
+                  <div>
+                    <p>
+                      In case of an emergency, please contact us by phone{' '}
+                      <Link
+                        href={`tel:${phone}`}
+                        className="text-islamic-primary-green hover:text-islamic-primary-green-hover cursor-pointer transition-colors duration-300"
+                      >
+                        {phone}
+                      </Link>
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="absolute bottom-[-220px] z-[-1] self-center min-[385px]:bottom-[-300px] lg:hidden">
-            <Image src={mobileStarsImgData} alt="" />
-          </div>
-          <div className="right-0 top-[10%] hidden lg:absolute lg:block">
-            <Image src={desktopStarsImgData} alt="" width={475} height={600} />
+
+            <div className="relative mb-[-30%] w-full flex-1 lg:mb-[0px] lg:min-h-[200px]">
+              <div className="pointer-events-none z-[-1] select-none lg:absolute lg:right-[0px] lg:top-[50%] lg:origin-right lg:translate-x-[80%] lg:translate-y-[-50%] lg:scale-150">
+                <div className="from-islamic-bg-black absolute bottom-[0px] h-[70%] w-full bg-gradient-to-t from-55% to-transparent lg:right-0 lg:h-full lg:w-[90%] lg:bg-gradient-to-l" />
+                <Image
+                  alt=""
+                  src={contactPageImageData}
+                  width={1430 / 2.4}
+                  height={1000 / 2.4}
+                  className="w-full lg:w-auto lg:max-w-none"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </Container>
