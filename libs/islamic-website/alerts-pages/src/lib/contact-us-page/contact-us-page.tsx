84176a7017a2681d586041be257f0815845c2f1f
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-09-26 15:59:17 +0300

    fix(islamic-website): contact us page markup

--- libs/islamic-website/alerts-pages/src/lib/contact-us-page/contact-us-page.tsx
+++ libs/islamic-website/alerts-pages/src/lib/contact-us-page/contact-us-page.tsx
@@ -1,60 +1,54 @@
 import { Container } from '@haqq/islamic-website-ui-kit';
-import fraudCubesImgData from '../../assets/images/fraud-cubes.webp';
 import Image from 'next/image';
 import Link from 'next/link';
+import mobileStarsImgData from '../../assets/images/contact-stars-mobile.webp';
+import desktopStarsImgData from '../../assets/images/contact-stars-desktop.webp';
 
+const phone = '+9715855474242';
 export function ContactUsPage() {
   return (
-    <section className="gap-y-[24px] overflow-x-clip pb-[60px] pt-[32px] md:pb-[90px] lg:gap-y-[40px] lg:pb-[180px] lg:pt-[80px]">
+    <section className="">
       <Container className="relative">
-        <div className="flex flex-col gap-y-[24px] md:gap-y-[32px] lg:gap-y-[40px]">
+        <div className="flex flex-col items-start pb-[36px] pt-[32px] md:pb-[46px] md:pt-[60px] lg:py-[244px] xl:pb-[324px] xl:pt-[284px]">
           <h1 className="text-[46px] font-[600] leading-[52px] md:text-[60px] md:leading-none lg:text-[80px]">
             Contact us
           </h1>
-        </div>
-        <div className="mt-[32px] flex flex-col items-center md:mt-[44px] md:flex-row md:items-start lg:mt-[60px]">
-          <div className="md:w-2/3">
-            <div className="flex w-full flex-col gap-12 text-xl">
+          <div className="flex flex-col lg:flex-row">
+            <div className="mt-[24px] flex max-w-[500px] flex-col gap-[16px] md:mt-[28px] md:gap-[20px] lg:mt-[36px] lg:gap-[24px] xl:max-w-[850px] xl:flex-row xl:gap-[32px]">
               <p>
-                For questions related to anti-scam, please write to us at:{' '}
-                <br />
+                For questions related to anti-scam, please write to us at{' '}
                 <Link
                   href="mailto:scamalert@islmamicoin.net"
-                  className="text-islamic-primary-green hover:text-islamic-primary-green-hover text-2xl font-semibold underline transition-colors duration-150"
+                  className="text-islamic-primary-green hover:text-islamic-primary-green-hover cursor-pointer transition-colors duration-300"
                 >
                   scamalert@islmamicoin.net
                 </Link>
               </p>
               <p>
                 For other legal and privacy questions, please write to us at{' '}
-                <br />
                 <Link
-                  href="mailto:compliance@islamiccoin.net"
-                  className="text-islamic-primary-green hover:text-islamic-primary-green-hover text-2xl font-semibold underline transition-colors duration-150"
+                  href="mailto:complience@islmamicoin.net"
+                  className="text-islamic-primary-green hover:text-islamic-primary-green-hover cursor-pointer transition-colors duration-300"
                 >
-                  compliance@islamiccoin.net
+                  complience@islmamicoin.net
                 </Link>
               </p>
               <p>
-                In case of an emergency, please contact us by phone: <br />
+                In case of an emergency, please contact us by phone{' '}
                 <Link
-                  href="tel:+9715855474242"
-                  className="text-islamic-primary-green hover:text-islamic-primary-green-hover text-2xl font-semibold underline transition-colors duration-150"
+                  href={`tel:${phone}`}
+                  className="text-islamic-primary-green hover:text-islamic-primary-green-hover cursor-pointer transition-colors duration-300"
                 >
-                  +9715855474242
+                  {phone}
                 </Link>
               </p>
             </div>
           </div>
-
-          <div className="mt-[32px] md:mt-0 md:flex-1">
-            <Image
-              src={fraudCubesImgData}
-              width={502}
-              height={764}
-              alt=""
-              className="pointer-events-none select-none md:absolute md:right-[-385px] md:top-[350px] md:translate-x-[-50%] md:translate-y-[-50%] lg:right-[-295px] xl:right-[-265px]"
-            />
+          <div className="absolute bottom-[-220px] z-[-1] self-center min-[385px]:bottom-[-300px] lg:hidden">
+            <Image src={mobileStarsImgData} alt="" />
+          </div>
+          <div className="right-0 top-[10%] hidden lg:absolute lg:block">
+            <Image src={desktopStarsImgData} alt="" width={475} height={600} />
           </div>
         </div>
       </Container>
