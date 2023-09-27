6ab30ae215df83a6d257ec9dd3643d860e0e64e8
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-09-27 11:44:01 +0300

    fix(islamic-website): id locale enabled

--- apps/islamic-website/middleware.ts
+++ apps/islamic-website/middleware.ts
@@ -2,7 +2,7 @@ import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['en', 'ar'],
+  locales: ['en', 'ar', 'id'],
 
   // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
   defaultLocale: 'en',
