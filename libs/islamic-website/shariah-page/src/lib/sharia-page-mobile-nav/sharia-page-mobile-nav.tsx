9a7e971cd922fac4932a3a04856f8c10dc2dad3d
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-09-29 00:30:12 +0300

    fix(islamic-website): mobile page nav position

--- libs/islamic-website/shariah-page/src/lib/sharia-page-mobile-nav/sharia-page-mobile-nav.tsx
+++ libs/islamic-website/shariah-page/src/lib/sharia-page-mobile-nav/sharia-page-mobile-nav.tsx
@@ -14,6 +14,7 @@ export function ShariPageMobileNav({
   onSectionSelect: (newSection: string) => void;
 }) {
   const [isBlurred, setBlured] = useState(false);
+  console.log('KEEEk');
 
   useEffect(() => {
     const offset = 500;
@@ -76,7 +77,7 @@ function FixedPageNavigation({
   }, [activeSection]);
 
   return (
-    <div className="fixed left-0 top-[72px] z-10 w-full border-b-[1px] border-[#2F2F2F] bg-[#010304CC] backdrop-blur-[6px]">
+    <div className="fixed left-0 top-[136px] z-[50] w-full border-b-[1px] border-[#2F2F2F] bg-[#010304CC] backdrop-blur-[6px] md:top-[112px]">
       <Container>
         <div className="flex flex-col">
           <div
