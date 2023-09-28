42537435eb28083063502a7a2d5c7995133b03eb
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-09-29 00:32:27 +0300

    fix(islamic-website): mobile page nav z-index

--- libs/islamic-website/shariah-page/src/lib/sharia-page-mobile-nav/sharia-page-mobile-nav.tsx
+++ libs/islamic-website/shariah-page/src/lib/sharia-page-mobile-nav/sharia-page-mobile-nav.tsx
@@ -77,7 +77,7 @@ function FixedPageNavigation({
   }, [activeSection]);
 
   return (
-    <div className="fixed left-0 top-[136px] z-[50] w-full border-b-[1px] border-[#2F2F2F] bg-[#010304CC] backdrop-blur-[6px] md:top-[112px]">
+    <div className="fixed left-0 top-[136px] z-[10] w-full border-b-[1px] border-[#2F2F2F] bg-[#010304CC] backdrop-blur-[6px] md:top-[112px]">
       <Container>
         <div className="flex flex-col">
           <div
