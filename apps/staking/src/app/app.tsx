16cf943820bd368bef829cfc4d3ef84cc42dbce9
Oleg Shilov <me@olegshilov.com>
2023-10-14 02:25:18 +0700

    fix: build apps without module federation, shared is not buildable lib now

--- apps/staking/src/app/app.tsx
+++ apps/staking/src/app/app.tsx
@@ -1,8 +1,19 @@
-import { Suspense } from 'react';
+import { Suspense, lazy } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { NotFoundPage, PendingPage } from '@haqq/shell-ui-kit';
-import { ValidatorListPage } from '@haqq/staking/validator-list-page';
-import { ValidatorDetailsPage } from '@haqq/staking/validator-details-page';
+
+const ValidatorListPage = lazy(async () => {
+  const { ValidatorListPage } = await import(
+    '@haqq/staking/validator-list-page'
+  );
+  return { default: ValidatorListPage };
+});
+const ValidatorDetailsPage = lazy(async () => {
+  const { ValidatorDetailsPage } = await import(
+    '@haqq/staking/validator-details-page'
+  );
+  return { default: ValidatorDetailsPage };
+});
 
 export function App() {
   return (
