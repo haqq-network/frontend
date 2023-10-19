1622156b3393a80cc2fbfe2be110317461b19698
Oleg Shilov <me@olegshilov.com>
2023-10-19 16:22:20 +0700

    ci: add commit sha to envs

--- apps/governance/src/environments/environment.prod.ts
+++ apps/governance/src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   isProduction: false,
   sentryDsn: process.env['NX_GOVERNANCE_SENTRY_DSN'],
   walletConnectProjectId: process.env['NX_WALLETCONNECT_PROJECT_ID'],
+  commitSha: process.env['GIT_COMMIT_SHA'],
 };
