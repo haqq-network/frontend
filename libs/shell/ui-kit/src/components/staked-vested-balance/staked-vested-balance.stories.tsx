7ef5cadff257efa5a383ff5e69b0be5e3be5d2da
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-10-24 15:17:13 +0300

    fix(shell): add staked vested component & story

---
+++ libs/shell/ui-kit/src/components/staked-vested-balance/staked-vested-balance.stories.tsx
@@ -0,0 +1,41 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { StakedVestedBalance as StakedVestedBalanceComponent } from './staked-vested-balance';
+
+const meta: Meta<typeof StakedVestedBalanceComponent> = {
+  component: StakedVestedBalanceComponent,
+  title: 'shell/ui-kit/Staked Vested Balance',
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof StakedVestedBalanceComponent>;
+
+export const Default: Story = {
+  args: {
+    staked: 40,
+    vested: 60,
+  },
+};
+
+export const StakedZero: Story = {
+  args: {
+    staked: 0,
+    vested: 100,
+  },
+};
+
+export const VestedZero: Story = {
+  args: {
+    staked: 100,
+    vested: 0,
+  },
+};
+
+export const NoVestingOrStake: Story = {
+  args: {
+    staked: 0,
+    vested: 0,
+  },
+};
