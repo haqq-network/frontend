3eb26ba0e6b58794d65414397263ef04fe4fc806
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-10-24 15:17:13 +0300

    fix(shell): add staked vested component & story

---
+++ libs/shell/ui-kit/src/components/staked-vested-balance/staked-vested-balance.tsx
@@ -0,0 +1,41 @@
+export function StakedVestedBalance({
+  staked,
+  vested,
+}: {
+  staked: number;
+  vested: number;
+}) {
+  return (
+    <div className="flex flex-col">
+      <div
+        className="flex flex-row gap-[4px]"
+        style={{ maxWidth: `${staked}+${vested}%` }}
+      >
+        {staked > 0 && (
+          <div
+            className="h-[6px] min-w-[6px] rounded-[4px] bg-[#0489D4]"
+            style={{ width: `${staked}%` }}
+          />
+        )}
+        {vested > 0 && (
+          <div
+            className="h-[6px] min-w-[6px] rounded-[4px] bg-[#E3A13F]"
+            style={{ width: `${vested}%` }}
+          />
+        )}
+      </div>
+      <div className="flex select-none flex-col justify-between pt-[4px]">
+        {vested > 0 && (
+          <div className="text-[12px] font-[500] leading-[18px] text-[#E3A13F]">
+            Vested: {vested}
+          </div>
+        )}
+        {staked > 0 && (
+          <div className="text-[12px] font-[500] leading-[18px] text-[#0489D4]">
+            Staked: {staked}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
--- libs/shell/ui-kit/src/index.ts
+++ libs/shell/ui-kit/src/index.ts
@@ -55,3 +55,4 @@ export * from './utils/is-number';
 export * from './components/low-balance-alert/low-balance-alert';
 export * from './components/select/select';
 export * from './components/validator-status/validator-status';
+export * from './components/staked-vested-balance/staked-vested-balance';
