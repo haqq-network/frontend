e08756c66e318fe4886655dbff6167625cb2ee15
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-10-25 23:30:42 +0300

    chore(haqq-website): 5 min revalidate time

--- apps/haqq-website/constants.ts
+++ apps/haqq-website/constants.ts
@@ -4,7 +4,7 @@ export const DEPLOY_URL = process.env['NEXT_PUBLIC_VERCEL_URL']
 export const STORYBLOK_ACCESS_TOKEN =
   process.env['HAQQ_STORYBLOK_ACCESS_TOKEN'];
 export const VERCEL_ENV = process.env['VERCEL_ENV'];
-export const REVALIDATE_TIME = 3600;
+export const REVALIDATE_TIME = 300;
 export const FALCONER_ENDPOINT = process.env['FALCONER_ENDPOINT'];
 export const TURNSTILE_SITEKEY = process.env['TURNSTILE_SITEKEY'];
 export const NX_WALLETCONNECT_PROJECT_ID =
