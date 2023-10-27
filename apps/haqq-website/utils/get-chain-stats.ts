741e4b9993c9fe43bdb6b656b28a221797e087cb
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-10-28 00:39:59 +0300

    fix(haqq-website): get chain stats from falconer

--- apps/haqq-website/utils/get-chain-stats.ts
+++ apps/haqq-website/utils/get-chain-stats.ts
@@ -1,5 +1,5 @@
 import { cache } from 'react';
-import { REVALIDATE_TIME } from '../constants';
+import { FALCONER_ENDPOINT, REVALIDATE_TIME } from '../constants';
 
 export const revalidate = REVALIDATE_TIME;
 
@@ -22,16 +22,44 @@ export const getMainnetAccounts = cache(async (defaultValue: number) => {
 });
 
 export const getChainStats = cache(async () => {
-  const mainnetAccountsCreated = await getMainnetAccounts(1163361);
+  try {
+    const response = await fetch(`${FALCONER_ENDPOINT}/haqq/chain_stats`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      next: {
+        revalidate,
+      },
+    });
 
+    if (response.ok) {
+      const data = await response.json();
+
+      return {
+        mainnetAccountsCreated: Number(data.accounts),
+        transactionsInLast24Hours: Number(data.transactionsIn24Hour),
+        secondsToConsensusFinality: Number(data.consensusFinality),
+        averageCostPerTransaction: Number(data.transactionAvgCost),
+        era: Number(data.coinomicsEra),
+        emissionRate: Number(data.coinomicsEmissionRate),
+        emittedAlready: Number(data.supply),
+        willBeEmitted: Number(data.coinomicsWillBeMinted),
+      };
+    } else {
+      console.log('Response was not ok.', response);
+    }
+  } catch (error) {
+    console.error(error);
+  }
   return {
-    mainnetAccountsCreated,
-    transactionsInLast24Hours: 15000,
-    secondsToConsensusFinality: 5.6,
-    averageCostPerTransaction: 147,
-    era: 1,
+    mainnetAccountsCreated: 0,
+    transactionsInLast24Hours: 0,
+    secondsToConsensusFinality: 0,
+    averageCostPerTransaction: 0,
+    era: 0,
     emissionRate: 0,
-    emittedAlready: 20000000000,
-    willBeEmitted: 80000000000,
+    emittedAlready: 0,
+    willBeEmitted: 0,
   };
 });
