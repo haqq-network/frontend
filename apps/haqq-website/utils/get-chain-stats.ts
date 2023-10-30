da80c959b675c33db59c2ec7a5f703a3c2dabd65
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-10-30 12:06:00 +0300

    fix(islamic-website, haqq-website): use parseInt with radix 10

--- apps/haqq-website/utils/get-chain-stats.ts
+++ apps/haqq-website/utils/get-chain-stats.ts
@@ -37,14 +37,17 @@ export const getChainStats = cache(async () => {
       const data = await response.json();
 
       return {
-        mainnetAccountsCreated: Number(data.accounts),
-        transactionsInLast24Hours: Number(data.transactionsIn24Hour),
-        secondsToConsensusFinality: Number(data.consensusFinality),
-        averageCostPerTransaction: Number(data.transactionAvgCost),
-        era: Number(data.coinomicsEra),
-        emissionRate: Number(data.coinomicsEmissionRate),
-        emittedAlready: Number(data.supply),
-        willBeEmitted: Number(data.coinomicsWillBeMinted),
+        mainnetAccountsCreated: Number.parseInt(data.accounts, 10),
+        transactionsInLast24Hours: Number.parseInt(
+          data.transactionsIn24Hour,
+          10,
+        ),
+        secondsToConsensusFinality: Number.parseInt(data.consensusFinality, 10),
+        averageCostPerTransaction: Number.parseInt(data.transactionAvgCost, 10),
+        era: Number.parseInt(data.coinomicsEra, 10),
+        emissionRate: Number.parseInt(data.coinomicsEmissionRate, 10),
+        emittedAlready: Number.parseInt(data.supply, 10),
+        willBeEmitted: Number.parseInt(data.coinomicsWillBeMinted, 10),
       };
     } else {
       console.log('Response was not ok.', response);
