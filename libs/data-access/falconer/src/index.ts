887d09bcde7c7be5d1991ce23cb44ae907f7d1e5
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-11-03 11:46:37 +0300

    feat(data-access): add getPrivacyPolicy method

--- libs/data-access/falconer/src/index.ts
+++ libs/data-access/falconer/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/get-members-data/get-members-data';
 export * from './lib/get-whitepaper-data/get-whitepaper';
 export * from './lib/get-news-data/get-news-data';
 export * from './lib/get-islamic-index-page-data/get-islamic-index-page-data';
+export * from './lib/get-privacy-policy-data/get-privacy-policy';
