3680b0d02330bbf74a85f1c9e4e7db9db7c4cbdd
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-12-19 02:39:34 +0300

    fix(islamic-website): main page bg image

--- libs/islamic-website/index-page/src/lib/index-page/index-page.tsx
+++ libs/islamic-website/index-page/src/lib/index-page/index-page.tsx
@@ -5,7 +5,7 @@ import { LearnAndGrowBlock } from '../learn-and-grow-block/learn-and-grow-block'
 import { NewsBlock } from '../news-block/news-block';
 import { PortfolioBlock } from '../portfolio-block/portfolio-block';
 import { ChainStats, WhyBlock } from '../why-block/why-block';
-import { Fragment } from 'react';
+import { Fragment, PropsWithChildren } from 'react';
 import { Marquee } from '../marquee/marquee';
 import { Container, Member, NewsPost } from '@haqq/islamic-website-ui-kit';
 import { FinanceBlock } from '../finance-block/finance-block';
@@ -49,7 +49,7 @@ function Hero({ stats }: { stats: ChainStats }) {
   const t = useTranslations('index-page.hero-block');
 
   return (
-    <section className="overflow-x-clip">
+    <HeroBg>
       <Container className="relative">
         <HeroBlock />
 
@@ -59,35 +59,36 @@ function Hero({ stats }: { stats: ChainStats }) {
 
         <WhyBlock stats={stats} />
       </Container>
-
-      <HeroBg />
-    </section>
+    </HeroBg>
   );
 }
 
-function HeroBg() {
+function HeroBg({ children }: PropsWithChildren) {
   return (
-    <Container className="relative">
-      <div
-        className={clsx(
-          'absolute z-[-1] select-none',
-          'h-[1011px] w-[1038px] md:h-[877px] md:w-[901px] lg:h-[1401px] lg:w-[1439px]',
-          '-top-1/2 right-1/2 translate-x-[37%] translate-y-[-21.3%]',
-          'md:translate-x-1/2 md:translate-y-[-16.1%]',
-          'lg:translate-y-[-23.45%]',
-          'xl:translate-y-[-24.9%]',
-          'min-[1440px]:translate-y-[-23.8%]',
-        )}
-      >
-        <div className="z-1 pointer-events-none absolute inset-0 scale-[3.5] bg-gradient-to-r from-[#010304] from-10% to-transparent md:scale-100 lg:scale-[1.5] xl:scale-100" />
-        <Image
-          src="/assets/images/moon.webp"
-          alt=""
-          fill
-          className="pointer-events-none z-[-2]"
-          priority
-        />
-      </div>
-    </Container>
+    <section className="overflow-x-clip">
+      <Container className="relative">
+        <div
+          className={clsx(
+            'absolute z-[-1] select-none',
+            'h-[1011px] w-[1038px] md:h-[877px] md:w-[901px] lg:h-[1401px] lg:w-[1439px]',
+            '-top-1/2 right-1/2 translate-x-[37%] translate-y-[-21.3%]',
+            'md:translate-x-1/2 md:translate-y-[-16.1%]',
+            'lg:translate-y-[-23.45%]',
+            'xl:translate-y-[-24.9%]',
+            'min-[1440px]:translate-y-[-23.8%]',
+          )}
+        >
+          <div className="z-1 pointer-events-none absolute inset-0 scale-[3.5] bg-gradient-to-r from-[#010304] from-10% to-transparent md:scale-100 lg:scale-[1.5] xl:scale-100" />
+          <Image
+            src="/assets/images/moon.webp"
+            alt=""
+            fill
+            className="pointer-events-none z-[-2]"
+            priority
+          />
+        </div>
+      </Container>
+      {children}
+    </section>
   );
 }
