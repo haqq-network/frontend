727236cd503d26b91e6c4eb47ee4fe3351d6d51f
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-12-19 02:39:49 +0300

    fix(islamic-website): whitepaper page bg image

--- libs/islamic-website/whitepaper-page/src/lib/whitepaper-page/whitepaper-page.tsx
+++ libs/islamic-website/whitepaper-page/src/lib/whitepaper-page/whitepaper-page.tsx
@@ -6,31 +6,35 @@ import {
 import clsx from 'clsx';
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
+import { PropsWithChildren } from 'react';
 
-function WhitepaperPageBgImage() {
+function WhitepaperPageBgImage({ children }: PropsWithChildren) {
   return (
-    <Container className="relative">
-      <div
-        className={clsx(
-          'absolute z-[-1] select-none',
-          'h-[1011px] w-[1038px] md:h-[877px] md:w-[901px] lg:h-[1401px] lg:w-[1439px]',
-          '-top-1/2 right-1/2 translate-x-[37%] translate-y-[-24.5%]',
-          'md:translate-x-1/2 md:translate-y-[-22%]',
-          'lg:translate-y-[-28.3%]',
-          'xl:translate-y-[-29.75%]',
-          'min-[1440px]:translate-y-[-28.7%]',
-        )}
-      >
-        <div className="z-1 pointer-events-none absolute inset-0 scale-[3.5] bg-gradient-to-r from-[#010304] from-10% to-transparent md:scale-100 lg:scale-[1.5] xl:scale-100" />
-        <Image
-          src="/assets/images/moon.webp"
-          alt=""
-          fill
-          className="pointer-events-none z-[-2]"
-          priority
-        />
-      </div>
-    </Container>
+    <section className="overflow-x-clip">
+      <Container className="relative">
+        <div
+          className={clsx(
+            'absolute z-[-1] select-none',
+            'h-[1011px] w-[1038px] md:h-[877px] md:w-[901px] lg:h-[1401px] lg:w-[1439px]',
+            '-top-1/2 right-1/2 translate-x-[37%] translate-y-[-24.5%]',
+            'md:translate-x-1/2 md:translate-y-[-22%]',
+            'lg:translate-y-[-28.3%]',
+            'xl:translate-y-[-29.75%]',
+            'min-[1440px]:translate-y-[-28.7%]',
+          )}
+        >
+          <div className="z-1 pointer-events-none absolute inset-0 scale-[3.5] bg-gradient-to-r from-[#010304] from-10% to-transparent md:scale-100 lg:scale-[1.5] xl:scale-100" />
+          <Image
+            src="/assets/images/moon.webp"
+            alt=""
+            fill
+            className="pointer-events-none z-[-2]"
+            priority
+          />
+        </div>
+      </Container>
+      {children}
+    </section>
   );
 }
 
@@ -38,7 +42,7 @@ export function WhitepaperPage({ whitepaper }: { whitepaper?: string }) {
   const t = useTranslations('whitepaper-page');
 
   return (
-    <section>
+    <WhitepaperPageBgImage>
       <Container className="mt-[32px] flex flex-col pb-[60px] text-white md:mt-[52px] md:pb-[100px] lg:mt-[68px] lg:pb-[140px]">
         <h1 className="text-[46px] font-[600] leading-[52px] md:text-[60px] md:leading-none lg:text-[80px]">
           {t('title')}
@@ -66,8 +70,6 @@ export function WhitepaperPage({ whitepaper }: { whitepaper?: string }) {
           </article>
         </div>
       </Container>
-
-      <WhitepaperPageBgImage />
-    </section>
+    </WhitepaperPageBgImage>
   );
 }
