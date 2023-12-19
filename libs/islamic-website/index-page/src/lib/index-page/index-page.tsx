c9126fd7c61620ae2177f93511737095edfc9a81
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-12-19 13:15:42 +0300

    fix(islamic-website): correct bg images index and whitepaper page

--- libs/islamic-website/index-page/src/lib/index-page/index-page.tsx
+++ libs/islamic-website/index-page/src/lib/index-page/index-page.tsx
@@ -5,7 +5,7 @@ import { LearnAndGrowBlock } from '../learn-and-grow-block/learn-and-grow-block'
 import { NewsBlock } from '../news-block/news-block';
 import { PortfolioBlock } from '../portfolio-block/portfolio-block';
 import { ChainStats, WhyBlock } from '../why-block/why-block';
-import { Fragment, PropsWithChildren } from 'react';
+import { Fragment } from 'react';
 import { Marquee } from '../marquee/marquee';
 import { Container, Member, NewsPost } from '@haqq/islamic-website-ui-kit';
 import { FinanceBlock } from '../finance-block/finance-block';
@@ -49,8 +49,9 @@ function Hero({ stats }: { stats: ChainStats }) {
   const t = useTranslations('index-page.hero-block');
 
   return (
-    <HeroBg>
-      <Container className="relative">
+    <div className="overflow-x-clip">
+      <HeroBg />
+      <Container>
         <HeroBlock />
 
         <Marquee className="my-[80px] md:my-[100px] xl:mt-[140px]">
@@ -59,36 +60,33 @@ function Hero({ stats }: { stats: ChainStats }) {
 
         <WhyBlock stats={stats} />
       </Container>
-    </HeroBg>
+    </div>
   );
 }
 
-function HeroBg({ children }: PropsWithChildren) {
+function HeroBg() {
   return (
-    <section className="overflow-x-clip">
-      <Container className="relative">
-        <div
-          className={clsx(
-            'absolute z-[-1] select-none',
-            'h-[1011px] w-[1038px] md:h-[877px] md:w-[901px] lg:h-[1401px] lg:w-[1439px]',
-            '-top-1/2 right-1/2 translate-x-[37%] translate-y-[-21.3%]',
-            'md:translate-x-1/2 md:translate-y-[-16.1%]',
-            'lg:translate-y-[-23.45%]',
-            'xl:translate-y-[-24.9%]',
-            'min-[1440px]:translate-y-[-23.8%]',
-          )}
-        >
-          <div className="z-1 pointer-events-none absolute inset-0 scale-[3.5] bg-gradient-to-r from-[#010304] from-10% to-transparent md:scale-100 lg:scale-[1.5] xl:scale-100" />
-          <Image
-            src="/assets/images/moon.webp"
-            alt=""
-            fill
-            className="pointer-events-none z-[-2]"
-            priority
-          />
-        </div>
-      </Container>
-      {children}
-    </section>
+    <Container className="relative">
+      <div
+        className={clsx(
+          'absolute z-[-1] select-none',
+          'h-[1011px] w-[1038px] md:h-[877px] md:w-[901px] lg:h-[1401px] lg:w-[1439px]',
+          '-top-1/2 right-1/2 translate-x-[37%] translate-y-[-21.3%]',
+          'md:translate-x-1/2 md:translate-y-[-16.1%]',
+          'lg:translate-y-[-23.45%]',
+          'xl:translate-y-[-24.9%]',
+          'min-[1440px]:translate-y-[-23.8%]',
+        )}
+      >
+        <div className="z-1 pointer-events-none absolute inset-0 scale-[3.5] bg-gradient-to-r from-[#010304] from-10% to-transparent md:scale-100 lg:scale-[1.5] xl:scale-100" />
+        <Image
+          src="/assets/images/moon.webp"
+          alt=""
+          fill
+          className="pointer-events-none z-[-2]"
+          priority
+        />
+      </div>
+    </Container>
   );
 }
--- libs/islamic-website/whitepaper-page/src/lib/whitepaper-page/whitepaper-page.tsx
+++ libs/islamic-website/whitepaper-page/src/lib/whitepaper-page/whitepaper-page.tsx
@@ -6,35 +6,31 @@ import {
 import clsx from 'clsx';
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
-import { PropsWithChildren } from 'react';
 
-function WhitepaperPageBgImage({ children }: PropsWithChildren) {
+function WhitepaperPageBgImage() {
   return (
-    <section className="overflow-x-clip">
-      <Container className="relative">
-        <div
-          className={clsx(
-            'absolute z-[-1] select-none',
-            'h-[1011px] w-[1038px] md:h-[877px] md:w-[901px] lg:h-[1401px] lg:w-[1439px]',
-            '-top-1/2 right-1/2 translate-x-[37%] translate-y-[-24.5%]',
-            'md:translate-x-1/2 md:translate-y-[-22%]',
-            'lg:translate-y-[-28.3%]',
-            'xl:translate-y-[-29.75%]',
-            'min-[1440px]:translate-y-[-28.7%]',
-          )}
-        >
-          <div className="z-1 pointer-events-none absolute inset-0 scale-[3.5] bg-gradient-to-r from-[#010304] from-10% to-transparent md:scale-100 lg:scale-[1.5] xl:scale-100" />
-          <Image
-            src="/assets/images/moon.webp"
-            alt=""
-            fill
-            className="pointer-events-none z-[-2]"
-            priority
-          />
-        </div>
-      </Container>
-      {children}
-    </section>
+    <Container className="relative">
+      <div
+        className={clsx(
+          'absolute z-[-1] select-none',
+          'h-[1011px] w-[1038px] md:h-[877px] md:w-[901px] lg:h-[1401px] lg:w-[1439px]',
+          '-top-1/2 right-1/2 translate-x-[37%] translate-y-[-24.5%]',
+          'md:translate-x-1/2 md:translate-y-[-22%]',
+          'lg:translate-y-[-28.3%]',
+          'xl:translate-y-[-29.75%]',
+          'min-[1440px]:translate-y-[-28.7%]',
+        )}
+      >
+        <div className="z-1 pointer-events-none absolute inset-0 scale-[3.5] bg-gradient-to-r from-[#010304] from-10% to-transparent md:scale-100 lg:scale-[1.5] xl:scale-100" />
+        <Image
+          src="/assets/images/moon.webp"
+          alt=""
+          fill
+          className="pointer-events-none z-[-2]"
+          priority
+        />
+      </div>
+    </Container>
   );
 }
 
@@ -42,7 +38,8 @@ export function WhitepaperPage({ whitepaper }: { whitepaper?: string }) {
   const t = useTranslations('whitepaper-page');
 
   return (
-    <WhitepaperPageBgImage>
+    <div className="overflow-x-clip">
+      <WhitepaperPageBgImage />
       <Container className="mt-[32px] flex flex-col pb-[60px] text-white md:mt-[52px] md:pb-[100px] lg:mt-[68px] lg:pb-[140px]">
         <h1 className="text-[46px] font-[600] leading-[52px] md:text-[60px] md:leading-none lg:text-[80px]">
           {t('title')}
@@ -70,6 +67,6 @@ export function WhitepaperPage({ whitepaper }: { whitepaper?: string }) {
           </article>
         </div>
       </Container>
-    </WhitepaperPageBgImage>
+    </div>
   );
 }
