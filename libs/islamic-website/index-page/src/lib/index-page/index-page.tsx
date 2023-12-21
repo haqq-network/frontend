1163a56b320037e95e494f82ba97d1866f9019de
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-12-21 15:42:51 +0300

    fix(islamic-website): use moon animated background component in main and wp pages

--- libs/islamic-website/index-page/src/lib/index-page/index-page.tsx
+++ libs/islamic-website/index-page/src/lib/index-page/index-page.tsx
@@ -1,28 +1,22 @@
-'use client';
-
-import Image from 'next/image';
 import { HeroBlock } from '../hero-block/hero-block';
 import { JoinCommunityBlock } from '../join-community-block/join-community-block';
 import { LearnAndGrowBlock } from '../learn-and-grow-block/learn-and-grow-block';
 import { NewsBlock } from '../news-block/news-block';
 import { PortfolioBlock } from '../portfolio-block/portfolio-block';
 import { ChainStats, WhyBlock } from '../why-block/why-block';
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment } from 'react';
 import { Marquee } from '../marquee/marquee';
-import { Container, Member, NewsPost } from '@haqq/islamic-website-ui-kit';
+import {
+  Container,
+  Member,
+  MoonAnimatedBg,
+  NewsPost,
+} from '@haqq/islamic-website-ui-kit';
 import { FinanceBlock } from '../finance-block/finance-block';
 import { BoardMembersBlock } from '../board-members-block/board-members-block';
 import clsx from 'clsx';
 import { FundsBlock } from '../funds-block/funds-block';
 import { useTranslations } from 'next-intl';
-
-function delay(milliseconds: number): Promise<void> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, milliseconds);
-  });
-}
 
 export function IndexPage({
   news,
@@ -60,7 +54,15 @@ function Hero({ stats }: { stats: ChainStats }) {
 
   return (
     <div className="overflow-x-clip">
-      <HeroBg />
+      <MoonAnimatedBg
+        className={clsx(
+          'translate-x-[37%] translate-y-[-21.3%]',
+          'md:translate-x-1/2 md:translate-y-[-16.1%]',
+          'lg:translate-y-[-23.45%]',
+          'xl:translate-y-[-24.9%]',
+          'min-[1440px]:translate-y-[-23.8%]',
+        )}
+      />
       <Container>
         <HeroBlock />
 
@@ -71,56 +73,5 @@ function Hero({ stats }: { stats: ChainStats }) {
         <WhyBlock stats={stats} />
       </Container>
     </div>
-  );
-}
-function HeroBg() {
-  const [animationStep, setAnimationStep] = useState<1 | 2 | 3>(1);
-
-  useEffect(() => {
-    const runAnimation = async () => {
-      await delay(3000);
-      setAnimationStep(2);
-      await delay(4000);
-      setAnimationStep(3);
-    };
-
-    runAnimation();
-  }, []);
-
-  return (
-    <Container className="relative">
-      <div
-        className={clsx(
-          'absolute z-[-1] select-none',
-          'h-[1011px] w-[1038px] md:h-[877px] md:w-[901px] lg:h-[1401px] lg:w-[1439px]',
-          '-top-1/2 right-1/2 translate-x-[37%] translate-y-[-21.3%]',
-          'md:translate-x-1/2 md:translate-y-[-16.1%]',
-          'lg:translate-y-[-23.45%]',
-          'xl:translate-y-[-24.9%]',
-          'min-[1440px]:translate-y-[-23.8%]',
-          'duration-[7000ms]',
-          animationStep === 1 && 'scale-[.9] opacity-0 ease-in',
-          animationStep === 2 && 'opacity-90',
-          animationStep === 3 && 'scale-[1] opacity-100 ease-out',
-        )}
-      >
-        <div
-          className={clsx(
-            'z-1 pointer-events-none absolute inset-0 scale-[3.5] transform rounded-full bg-gradient-to-r from-[#010304] from-10% to-transparent duration-[4000ms] ease-out md:scale-100 lg:scale-[1.5] xl:scale-100',
-            animationStep === 2 && 'rotate-90',
-          )}
-        />
-        <Image
-          src="/assets/images/moon.webp"
-          alt=""
-          fill
-          className="pointer-events-none z-[-2]"
-          priority
-          onLoad={() => {
-            setAnimationStep(1);
-          }}
-        />
-      </div>
-    </Container>
   );
 }
