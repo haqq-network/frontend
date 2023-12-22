960e4ad06428c10f0af912c3635c294236829ed9
Oleg Shilov <me@olegshilov.com>
2023-12-22 15:08:54 +0700

    feat(islamic-website): add sentry integration to islamic website

---
+++ apps/islamic-website/app/error.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useEffect } from 'react';
+import * as Sentry from '@sentry/nextjs';
+
+export default function ErrorPage({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to Sentry
+    Sentry.captureException(error);
+  }, [error]);
+
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button
+        onClick={() => {
+          reset();
+        }}
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
---
+++ apps/islamic-website/app/global-error.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import * as Sentry from '@sentry/nextjs';
+import NextError from 'next/error';
+import { useEffect } from 'react';
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
+
+  return (
+    <html>
+      <body>
+        <h2>Something went wrong!</h2>
+
+        {/* This is the default Next.js error component but it doesn't allow omitting the statusCode property yet. */}
+        <NextError statusCode={undefined as any} />
+
+        <button
+          onClick={() => {
+            reset();
+          }}
+        >
+          Try again
+        </button>
+      </body>
+    </html>
+  );
+}
