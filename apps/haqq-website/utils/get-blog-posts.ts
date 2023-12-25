3217c102c4171af8179d3939f51f03babc33dc41
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-12-25 13:23:13 +0300

    feat(data-access): get blog data from falconer

--- apps/haqq-website/utils/get-blog-posts.ts
+++ apps/haqq-website/utils/get-blog-posts.ts
@@ -1,90 +1,37 @@
-import { storyblokInit, apiPlugin } from '@storyblok/js';
-import {
-  REVALIDATE_TIME,
-  STORYBLOK_ACCESS_TOKEN,
-  VERCEL_ENV,
-} from '../constants';
+import { REVALIDATE_TIME } from '../constants';
 import { cache } from 'react';
-import { Post } from '@haqq/haqq-website/blog';
+import { getHAQQBlogPosts } from '@haqq/data-access-falconer';
 
 export const revalidate = REVALIDATE_TIME;
 
-export interface StoryblokPost {
-  _uid: string;
-  title: string;
-  date: string;
-  slug: string;
-  description: string;
-  image: {
-    filename: null | string;
-  };
-  featured: boolean;
-  published: boolean;
-  content: string;
-  tags: string[];
-}
+export const getHAQQBlockPostsFromFalconer = cache(async () => {
+  try {
+    const posts = await getHAQQBlogPosts({
+      next: {
+        revalidate,
+      },
+    });
 
-function mapStorybookToPosts(data: { posts: StoryblokPost[] }): Post[] {
-  const posts = data.posts.map((post) => {
-    if (!post.published) {
-      return null;
+    const tags = [];
+
+    for (const post of posts) {
+      tags.push(...post.tags);
     }
 
-    const image =
-      post.image.filename && post.image.filename !== ''
-        ? {
-            src: post.image.filename,
-            width: Number(post.image.filename.split('/')[5].split('x')[0]),
-            height: Number(post.image.filename.split('/')[5].split('x')[1]),
-          }
-        : null;
-
-    return {
-      id: post._uid,
-      title: post.title,
-      slug: post.slug,
-      date: post.date,
-      description: post.description ?? '',
-      content: post.content ?? '',
-      image,
-      isFeatured: post.featured,
-      tags: post.tags,
-    } as Post;
-  });
-  const filteredPosts = posts.filter(Boolean) as Post[];
-  const sortedPosts = filteredPosts.sort((a, b) => {
-    return new Date(b.date).getTime() - new Date(a.date).getTime();
-  });
-
-  return sortedPosts;
-}
-
-export const getBlogPosts = cache(async () => {
-  const { storyblokApi } = storyblokInit({
-    accessToken: STORYBLOK_ACCESS_TOKEN,
-    use: [apiPlugin],
-  });
-
-  if (!storyblokApi) {
-    throw new Error('Failed to init storyblok');
+    return { posts, tags: [...new Set(tags)] };
+  } catch (error) {
+    console.error(error);
   }
 
-  const response = await storyblokApi.get('cdn/stories/blog', {
-    version: VERCEL_ENV === 'production' ? 'published' : 'draft',
-  });
-
-  const posts = mapStorybookToPosts(response.data.story.content);
-  const tags = [];
-
-  for (const post of posts) {
-    tags.push(...post.tags);
-  }
-
-  return { posts, tags: [...new Set(tags)] };
+  return { posts: [], tags: [] };
 });
 
 export const getBlogPost = cache(async (slug: string) => {
-  const { posts } = await getBlogPosts();
+  const posts = await getHAQQBlogPosts({
+    next: {
+      revalidate,
+    },
+  });
 
   return posts.find((post) => {
     return post.slug === slug;
---
+++ libs/data-access/falconer/src/lib/haqq/get-blog-posts.ts
@@ -0,0 +1,30 @@
+import { FALCONER_ENDPOINT } from '../../constants';
+import { FalconerRequestInit } from '../../types';
+
+interface Post {
+  id: string;
+  title: string;
+  slug: string;
+  date: string;
+  content?: string;
+  description?: string;
+  image: { src: string; width: number; height: number } | null;
+  isFeatured?: boolean;
+  tags: string[];
+}
+
+export async function getHAQQBlogPosts(options: Partial<FalconerRequestInit>) {
+  const requestUrl = new URL('/haqq/blog_posts', FALCONER_ENDPOINT);
+  const response = await fetch(requestUrl, {
+    method: 'GET',
+    ...options,
+  });
+
+  if (!response.ok) {
+    throw new Error('Blog posts fetch failed');
+  }
+
+  const responseJson = await response.json();
+
+  return responseJson.posts as Post[];
+}
