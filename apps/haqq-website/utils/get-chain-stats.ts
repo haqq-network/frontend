8af775c4961aeee0d268994fe21223c468b06178
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-12-25 18:09:15 +0300

    chore(data-access): names of methods according to a single standard

--- apps/haqq-website/utils/get-chain-stats.ts
+++ apps/haqq-website/utils/get-chain-stats.ts
@@ -1,12 +1,12 @@
 import { cache } from 'react';
 import { REVALIDATE_TIME } from '../constants';
-import { getHaqqChainStats } from '@haqq/data-access-falconer';
+import { getHaqqChainStatsData } from '@haqq/data-access-falconer';
 
 export const revalidate = REVALIDATE_TIME;
 
 export const getChainStatsFromFalconer = cache(async () => {
   try {
-    const stats = await getHaqqChainStats({
+    const stats = await getHaqqChainStatsData({
       next: {
         revalidate,
       },
