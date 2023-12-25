499450ccd24ef962e8c5533a276c9bbe8f949d9b
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-12-25 16:10:48 +0300

    fix(data-access): resolve requested changes

--- apps/haqq-website/utils/get-partners.ts
+++ apps/haqq-website/utils/get-partners.ts
@@ -1,12 +1,12 @@
 import { REVALIDATE_TIME } from '../constants';
 import { cache } from 'react';
-import { getHAQQPartners } from '@haqq/data-access-falconer';
+import { getHaqqPartners } from '@haqq/data-access-falconer';
 
 export const revalidate = REVALIDATE_TIME;
 
 export const getHAQQPartnersFromFalconer = cache(async () => {
   try {
-    const data = await getHAQQPartners({
+    const data = await getHaqqPartners({
       next: {
         revalidate,
       },
