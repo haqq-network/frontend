17295d62961e284f8f74f2513318c222d63cebac
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-12-25 16:52:02 +0300

    feat(haqq-website): add robots.ts

---
+++ apps/haqq-website/app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from 'next';
+import { DEPLOY_URL } from '../constants';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '*',
+    },
+    sitemap: `${DEPLOY_URL}/sitemap.xml`,
+    host: DEPLOY_URL,
+  };
+}
