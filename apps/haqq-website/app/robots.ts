fda7529ad9a6d05636291a2bf122ccd069a0b02a
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-12-26 13:28:01 +0300

    fix(haqq-website): try to get correct host

--- apps/haqq-website/app/robots.ts
+++ apps/haqq-website/app/robots.ts
@@ -1,13 +1,17 @@
 import { MetadataRoute } from 'next';
-import { DEPLOY_URL } from '../constants';
+import { VERCEL_ENV } from '../constants';
+import { headers } from 'next/headers';
 
 export default function robots(): MetadataRoute.Robots {
+  const host = headers().get('x-forwarded-host') || headers().get('host');
+  const protocol = VERCEL_ENV === 'production' ? 'https' : 'http';
+
   return {
     rules: {
       userAgent: '*',
-      allow: '*',
+      allow: '/',
     },
-    sitemap: `${DEPLOY_URL}/sitemap.xml`,
-    host: DEPLOY_URL,
+    sitemap: `${protocol}://${host}/sitemap.xml`,
+    host: `${protocol}://${host}`,
   };
 }
