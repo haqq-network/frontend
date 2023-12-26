d6dafc83858fef9d65a8996424024ea965212387
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-12-26 13:26:55 +0300

    chore(islamic-website): correct allow at robots.ts

--- apps/islamic-website/app/robots.ts
+++ apps/islamic-website/app/robots.ts
@@ -10,7 +10,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '*',
+      allow: '/',
     },
     sitemap: sitemapUrls,
     host: DEPLOY_URL,
