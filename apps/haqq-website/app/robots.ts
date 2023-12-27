d67c1c0c72951c305c95e629bfd34d319e1c77c1
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-12-27 16:36:19 +0300

    fix(haqq-website): correct user agents robots txt

--- apps/haqq-website/app/robots.ts
+++ apps/haqq-website/app/robots.ts
@@ -3,11 +3,16 @@ import { SITE_URL } from '../constants';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/events/*',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        disallow: '/events/*',
+      },
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
     sitemap: new URL('/sitemap.xml', SITE_URL).toString(),
     host: new URL(SITE_URL).toString(),
   };
