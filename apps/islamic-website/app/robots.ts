e6685bc6d6c1445179761bd2ae97e5cbc5bcbc80
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-12-28 17:24:49 +0300

    fix(islamic-website): correct link to single sitemap in robots.txt

--- apps/islamic-website/app/robots.ts
+++ apps/islamic-website/app/robots.ts
@@ -1,18 +1,13 @@
 import { MetadataRoute } from 'next';
 import { SITE_URL } from '../constants';
-import { SUPPORTED_LOCALES } from '../constants';
 
 export default function robots(): MetadataRoute.Robots {
-  const sitemapUrls = SUPPORTED_LOCALES.map((locale) => {
-    return new URL(`/${locale}/sitemap.xml`, SITE_URL).toString();
-  });
-
   return {
     rules: {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: sitemapUrls,
+    sitemap: new URL('/sitemap.xml', SITE_URL).toString(),
     host: new URL(SITE_URL).toString(),
   };
 }
