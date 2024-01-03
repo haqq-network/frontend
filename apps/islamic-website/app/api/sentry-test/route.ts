c3dc2939c5b5d2a12a1fae52a4d9fdf8f196583c
Oleg Shilov <me@olegshilov.com>
2024-01-03 19:56:45 +0700

    fix(islamic-website): update codebase to work with latest next-intl
    - cleaup

--- apps/islamic-website/app/api/sentry-test/route.ts
+++
@@ -1,5 +0,0 @@
-// A faulty API route to test Sentry's error monitoring
-export async function GET() {
-  // throw new Error('Sentry Example API Route Error');
-  return Response.json({ data: null });
-}
--- apps/islamic-website/constants.ts
+++ apps/islamic-website/constants.ts
@@ -9,5 +9,5 @@ export const VERCEL_ENV = process.env['VERCEL_ENV'];
 export const REVALIDATE_TIME = 300;
 export const FALCONER_ENDPOINT = process.env['FALCONER_ENDPOINT'];
 export const TURNSTILE_SITEKEY = process.env['TURNSTILE_SITEKEY'];
-export const SUPPORTED_LOCALES = ['en', 'ar', 'id'] as const;
+export const SUPPORTED_LOCALES: Readonly<string[]> = ['en', 'ar', 'id'];
 export const SITE_URL = 'https://islamiccoin.net';
