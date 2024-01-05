0bbb9873092af983531bbceebc6a6d8418c0aacb
Oleg Shilov <me@olegshilov.com>
2024-01-05 13:42:32 +0700

    chore(storybook-host): add haqq logo to storybook
    - optimize images

---
+++ libs/storybook-host/.storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export const haqqStorybookTheme = create({
+  base: 'dark',
+  brandTitle: 'HAQQ Storybook',
+  brandUrl: 'https://haqq.network',
+  brandImage: 'https://haqq.network/assets/images/haqq-logo-storybook.svg',
+  brandTarget: '_blank',
+});
