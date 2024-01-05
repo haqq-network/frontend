5312de17eb0f1bf40e31836078d8b5cddc8e1af9
Oleg Shilov <me@olegshilov.com>
2024-01-05 13:56:18 +0700

    chore(haqq-ui-kit): rename stories

--- libs/haqq-website/ui-kit/src/lib/blog-post-card/blog-post-card.stories.tsx
+++ libs/haqq-website/ui-kit/src/lib/blog-post-card/blog-post-card.stories.tsx
@@ -3,7 +3,7 @@ import { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof BlogPostCardComponent> = {
   component: BlogPostCardComponent,
-  title: 'website/ui-kit/BlogPostCard',
+  title: 'haqq-website/ui-kit/BlogPostCard',
   parameters: {
     layout: 'centered',
   },
--- libs/haqq-website/ui-kit/src/lib/breadcrumb/breadcrumb.stories.tsx
+++ libs/haqq-website/ui-kit/src/lib/breadcrumb/breadcrumb.stories.tsx
@@ -3,7 +3,7 @@ import { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof BreadcrumbComponent> = {
   component: BreadcrumbComponent,
-  title: 'website/ui-kit',
+  title: 'haqq-website/ui-kit',
 };
 
 export default meta;
