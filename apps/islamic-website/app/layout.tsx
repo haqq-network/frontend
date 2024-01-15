3a9b889525a2f1b315a849d581ff6674336f3be0
Evgeniy Abramov <i@kio.ninja>
2024-01-15 19:55:02 +0400

    chore: test locale changes

--- apps/islamic-website/app/layout.tsx
+++ apps/islamic-website/app/layout.tsx
@@ -1,5 +1,18 @@
 import { PropsWithChildren } from 'react';
+import { SUPPORTED_LOCALES } from '../constants';
+import { unstable_setRequestLocale } from 'next-intl/server';
 
-export default function RootLayout({ children }: PropsWithChildren) {
+export default function RootLayout({
+  children,
+  params: { locale },
+}: PropsWithChildren & { params: { locale: string } }) {
+  unstable_setRequestLocale(locale);
+
   return children;
+}
+
+export function generateStaticParams() {
+  return SUPPORTED_LOCALES.map((locale) => {
+    return { locale };
+  });
 }
