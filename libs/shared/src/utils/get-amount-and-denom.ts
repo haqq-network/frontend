4bfba65e0576d811a850c2657c25b944e120116e
Oleg Shilov <me@olegshilov.com>
2024-01-24 23:52:25 +0400

    fix(shared): modify fee calculation to add instead of subtract
    closes: HQW-156

--- libs/shared/src/utils/get-amount-and-denom.ts
+++ libs/shared/src/utils/get-amount-and-denom.ts
@@ -4,7 +4,7 @@ export function getAmountAndDenom(amount: number, fee?: Fee) {
   // Multiply the amount by 10^18 and convert it to BigInt
   const bigIntAmount = BigInt(amount) * BigInt(10 ** 18);
 
-  const finalAmount = fee ? bigIntAmount - BigInt(fee.amount) : bigIntAmount;
+  const finalAmount = fee ? bigIntAmount + BigInt(fee.amount) : bigIntAmount;
 
   return {
     amount: finalAmount.toString(),
