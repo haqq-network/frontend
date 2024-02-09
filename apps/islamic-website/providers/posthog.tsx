024d7c4dc0bb562596396a3b9f468a1434e385b0
Oleg Shilov <me@olegshilov.com>
2024-02-09 18:29:38 +0300

    feat(islamic-website): add rewrite rules for posthog to nestjs config

--- apps/islamic-website/providers/posthog.tsx
+++ apps/islamic-website/providers/posthog.tsx
@@ -1,10 +1,12 @@
 'use client';
 import posthog from 'posthog-js';
 import { PostHogProvider } from 'posthog-js/react';
+import { DEPLOY_URL } from '../constants';
 
 if (typeof window !== 'undefined') {
   posthog.init(process.env['NEXT_PUBLIC_POSTHOG_KEY']!, {
-    api_host: process.env['NEXT_PUBLIC_POSTHOG_HOST'],
+    api_host: new URL('/ingest', DEPLOY_URL).toString(),
+    ui_host: process.env['NEXT_PUBLIC_POSTHOG_HOST'],
     capture_pageview: false,
   });
 }
