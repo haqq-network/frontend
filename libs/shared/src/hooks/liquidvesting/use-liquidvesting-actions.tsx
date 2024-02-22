c5da0aec14b4f74c355c998d8270b08a1db53b38
Oleg Shilov <me@olegshilov.com>
2024-02-22 09:20:49 +0300

    feat(shared): implement liquidvesting redeem

--- libs/shared/src/hooks/liquidvesting/use-liquidvesting-actions.tsx
+++ libs/shared/src/hooks/liquidvesting/use-liquidvesting-actions.tsx
@@ -8,7 +8,12 @@ import {
 } from '@evmos/transactions';
 import { type Hex } from 'viem';
 import { useNetwork, useWalletClient } from 'wagmi';
-import { MsgLiquidateParams, createTxMsgLiquidate } from './liquidvesting';
+import {
+  MsgLiquidateParams,
+  createTxMsgLiquidate,
+  createTxMsgRedeem,
+  MsgRedeemParams,
+} from './liquidvesting';
 import { DEFAULT_FEE, getChainParams } from '../../chains/get-chain-params';
 import { mapToCosmosChain } from '../../chains/map-to-cosmos-chain';
 import { useCosmosService } from '../../providers/cosmos-provider';
@@ -16,42 +21,15 @@ import { useSupportedChains } from '../../providers/wagmi-provider';
 import { getAmountIncludeFee } from '../../utils/get-amount-include-fee';
 import { useAddress } from '../use-address/use-address';
 
-export function useLiquidVestingActions() {
-  const {
-    broadcastTransaction,
-    getAccountBaseInfo,
-    getPubkey,
-    getTransactionStatus,
-  } = useCosmosService();
+export function useLiquidateActions() {
+  const { broadcastTransaction, getPubkey, getTransactionStatus, getSender } =
+    useCosmosService();
   const { haqqAddress, ethAddress } = useAddress();
   const chains = useSupportedChains();
   const { chain = chains[0] } = useNetwork();
   const chainParams = getChainParams(chain.id);
   const haqqChain = mapToCosmosChain(chainParams);
   const { data: walletClient } = useWalletClient();
-
-  const getSender = useCallback(
-    async (address: string, pubkey: string) => {
-      try {
-        const accInfo = await getAccountBaseInfo(address);
-
-        if (!accInfo) {
-          throw new Error('no base account info');
-        }
-
-        return {
-          accountAddress: address,
-          sequence: parseInt(accInfo.sequence, 10),
-          accountNumber: parseInt(accInfo.account_number, 10),
-          pubkey,
-        };
-      } catch (error) {
-        console.error((error as Error).message);
-        throw error;
-      }
-    },
-    [getAccountBaseInfo],
-  );
 
   const signTransaction = useCallback(
     async (msg: TxGenerated, sender: Sender) => {
@@ -60,9 +38,6 @@ export function useLiquidVestingActions() {
           method: 'eth_signTypedData_v4',
           params: [ethAddress as Hex, JSON.stringify(msg.eipToSign)],
         });
-
-        // const vsr = extractVRS(signature);
-        // console.log({ signature, vsr });
 
         const extension = signatureToWeb3Extension(
           haqqChain,
@@ -88,7 +63,7 @@ export function useLiquidVestingActions() {
     [ethAddress, haqqChain, walletClient],
   );
 
-  const getLiquidVestingParams = useCallback(
+  const getLiquidateParams = useCallback(
     (
       liquidateTo: string,
       amount: number,
@@ -103,6 +78,21 @@ export function useLiquidVestingActions() {
     [],
   );
 
+  const getRedeemParams = useCallback(
+    (
+      redeemTo: string,
+      amount: number,
+      balance: number,
+      fee: Fee,
+    ): MsgRedeemParams => {
+      return {
+        redeemTo,
+        ...getAmountIncludeFee(amount, balance, fee),
+      };
+    },
+    [],
+  );
+
   const handleLiquidate = useCallback(
     async (address?: string, amount?: number, balance?: number) => {
       const pubkey = await getPubkey(ethAddress as string);
@@ -110,7 +100,7 @@ export function useLiquidVestingActions() {
       const memo = 'Convert to liquid token';
 
       if (sender && address && haqqChain) {
-        const params = getLiquidVestingParams(
+        const params = getLiquidateParams(
           address,
           amount ?? 0,
           balance ?? 0,
@@ -149,7 +139,60 @@ export function useLiquidVestingActions() {
       getSender,
       haqqAddress,
       haqqChain,
-      getLiquidVestingParams,
+      getLiquidateParams,
+      signTransaction,
+      broadcastTransaction,
+      getTransactionStatus,
+    ],
+  );
+
+  const handleRedeem = useCallback(
+    async (address?: string, amount?: number, balance?: number) => {
+      const pubkey = await getPubkey(ethAddress as string);
+      const sender = await getSender(haqqAddress as string, pubkey);
+      const memo = 'Redeem liquid token';
+
+      if (sender && address && haqqChain) {
+        const params = getRedeemParams(
+          address,
+          amount ?? 0,
+          balance ?? 0,
+          DEFAULT_FEE,
+        );
+
+        const msg = createTxMsgRedeem(
+          haqqChain,
+          sender,
+          DEFAULT_FEE,
+          memo,
+          params,
+        );
+
+        const rawTx = await signTransaction(msg, sender);
+        const txResponse = await broadcastTransaction(rawTx);
+
+        if (txResponse.code !== 0) {
+          throw new Error(txResponse.raw_log);
+        }
+
+        const transactionStatus = await getTransactionStatus(txResponse.txhash);
+
+        if (transactionStatus === null) {
+          throw new Error('Transaction not found');
+        }
+
+        return transactionStatus.tx_response;
+      } else {
+        throw new Error('No sender or Validator address');
+      }
+    },
+    [
+      getPubkey,
+      ethAddress,
+      getSender,
+      haqqAddress,
+      haqqChain,
+      getRedeemParams,
       signTransaction,
       broadcastTransaction,
       getTransactionStatus,
@@ -159,6 +202,7 @@ export function useLiquidVestingActions() {
   return useMemo(() => {
     return {
       liquidate: handleLiquidate,
+      redeem: handleRedeem,
     };
-  }, [handleLiquidate]);
+  }, [handleLiquidate, handleRedeem]);
 }
