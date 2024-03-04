abe69c02451c3fcaf4839d3d281a402620774b57
Oleg Shilov <me@olegshilov.com>
2024-03-04 21:04:43 +0700

    feat(shared): add method get estimated fee for unsigned transaction

---
+++ libs/shared/src/utils/get-estimated-fee.ts
@@ -0,0 +1,43 @@
+const FALCONER_ENDPOINT = 'https://falconer.haqq.sh' as const;
+
+export interface EstimatedFeeRequest {
+  chainId: string;
+  bodyBytes: string;
+  fromAddress: string;
+}
+
+export interface EstimatedFeeResponse {
+  fee: string;
+  gas_price: string;
+  gas_used: string;
+}
+
+export async function getEstimatedFee({
+  chainId,
+  bodyBytes,
+  fromAddress,
+}: EstimatedFeeRequest): Promise<EstimatedFeeResponse> {
+  const requestUrl = new URL('/tx/simulate', FALCONER_ENDPOINT);
+
+  const response = await fetch(requestUrl.toString(), {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      chain_id: chainId,
+      b64_body_bytes: bodyBytes,
+      from_addr: fromAddress,
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error(
+      `Transaction simulation failed. Status: ${response.status}`,
+    );
+  }
+
+  const responseJson: EstimatedFeeResponse = await response.json();
+
+  return responseJson;
+}
