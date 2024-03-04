5994ec439624dd17b6ac41c9418fcfc84180b324
Oleg Shilov <me@olegshilov.com>
2024-03-04 21:05:04 +0700

    feat(share): add useDebounceValue hood

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -40,3 +40,4 @@ export * from './utils/get-amount-include-fee';
 export * from './hooks/liquidvesting/use-liquidvesting-actions';
 export * from './hooks/erc20/use-token-pairs';
 export * from './utils/get-estimated-fee';
+export * from './hooks/use-debounce-value/use-debounce-value';
