3ed1dd5797d5784b0ba34361804be0cb3e936780
Oleg Shilov <me@olegshilov.com>
2024-03-05 00:43:22 +0700

    feat(shared): add useThrottle hook

--- libs/shared/src/hooks/use-debounce/use-debounce.ts
+++ libs/shared/src/hooks/use-debounce/use-debounce.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-export function useDebounceValue<T>(value: T, delay: number): T {
+export function useDebounce<T>(value: T, delay: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
