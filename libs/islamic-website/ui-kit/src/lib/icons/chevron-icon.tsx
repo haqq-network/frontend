a05e0c7762642d374d5974be2791100ff87ac385
Jihad Hadj Husein <skiptomylou999@gmail.com>
2024-03-14 13:50:34 +0300

    feat(islamic-ui-kit): add icons

---
+++ libs/islamic-website/ui-kit/src/lib/icons/chevron-icon.tsx
@@ -0,0 +1,19 @@
+export function ChevronIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M5.29289 14L6.70711 15.4142L12 10.1213L17.2929 15.4142L18.7071 14L12 7.29289L5.29289 14Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
