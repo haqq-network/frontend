b612e46154bec27684f0dd8ca082582e89196b01
Jihad Hadj Husein <skiptomylou999@gmail.com>
2024-03-14 15:28:21 +0300

    fix(islamic-ui-kit): correct icon size in breadcrumb

--- libs/islamic-website/ui-kit/src/lib/breadcrumb/breadcrumb.tsx
+++ libs/islamic-website/ui-kit/src/lib/breadcrumb/breadcrumb.tsx
@@ -10,11 +10,11 @@ export function Breadcrumb({
   onBackClick: () => void;
 }) {
   return (
-    <div className=" flex  items-center gap-x-[6px] py-[10px] text-[12px] leading-[1.5em]">
+    <div className="flex items-center gap-x-[6px] py-[10px] text-[12px] leading-[1.5em]">
       <div className="cursor-pointer" onClick={onBackClick}>
         {backTitle}
       </div>
-      <ChevronIcon className="mb-[-2px] rotate-90" />
+      <ChevronIcon className="mb-[-2px] h-[18px] w-[18px] rotate-90" />
       <div className="truncate text-white/50">{title}</div>
     </div>
   );
