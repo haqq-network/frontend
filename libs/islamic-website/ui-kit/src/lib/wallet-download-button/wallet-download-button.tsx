e2f167de1059374a17e0cc749c7137634eda7770
Oleg Shilov <me@olegshilov.com>
2024-04-02 21:02:10 +0700

    feat(islamic-website): add data-attr to markets link in header

--- libs/islamic-website/ui-kit/src/lib/wallet-download-button/wallet-download-button.tsx
+++ libs/islamic-website/ui-kit/src/lib/wallet-download-button/wallet-download-button.tsx
@@ -15,19 +15,19 @@ export function WalletDownloadButton({
   title?: string;
 }) {
   return (
-    <div
+    <span
       className={clsx(
         'group flex w-fit min-w-[172px] select-none items-center justify-center gap-x-[8px] rounded-xl px-[24px] py-[10px]',
         'cursor-pointer bg-white text-[#2F2F2F]',
         className,
       )}
     >
-      <div className="group-hover:text-islamic-primary-green group-hover:transition-colors group-hover:duration-300">
+      <span className="group-hover:text-islamic-primary-green group-hover:transition-colors group-hover:duration-300">
         {type === 'apple' && <AppStoreLogoIcon />}
         {type === 'google' && <GooglePlayLogoIcon />}
         {type === 'apk' && <AndroidLogoIcon />}
-      </div>
-      <div className="flex flex-col text-start">
+      </span>
+      <span className="flex flex-col text-start">
         <span className="group-hover:text-islamic-primary-green/50 text-[10px] leading-[12px] text-[#8E8E8E] group-hover:transition-colors group-hover:duration-300">
           {title && title}
         </span>
@@ -36,7 +36,7 @@ export function WalletDownloadButton({
           {type === 'google' && 'Google Play'}
           {type === 'apk' && 'Download .apk'}
         </span>
-      </div>
-    </div>
+      </span>
+    </span>
   );
 }
