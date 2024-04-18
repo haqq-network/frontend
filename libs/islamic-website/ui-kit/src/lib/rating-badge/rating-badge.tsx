b1903e3db2f436f8a6fcb1a67ee8a8729ab3e1dc
Jihad Hadj Husein <skiptomylou999@gmail.com>
2024-04-18 15:43:44 +0300

    chore(islamic-ui-kit): remove unused

--- libs/islamic-website/ui-kit/src/lib/rating-badge/rating-badge.tsx
+++ libs/islamic-website/ui-kit/src/lib/rating-badge/rating-badge.tsx
@@ -1,20 +1,11 @@
 import { RatingStarIcon } from '../icons';
 import { Text } from '../text/text';
 
-<<<<<<< Updated upstream
 export function RatingBadge({ rating }: { rating: number }) {
-=======
-interface RatingBadgeProps {
-  rating: number;
-  storeName: string;
-}
-
-export function RatingBadge({ rating }: RatingBadgeProps) {
   const ratingStars = Array.from({ length: 5 }, (_, i) => {
     return <RatingStarIcon key={i} />;
   });
 
->>>>>>> Stashed changes
   return (
     <div className="flex items-center gap-x-[10px]">
       <div className="relative">
@@ -24,10 +15,6 @@ export function RatingBadge({ rating }: RatingBadgeProps) {
         <div
           className="absolute left-0 top-0 z-10 overflow-hidden"
           style={{ width: (rating / 5) * 100 + '%' }}
-<<<<<<< Updated upstream
-          className="absolute left-0 top-0 z-10"
-=======
->>>>>>> Stashed changes
         >
           <div className="flex w-[96px] flex-row gap-x-[4px] text-[#fcc310]">
             {ratingStars}
