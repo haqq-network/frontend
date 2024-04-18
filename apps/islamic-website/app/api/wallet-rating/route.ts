266a8469e44e0bc176727997146f946d37a9ef91
Jihad Hadj Husein <skiptomylou999@gmail.com>
2024-04-18 15:41:05 +0300

    feat(islamic-website): add wallet rating get route

---
+++ apps/islamic-website/app/api/wallet-rating/route.ts
@@ -0,0 +1,23 @@
+import store from 'app-store-scraper';
+import gplay from 'google-play-scraper';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  const [appStoreResponse, googlePlayResponse] = await Promise.all([
+    store.app({ id: 6443843352 }),
+    gplay.app({ appId: 'com.haqq.wallet' }),
+  ]);
+
+  return NextResponse.json<{
+    appStoreRating: number;
+    googlePlayRating: number;
+  }>(
+    {
+      appStoreRating: appStoreResponse.score,
+      googlePlayRating: googlePlayResponse.score,
+    },
+    {
+      status: 200,
+    },
+  );
+}
