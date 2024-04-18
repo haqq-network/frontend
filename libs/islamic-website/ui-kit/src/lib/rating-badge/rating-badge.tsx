4902d7427a04a59849da9ea2c74a175317a65ae0
Jihad Hadj Husein <skiptomylou999@gmail.com>
2024-04-18 14:10:24 +0300

    refactor(islamic-ui-kit): rating badge

--- libs/islamic-website/ui-kit/src/lib/rating-badge/rating-badge.tsx
+++ libs/islamic-website/ui-kit/src/lib/rating-badge/rating-badge.tsx
@@ -1,6 +1,3 @@
-'use client';
-import clsx from 'clsx';
-import styles from './rating-badge.module.css';
 import { Text } from '../text/text';
 
 interface RatingBadgeProps {
@@ -8,58 +5,71 @@ interface RatingBadgeProps {
   storeName: string;
 }
 
-export function RatingBadge({ rating, storeName }: RatingBadgeProps) {
+export function RatingBadge({ rating }: RatingBadgeProps) {
   return (
-    <div className="flex flex-col gap-y-[6px]">
-      <span className="rtl:font-handjet ltr:font-vcr text-[10px] uppercase leading-[16px] text-white/50">
-        {storeName}
-      </span>
-      <div className="flex items-center gap-x-[10px]">
-        <div className="relative">
-          <svg className="hidden">
-            <symbol id="fivestars">
-              <path
-                d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24"
-                fillRule="evenodd"
-              />
-              <path
-                d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24"
-                fillRule="evenodd"
-                className="translate-x-[24px]"
-              />
-              <path
-                d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24"
-                fillRule="evenodd"
-                className="translate-x-[48px]"
-              />
-              <path
-                d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24"
-                fillRule="evenodd"
-                className="translate-x-[72px]"
-              />
-              <path
-                d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24"
-                fillRule="evenodd"
-                className="translate-x-[96px]"
-              />
-            </symbol>
-          </svg>
-          <div className={styles['rating']}>
-            <progress
-              className={clsx(styles['rating-bg'])}
-              value={rating}
-              max="5"
-            />
-            <svg>
-              <use xlinkHref="#fivestars" />
-            </svg>
+    <div className="flex items-center gap-x-[10px]">
+      <div className="relative">
+        <div className="flex flex-row gap-x-[4px]">
+          <Star />
+          <Star />
+          <Star />
+          <Star />
+          <Star />
+        </div>
+        <div
+          style={{ width: (rating / 5) * 100 + '%' }}
+          className=" absolute left-0 top-0 z-10"
+        >
+          <div className="flex shrink-0 flex-row gap-x-[4px] text-[#fcc310]">
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
           </div>
         </div>
-
-        <Text className="text-[#FCC310]" isMono>
-          {rating.toFixed(1)}
-        </Text>
       </div>
+      <Text className="text-[#FCC310]" isMono>
+        {rating.toFixed(1)}
+      </Text>
     </div>
   );
 }
+
+function Star() {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M8.69134 1.77684C8.44156 1.18539 7.56104 1.18539 7.31127 1.77684L5.86752 5.19551C5.76115 5.44738 5.51256 5.61912 5.22779 5.63748L1.36236 5.8867C0.693626 5.92982 0.421531 6.72612 0.935898 7.13477L3.90903 9.49685C4.12807 9.67087 4.22303 9.94876 4.15339 10.212L3.20818 13.7847C3.04465 14.4028 3.75701 14.8949 4.32468 14.556L7.60593 12.5972C7.84767 12.4529 8.15494 12.4529 8.39668 12.5972L11.6779 14.556C12.2456 14.8949 12.958 14.4028 12.7944 13.7847L11.8492 10.212C11.7796 9.94876 11.8745 9.67087 12.0936 9.49685L15.0667 7.13477C15.5811 6.72612 15.309 5.92982 14.6402 5.8867L10.7748 5.63748C10.49 5.61912 10.2415 5.44738 10.1351 5.19551L8.69134 1.77684Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+
+// function GoldenStar() {
+//   return (
+//     <svg
+//       width="16"
+//       height="16"
+//       viewBox="0 0 16 16"
+//       fill="none"
+//       xmlns="http://www.w3.org/2000/svg"
+//     >
+//       <path
+//         fillRule="evenodd"
+//         clipRule="evenodd"
+//         d="M8.69134 1.77684C8.44156 1.18539 7.56104 1.18539 7.31127 1.77684L5.86752 5.19551C5.76115 5.44738 5.51256 5.61912 5.22779 5.63748L1.36236 5.8867C0.693626 5.92982 0.421531 6.72612 0.935898 7.13477L3.90903 9.49685C4.12807 9.67087 4.22303 9.94876 4.15339 10.212L3.20818 13.7847C3.04465 14.4028 3.75701 14.8949 4.32468 14.556L7.60593 12.5972C7.84767 12.4529 8.15494 12.4529 8.39668 12.5972L11.6779 14.556C12.2456 14.8949 12.958 14.4028 12.7944 13.7847L11.8492 10.212C11.7796 9.94876 11.8745 9.67087 12.0936 9.49685L15.0667 7.13477C15.5811 6.72612 15.309 5.92982 14.6402 5.8867L10.7748 5.63748C10.49 5.61912 10.2415 5.44738 10.1351 5.19551L8.69134 1.77684Z"
+//         fill="#fcc310"
+//       />
+//     </svg>
+//   );
+// }
