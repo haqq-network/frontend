68afea56cafa006b53d82642cbbc007f7ab896de
Jihad Hadj Husein <skiptomylou999@gmail.com>
2024-04-24 13:04:41 +0300

    fix(islamic-website): add some GA

--- apps/islamic-website/app/[locale]/layout.tsx
+++ apps/islamic-website/app/[locale]/layout.tsx
@@ -181,6 +181,21 @@ export default async function LocaleLayout({
                 />
                 <Script
                   async={true}
+                  defer={true}
+                  id="gtm-2"
+                  data-cookiecategory="analytics"
+                  dangerouslySetInnerHTML={{
+                    __html: `
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-WMP75MQS');
+                  `,
+                  }}
+                />
+                <Script
+                  async={true}
                   src="https://www.googletagmanager.com/gtag/js?id=G-5FLBNV5M30"
                   id="gtm-haqq"
                   data-cookiecategory="analytics"
@@ -194,6 +209,12 @@ export default async function LocaleLayout({
                    window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-5FLBNV5M30');
                   `,
                   }}
+                />
+                <Script
+                  async={true}
+                  src="https://www.googletagmanager.com/ns.html?id=GTM-WMP75MQS"
+                  id="gtm-haqq-3"
+                  data-cookiecategory="analytics"
                 />
 
                 <CookieConsentModal />
