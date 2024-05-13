8e0d228214cc26fa89490dbc1bfedf9aa3cdb887
Oleg Shilov <me@olegshilov.com>
2024-05-13 17:02:25 +0700

    fix(haqq-website): validate utmCampaign value in blog post

--- libs/haqq-website/blog/src/lib/blog-post-page.tsx
+++ libs/haqq-website/blog/src/lib/blog-post-page.tsx
@@ -84,7 +84,11 @@ export function BlogPostPage({
           `/blog/${post.slug}`,
           'https://haqq.network',
         ).toString()}
-        utmCampaign={post.utmCampaign}
+        utmCampaign={
+          post.utmCampaign !== null || post.utmCampaign !== ''
+            ? post.utmCampaign
+            : undefined
+        }
       />
 
       <RecentPostsBlock recentPosts={recentPosts} />
