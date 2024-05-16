b9dcd6048b95d4d9e7efec3eff1a9fc089b64341
Oleg Shilov <me@olegshilov.com>
2024-05-16 23:55:55 +0700

    fix(shell): auto connect wallet only if its not connected

--- apps/shell/src/hooks/use-autoconnect.tsx
+++ apps/shell/src/hooks/use-autoconnect.tsx
@@ -1,17 +1,20 @@
 'use client';
 import { useEffect } from 'react';
-import { useConnect } from 'wagmi';
+import { useAccount, useConnect } from 'wagmi';
 
 export function useHaqqWalletAutoConnect() {
+  const { isConnected } = useAccount();
   const { connect, connectors } = useConnect();
 
   useEffect(() => {
-    const connectorInstance = connectors.find((c) => {
-      return c.id === 'injected' && c.name.toLowerCase().includes('haqq');
-    });
+    if (!isConnected) {
+      const connectorInstance = connectors.find((c) => {
+        return c.id === 'injected' && c.name.toLowerCase().includes('haqq');
+      });
 
-    if (connectorInstance) {
-      connect({ connector: connectorInstance });
+      if (connectorInstance) {
+        connect({ connector: connectorInstance });
+      }
     }
-  }, [connect, connectors]);
+  }, [connect, connectors, isConnected]);
 }
