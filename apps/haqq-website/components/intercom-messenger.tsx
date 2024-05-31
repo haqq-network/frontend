7edbf1df3753516b7d88a8b71e0dc02144ebbb50
Oleg Shilov <me@olegshilov.com>
2024-05-31 16:53:14 +0700

    feat(haqq-website): add intercom messenger widget

---
+++ apps/haqq-website/components/intercom-messenger.tsx
@@ -0,0 +1,10 @@
+'use client';
+import Intercom from '@intercom/messenger-js-sdk';
+
+export function IntercomMessenger() {
+  Intercom({
+    app_id: 'klb9yu7o',
+  });
+
+  return null;
+}
--- apps/islamic-website/instrumentation.ts
+++ apps/islamic-website/instrumentation.ts
@@ -1,30 +1,25 @@
 // import * as Sentry from '@sentry/nextjs';
 
-// export async function register() {
-//   if (process.env.NEXT_RUNTIME === 'nodejs') {
-//     Sentry.init({
-//       dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+export async function register() {
+  // if (process.env.NEXT_RUNTIME === 'nodejs') {
+  //   Sentry.init({
+  //     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  //     // Adjust this value in production, or use tracesSampler for greater control
+  //     tracesSampleRate: 1,
+  //     // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  //     debug: false,
+  //     // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  //     // spotlight: process.env.NODE_ENV === 'development',
+  //   });
+  // }
 
-//       // Adjust this value in production, or use tracesSampler for greater control
-//       tracesSampleRate: 1,
-
-//       // Setting this option to true will print useful information to the console while you're setting up Sentry.
-//       debug: false,
-
-//       // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-//       // spotlight: process.env.NODE_ENV === 'development',
-//     });
-//   }
-
-//   if (process.env.NEXT_RUNTIME === 'edge') {
-//     Sentry.init({
-//       dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-
-//       // Adjust this value in production, or use tracesSampler for greater control
-//       tracesSampleRate: 1,
-
-//       // Setting this option to true will print useful information to the console while you're setting up Sentry.
-//       debug: false,
-//     });
-//   }
-// }
+  // if (process.env.NEXT_RUNTIME === 'edge') {
+  //   Sentry.init({
+  //     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  //     // Adjust this value in production, or use tracesSampler for greater control
+  //     tracesSampleRate: 1,
+  //     // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  //     debug: false,
+  //   });
+  // }
+}
