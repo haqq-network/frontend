252c5dee7766c41056c151de74722472c772cb79
Evgeniy Abramov <i@kio.ninja>
2024-06-01 21:53:47 +0800

    chore: up

--- libs/data-access/falconer/src/constants.ts
+++ libs/data-access/falconer/src/constants.ts
@@ -1,2 +1,2 @@
 // export const FALCONER_ENDPOINT = 'https://falconer.haqq.sh' as const;
-export const FALCONER_ENDPOINT = 'https://cb1e5e74ad10.ngrok.app' as const;
+export const FALCONER_ENDPOINT = 'https://6f00f239bbdc.ngrok.app' as const;
