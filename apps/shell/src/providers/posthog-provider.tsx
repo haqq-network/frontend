fcebadc3b7320f7985801817ac60e4c7420aa9df
Oleg Shilov <me@olegshilov.com>
2024-06-25 19:05:00 +0700

    fix(shell): use posthog credentials from client envs

--- apps/shell/src/providers/posthog-provider.tsx
+++ apps/shell/src/providers/posthog-provider.tsx
@@ -2,20 +2,21 @@
 import { PropsWithChildren, useMemo } from 'react';
 import posthog from 'posthog-js';
 import { PostHogProvider } from 'posthog-js/react';
+import { env } from '../env/client';
 
 export function PHProvider({ children }: PropsWithChildren) {
   const postHogInstance = useMemo(() => {
     if (
       typeof window === 'undefined' ||
-      !process.env.NEXT_PUBLIC_POSTHOG_KEY ||
-      !process.env.NEXT_PUBLIC_POSTHOG_HOST
+      !env.NEXT_PUBLIC_POSTHOG_KEY ||
+      !env.NEXT_PUBLIC_POSTHOG_HOST
     ) {
       return undefined;
     }
 
-    const phInstance = posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    const phInstance = posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
       api_host: '/api/ingest',
-      ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      ui_host: env.NEXT_PUBLIC_POSTHOG_HOST,
       capture_pageview: false,
       capture_pageleave: false,
       persistence: 'localStorage+cookie',
