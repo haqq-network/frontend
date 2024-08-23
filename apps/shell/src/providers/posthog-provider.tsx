6529dfcc502faa3da5a0df370c878ca900ea88d1
Oleg Shilov <me@olegshilov.com>
2024-08-24 03:05:46 +0700

    chore(shell): add log about identified wallet user, track pageleave with posthog

--- apps/shell/src/providers/posthog-provider.tsx
+++ apps/shell/src/providers/posthog-provider.tsx
@@ -18,7 +18,7 @@ export function PHProvider({ children }: PropsWithChildren) {
       api_host: '/api/ingest',
       ui_host: env.NEXT_PUBLIC_POSTHOG_HOST,
       capture_pageview: false,
-      capture_pageleave: false,
+      capture_pageleave: true,
       persistence: 'localStorage+cookie',
     });
 
