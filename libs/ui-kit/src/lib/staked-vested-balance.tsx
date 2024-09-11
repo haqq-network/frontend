2802dbc2094f8b77680752299ac99e4cf65a9de7
Oleg Shilov <me@olegshilov.com>
2024-09-11 23:03:19 +0700

    feat(shell): render correct locked balances, add uc dao fund balance and unbonding amount

--- libs/ui-kit/src/lib/staked-vested-balance.tsx
+++ libs/ui-kit/src/lib/staked-vested-balance.tsx
@@ -2,51 +2,83 @@ import { useMemo } from 'react';
 import { formatNumber } from '../server';
 
 export function StakedVestedBalance({
+  available,
+  locked,
   staked,
   vested,
+  daoLocked,
+  unbonding,
 }: {
+  available: number;
+  locked: number;
   staked: number;
   vested: number;
+  daoLocked: number;
+  unbonding: number;
 }) {
-  const { vestedPercent, stakedPercent } = useMemo(() => {
-    const all = vested + staked;
-    const vestedPercent = (vested / all) * 100;
-    const stakedPercent = (staked / all) * 100;
-    return {
-      vestedPercent,
-      stakedPercent,
-    };
-  }, [vested, staked]);
+  const { vestedPercent, stakedPercent, daoLockedPercent, unbondingPercent } =
+    useMemo(() => {
+      const all = vested + staked + daoLocked + unbonding;
+      const vestedPercent = (vested / all) * 100;
+      const stakedPercent = (staked / all) * 100;
+      const daoLockedPercent = (daoLocked / all) * 100;
+      const unbondingPercent = (unbonding / all) * 100;
 
-  if (vested === 0 && staked === 0) {
+      return {
+        vestedPercent,
+        stakedPercent,
+        daoLockedPercent,
+        unbondingPercent,
+      };
+    }, [vested, staked, daoLocked, unbonding]);
+
+  if (vested === 0 && staked === 0 && daoLocked === 0) {
     return null;
   }
 
   return (
     <div className="flex flex-col">
       <div className="flex flex-row gap-[4px]">
+        {vested > 0 && (
+          <div
+            style={{ width: `${vestedPercent}%` }}
+            className="h-[6px] min-w-[6px] rounded-[8px] bg-[#B26F1D]"
+          />
+        )}
         {staked > 0 && (
           <div
             className="h-[6px] min-w-[6px] rounded-[4px] bg-[#0489D4]"
             style={{ width: `${stakedPercent}%` }}
           />
         )}
-        {vested > 0 && (
+        {daoLocked > 0 && (
           <div
-            className="h-[6px] min-w-[6px] rounded-[4px] bg-[#E3A13F]"
-            style={{ width: `${vestedPercent}%` }}
+            style={{ width: `${daoLockedPercent}%` }}
+            className="h-[6px] min-w-[6px] rounded-[8px] bg-[#04D484]"
+          />
+        )}
+        {unbonding > 0 && (
+          <div
+            style={{ width: `${unbondingPercent}%` }}
+            className="h-[6px] min-w-[6px] rounded-[8px] bg-white/50"
           />
         )}
       </div>
-      <div className="flex select-none flex-col justify-between pt-[4px]">
+      <div className="flex select-none flex-col justify-between pt-[4px] text-[12px] font-[500] leading-[18px]">
         {vested > 0 && (
-          <div className="text-[12px] font-[500] leading-[18px] text-[#E3A13F]">
-            Vested: {formatNumber(vested)}
-          </div>
+          <div className="text-[#E3A13F]">Vested: {formatNumber(vested)}</div>
         )}
         {staked > 0 && (
-          <div className="text-[12px] font-[500] leading-[18px] text-[#0489D4]">
-            Staked: {formatNumber(staked)}
+          <div className="text-[#0489D4]">Staked: {formatNumber(staked)}</div>
+        )}
+        {daoLocked > 0 && (
+          <div className="text-[#04D484]">
+            UnitedContributorsDAO: {formatNumber(daoLocked)}
+          </div>
+        )}
+        {unbonding > 0 && (
+          <div className="text-white/50">
+            Unbonding: {formatNumber(unbonding)}
           </div>
         )}
       </div>
