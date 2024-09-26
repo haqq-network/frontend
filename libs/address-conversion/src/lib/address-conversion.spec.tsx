64968d2c8b1c1e7ade037b8e4f4c1000cea6939f
viktoriabakun <viktoriabakun00@gmail.com>
2024-09-26 21:33:33 +0200

    feat(shell): remove address-conversion page test
    Signed-off-by: viktoriabakun <viktoriabakun00@gmail.com>

--- libs/address-conversion/src/lib/address-conversion.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import { AddressConversionPage } from './address-conversion-page';
-
-describe('AddressConversion', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<AddressConversionPage />);
-    expect(baseElement).toBeTruthy();
-  });
-});
