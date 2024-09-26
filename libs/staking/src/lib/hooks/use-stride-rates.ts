2730cb92ece43c2993e9fd4d0e37995aa03f5aef
garageinc <9-b-rinat@rambler.ru>
2024-09-26 22:04:10 +0300

    feat: add rates receiving from stride

---
+++ libs/staking/src/lib/hooks/use-stride-rates.ts
@@ -0,0 +1,39 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+
+const HAQQ_STRIDE_ID = 'haqq_11235-1';
+
+async function fetchRedemptionRate() {
+  const response = await fetch(
+    'https://stride-api.polkachu.com/Stride-Labs/stride/stakeibc/host_zone',
+  );
+  const data = await response.json();
+
+  const haqqZone = data.host_zone.find((zone: any) => {
+    return zone.chain_id === HAQQ_STRIDE_ID;
+  });
+
+  if (haqqZone) {
+    return haqqZone.redemption_rate;
+  } else {
+    throw new Error('Haqq zone not found');
+  }
+}
+
+export function useStrideRates(address?: string) {
+  const {
+    data: redemptionRate,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['redemptionRate', address],
+    queryFn: fetchRedemptionRate,
+    refetchInterval: 10000,
+    enabled: !!address,
+  });
+
+  console.log('redemptionRate', redemptionRate);
+
+  return { redemptionRate, isLoading, error };
+}
