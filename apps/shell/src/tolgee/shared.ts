1b86a4dc025ced089195feba660f5dee12057888
viktoriabakun <viktoriabakun00@gmail.com>
2024-10-24 16:28:53 +0200

    feat: setup namespaces
    Signed-off-by: viktoriabakun <viktoriabakun00@gmail.com>

--- apps/shell/src/tolgee/shared.ts
+++ apps/shell/src/tolgee/shared.ts
@@ -1,16 +1,45 @@
 import { DevTools, Tolgee, FormatSimple } from '@tolgee/web';
 
-export const ALL_LOCALES = ['en', 'ar', 'id', 'tr'];
+export type Locale = 'en' | 'ar' | 'id' | 'tr';
+export const ALL_LOCALES: Locale[] = ['en', 'ar', 'id', 'tr'];
+
+export type Namespace =
+  | 'common'
+  | 'utils'
+  | 'uc-dao'
+  | 'main'
+  | 'stacking'
+  | 'governance'
+  | 'authz';
+export const ALL_NAMESPACES: Namespace[] = [
+  'common',
+  'utils',
+  'uc-dao',
+  'main',
+  'stacking',
+  'governance',
+  'authz',
+];
 
 export const DEFAULT_LOCALE = 'en';
 
 const apiKey = process.env.NEXT_PUBLIC_TOLGEE_API_KEY;
 const apiUrl = process.env.NEXT_PUBLIC_TOLGEE_API_URL;
 
-export async function getStaticData(languages: string[]) {
+export async function getStaticData(languages: Locale[]) {
   const result: Record<string, any> = {};
   for (const lang of languages) {
-    result[lang] = (await import(`../../messages/${lang}.json`)).default;
+    for (const ns of ALL_NAMESPACES) {
+      try {
+        const data = (await import(`../i18n/${ns}/${lang}.json`)).default;
+        result[`${lang}:${ns}`] = data;
+      } catch (error) {
+        console.error(
+          `Error loading namespace "${ns}" for language "${lang}":`,
+          { error, languages, ALL_NAMESPACES },
+        );
+      }
+    }
   }
   return result;
 }
