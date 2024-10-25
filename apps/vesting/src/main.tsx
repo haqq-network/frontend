62aa18cbd4d147597243b752d59d6c1da7e8ea33
Oleg Shilov <me@olegshilov.com>
2024-10-25 21:49:55 +0700

    fix(monitoring): adjust Sentry replay sampling rates

--- apps/vesting/src/main.tsx
+++ apps/vesting/src/main.tsx
@@ -6,15 +6,17 @@ import { AppProviders } from './providers/app-providers';
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 1.0,
+  debug: false,
+  replaysOnErrorSampleRate: 0.15,
+  replaysSessionSampleRate: 0.05,
   integrations: [
     Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: true,
+      blockAllMedia: true,
+    }),
   ],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
 function startApp() {
