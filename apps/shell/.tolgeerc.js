fe3c1ddc282c615f8d46c5223769f65bd55d050f
viktoriabakun <viktoriabakun00@gmail.com>
2024-10-29 15:08:58 +0100

    chore: move tolgee config to apps, add tolgee scripts
    Signed-off-by: viktoriabakun <viktoriabakun00@gmail.com>

--- apps/shell/.tolgeerc.js
+++ apps/shell/.tolgeerc.js
@@ -2,31 +2,31 @@ module.exports = {
   $schema: 'https://tolgee.io/cli-schema.json',
   projectId: 10781,
   format: 'JSON_TOLGEE',
-  patterns: ['./libs/**/*.ts?(x)'],
+  patterns: ['../../libs/**/*.ts?(x)'],
   defaultNamespace: 'common',
   push: {
     files: [
       {
-        path: './apps/shell/src/i18n/en.json',
+        path: './src/i18n/en.json',
         language: 'en',
       },
       {
-        path: './apps/shell/src/i18n/ar.json',
+        path: './src/i18n/ar.json',
         language: 'ar',
       },
       {
-        path: './apps/shell/src/i18n/id.json',
+        path: './src/i18n/id.json',
         language: 'id',
       },
       {
-        path: './apps/shell/src/i18n/tr.json',
+        path: './src/i18n/tr.json',
         language: 'tr',
       },
     ],
     forceMode: 'OVERRIDE',
   },
   pull: {
-    path: './apps/shell/src/i18n',
+    path: './src/i18n',
   },
   apiKey: process.env.NEXT_PUBLIC_TOLGEE_API_KEY,
 };
