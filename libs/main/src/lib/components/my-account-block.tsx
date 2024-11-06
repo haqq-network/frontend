a218055cf3a08773774f6c7777098969c14a8e2c
Oleg Shilov <me@olegshilov.com>
2024-11-07 01:29:21 +0700

    chore: check tolge pull
    translations was updated in tolgee webapp

--- libs/main/src/lib/components/my-account-block.tsx
+++ libs/main/src/lib/components/my-account-block.tsx
@@ -280,7 +280,7 @@ function MyAccountConnected({
             </div>
           </div>
           <MyAccountAmountBlock
-            title={t('regular-staked', 'Regular STAKED', { ns: 'main' })}
+            title={t('regular-staked', 'Regular staked', { ns: 'main' })}
             value={`${formatNumber(balances.staked)} ${symbol.toLocaleUpperCase()}`}
           />
           <MyAccountAmountBlock
@@ -288,7 +288,7 @@ function MyAccountConnected({
             value={`${formatNumber(rewards)} ${symbol.toLocaleUpperCase()}`}
           />
           <MyAccountAmountBlock
-            title={t('liquid-staked', 'Liquid STAKED', { ns: 'main' })}
+            title={t('liquid-staked', 'Liquid staked', { ns: 'main' })}
             value={`${formatNumber(stIslmBalance)} stISLM`}
             subValue={`≈${formatNumber(islmAmountFromStIslm)} ISLM`}
           />
