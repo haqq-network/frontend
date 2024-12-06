9dfdde23f27ffb115766614963b6024bf18a4b63
Oleg Shilov <me@olegshilov.com>
2024-12-06 17:31:22 +0700

    feat(shared): use wallet backend in indexer balances query

--- libs/shared/src/hooks/use-indexer-balance-query/use-indexer-balance-query.tsx
+++ libs/shared/src/hooks/use-indexer-balance-query/use-indexer-balance-query.tsx
@@ -2,68 +2,6 @@ import { useQuery } from '@tanstack/react-query';
 import { nanoid } from 'nanoid';
 import { formatUnits } from 'viem';
 import { useAccount, useChains } from 'wagmi';
-import { haqqTestedge2 } from 'wagmi/chains';
-
-function createRequest(address: string, date: Date) {
-  return {
-    jsonrpc: '2.0',
-    id: nanoid(),
-    method: 'updates',
-    params: [[address], date.toISOString()],
-  };
-}
-
-export interface IndexerBalancesResponse {
-  /** List of addresses in response */
-  addresses: string[];
-  /** Balance available for spending */
-  available: Record<string, string>;
-  /** Total balance minus amount available for staking */
-  available_for_stake: Record<string, string>;
-  /** Current balance of the address */
-  balance: Record<string, string>;
-  /** Timestamp of last update */
-  last_update: string;
-  /** All locked tokens (schedule + staking) */
-  locked: Record<string, string>;
-  /** List of NFTs owned by address */
-  nfts: string[];
-  /** Token exchange rates */
-  rates: {
-    axlUSDC: Array<{
-      amount: string;
-      denom: string;
-    }>;
-  };
-  /** Total amount staked by user */
-  staked: Record<string, string>;
-  /** Amount of freely staked coins */
-  staked_free: Record<string, string>;
-  /** Amount locked in staking */
-  staked_locked: Record<string, string>;
-  /** Amount of vested tokens in staking */
-  staked_vested: Record<string, string>;
-  /** List of tokens owned by address */
-  tokens: string[];
-  /** Total balance including staking and available for staking */
-  total: Record<string, string>;
-  /** Total amount of locked tokens */
-  total_locked: Record<string, string>;
-  /** Total amount involved in staking */
-  total_staked: Record<string, string>;
-  /** List of transactions */
-  transactions: string[];
-  /** Tokens in unbonding period */
-  unbounding: Record<string, string>;
-  /** Unlock schedule timestamps */
-  unlock: Record<string, number>;
-  /** Locked tokens on balance */
-  vested: Record<string, string>;
-  /** Sum of all user tokens aISLM and aLIQUIDXXXX 1:1 */
-  dao_locked: Record<string, string>;
-  /** Uncollected staking rewards */
-  rewards: Record<string, string>;
-}
 
 export interface IndexerBalances {
   available: number;
@@ -80,37 +18,105 @@ export interface IndexerBalances {
   rewards: number;
 }
 
-const INDEXER_ENDPOINT = 'https://jsonrpc.indexer.haqq.network';
-const TESTEDGE_INDEXER_ENDPOINT =
-  'https://jsonrpc.indexer.testedge2.haqq.network';
+type IndexerBalance = Array<[string, number, string]>;
+type IndexerDate = Array<[string, number, number]>;
 
-// Helper function to safely parse numbers
-function safeParse(field: Record<string, string>, address: string) {
-  return address in field
-    ? Number.parseFloat(formatUnits(BigInt(field[address]), 18))
-    : 0;
+interface TokenRate {
+  amount: string;
+  denom: string;
 }
 
-function mapBalances(
-  balancesResponse: IndexerBalancesResponse,
-  address: string,
-): IndexerBalances {
+interface IndexerV2UpdatesResponse {
+  addresses: unknown[]; // Unused
+  available: IndexerBalance;
+  available_for_stake: IndexerBalance;
+  balance: IndexerBalance;
+  chain_id: number;
+  dao_locked: IndexerBalance;
+  last_update: string;
+  locked: IndexerBalance;
+  nfts: unknown[];
+  rates: Record<string, Record<string, TokenRate[]>>;
+  rewards: IndexerBalance;
+  staked: IndexerBalance;
+  staked_free: IndexerBalance;
+  staked_locked: IndexerBalance;
+  staked_vested: IndexerBalance;
+  tokens: unknown[]; // Unused
+  total: IndexerBalance;
+  total_locked: IndexerBalance;
+  total_staked: IndexerBalance;
+  unbounding: IndexerBalance;
+  unlock: IndexerDate;
+  vested: IndexerBalance;
+}
+
+// Create a request to the indexer
+function createRequest(
+  addresses: Record<number, string[]>,
+  date: Date,
+  currency = 'USD',
+) {
   return {
-    available: safeParse(balancesResponse.available, address),
-    availableForStake: safeParse(balancesResponse.available_for_stake, address),
-    balance: safeParse(balancesResponse.balance, address),
-    locked: safeParse(balancesResponse.locked, address),
-    staked: safeParse(balancesResponse.staked, address),
-    stakedFree: safeParse(balancesResponse.staked_free, address),
-    stakedLocked: safeParse(balancesResponse.staked_locked, address),
-    total: safeParse(balancesResponse.total, address),
-    vested: safeParse(balancesResponse.vested, address),
-    daoLocked: safeParse(balancesResponse.dao_locked, address),
-    unbonding: safeParse(balancesResponse.unbounding, address),
-    rewards: safeParse(balancesResponse.rewards, address),
+    jsonrpc: '2.0',
+    id: nanoid(),
+    method: 'updates_v2',
+    params: [addresses, date.toISOString(), currency],
   };
 }
 
+// Parse the balance from the indexer response
+function safeParseBalance(
+  balances: IndexerBalance,
+  address: string,
+  chainId: number,
+): number {
+  const balance = balances.find(([addr, chain]) => {
+    return addr === address && chain === chainId;
+  });
+
+  if (!balance) {
+    return 0;
+  }
+
+  return Number.parseFloat(formatUnits(BigInt(balance[2]), 18));
+}
+
+// Map the balances from the indexer response to the IndexerBalances type
+function mapBalances(
+  balancesResponse: IndexerV2UpdatesResponse,
+  address: string,
+  chainId: number,
+): IndexerBalances {
+  return {
+    available: safeParseBalance(balancesResponse.available, address, chainId),
+    availableForStake: safeParseBalance(
+      balancesResponse.available_for_stake,
+      address,
+      chainId,
+    ),
+    balance: safeParseBalance(balancesResponse.balance, address, chainId),
+    locked: safeParseBalance(balancesResponse.locked, address, chainId),
+    staked: safeParseBalance(balancesResponse.staked, address, chainId),
+    stakedFree: safeParseBalance(
+      balancesResponse.staked_free,
+      address,
+      chainId,
+    ),
+    stakedLocked: safeParseBalance(
+      balancesResponse.staked_locked,
+      address,
+      chainId,
+    ),
+    total: safeParseBalance(balancesResponse.total, address, chainId),
+    vested: safeParseBalance(balancesResponse.vested, address, chainId),
+    daoLocked: safeParseBalance(balancesResponse.dao_locked, address, chainId),
+    unbonding: safeParseBalance(balancesResponse.unbounding, address, chainId),
+    rewards: safeParseBalance(balancesResponse.rewards, address, chainId),
+  };
+}
+
+// Fetch the balances from the indexer
 export async function indexerBalancesFetcher(
   chainId: number,
   address?: string,
@@ -118,28 +124,30 @@ export async function indexerBalancesFetcher(
   if (!address) {
     return null;
   }
-  const isTestedge = chainId === haqqTestedge2.id;
+
+  const addresses = { [chainId]: [address] };
+
   const requestUrl = new URL(
-    isTestedge ? TESTEDGE_INDEXER_ENDPOINT : INDEXER_ENDPOINT,
+    'https://backend.wallet.production.haqq.network/api/all_networks/jsonrpc',
   );
 
-  const headers = new Headers();
-  headers.append('Content-Type', 'application/json');
+  const headers = new Headers({
+    'Content-Type': 'application/json',
+  });
 
-  const nowDate = new Date(Date.now());
-  const requestBody = createRequest(address, nowDate);
+  const requestBody = createRequest(addresses, new Date());
 
   try {
     const response = await fetch(requestUrl, {
       method: 'POST',
       headers,
       body: JSON.stringify(requestBody),
-      redirect: 'follow',
     });
 
     const responseJson = await response.json();
 
-    return mapBalances(responseJson.result, address);
+    // Map balances for the given chain
+    return mapBalances(responseJson.result, address, chainId);
   } catch (error) {
     console.error((error as Error).message);
     return null;
@@ -151,7 +159,7 @@ export function useIndexerBalanceQuery(address?: string) {
   const { chain = chains[0] } = useAccount();
 
   return useQuery({
-    queryKey: [chain.id, 'indexer-balance', address],
+    queryKey: ['indexer-balance', address, chain.id],
     enabled: !!address,
     queryFn: async () => {
       return await indexerBalancesFetcher(chain.id, address);
