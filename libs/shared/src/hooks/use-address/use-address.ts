54290d6dcfa3f42cf63c00dac19a489871ca97a7
garageinc <9-b-rinat@rambler.ru>
2024-12-16 16:32:21 +0300

    chore: self review

--- libs/shared/src/hooks/use-address/use-address.ts
+++ libs/shared/src/hooks/use-address/use-address.ts
@@ -1,11 +1,10 @@
 import { useMemo } from 'react';
-import { Hex } from 'viem';
 import { useAccount } from 'wagmi';
 import { ethToHaqq } from '../../utils/convert-address';
 
 export function useAddress() {
-  //const { address: ethAddress } = useAccount();
-  const ethAddress = '0x912A3b8cF600CbDD71ffC4224e80501221482346' as Hex;
+  const { address: ethAddress } = useAccount();
+  // const ethAddress = '0x912A3b8cF600CbDD71ffC4224e80501221482346' as Hex;
   const haqqAddress = useMemo(() => {
     if (ethAddress) {
       return ethToHaqq(ethAddress);
