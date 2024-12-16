e5e5de3471f10500059c56039d295404aa0238bb
garageinc <9-b-rinat@rambler.ru>
2024-12-16 16:40:16 +0300

    chore: self review

--- libs/shared/src/utils/get-amount-include-fee.spec.ts
+++ libs/shared/src/utils/get-amount-include-fee.spec.ts
@@ -1,9 +1,14 @@
+import { parseUnits } from 'viem';
 import { DEFAULT_FEE } from '@haqq/data-access-cosmos';
 import { getAmountIncludeFee } from './get-amount-include-fee';
 
 describe('getAmountIncludeFee', () => {
   it('should return correct object', () => {
-    const result = getAmountIncludeFee(100, 1000, DEFAULT_FEE);
+    const result = getAmountIncludeFee(
+      parseUnits('100', 18),
+      parseUnits('1000', 18),
+      DEFAULT_FEE,
+    );
 
     expect(result).toHaveProperty('amount');
     expect(result).toHaveProperty('denom');
@@ -13,30 +18,32 @@ describe('getAmountIncludeFee', () => {
   });
 
   it('should handle amount equal to 0', () => {
-    const result = getAmountIncludeFee(0, 1000, DEFAULT_FEE);
+    const result = getAmountIncludeFee(0n, parseUnits('1000', 18), DEFAULT_FEE);
     const expectedAmount = BigInt(0);
+
+    console.log(result);
 
     expect(result.amount).toBe(expectedAmount.toString());
   });
 
   it('should handle large amount', () => {
-    const amount = 123456789;
-    const balance = 1300000000;
-    const expectedAmount = BigInt(amount * 10 ** 18);
+    const amount = parseUnits('123456789', 18);
+    const balance = parseUnits('1300000000', 18);
+    const expectedAmount = amount;
     const result = getAmountIncludeFee(amount, balance, DEFAULT_FEE);
 
     expect(result.amount).toEqual(expectedAmount.toString());
   });
 
   it('should correctly calculate amount with fee', () => {
-    const amount = 1000;
-    const balance = 1000;
+    const amount = parseUnits('1000', 18);
+    const balance = parseUnits('1000', 18);
     const fee = {
       amount: '500000000000000000',
       denom: 'aISLM',
       gas: '21000',
     };
-    const expectedAmount = BigInt(amount * 10 ** 18) - BigInt(fee.amount);
+    const expectedAmount = amount - BigInt(fee.amount);
     const result = getAmountIncludeFee(amount, balance, fee);
 
     expect(result).toEqual({
