1ed96d0713be9d479907e184d2f2e43c2e25ab45
viktoriabakun <viktoriabakun00@gmail.com>
2025-01-13 18:57:29 +0100

    fix: isolate client component in validator-details-page
    Signed-off-by: viktoriabakun <viktoriabakun00@gmail.com>

--- libs/staking/src/lib/validator-details-page.tsx
+++ libs/staking/src/lib/validator-details-page.tsx
@@ -1,19 +1,13 @@
 import { Fragment } from 'react';
-import { useTranslate } from '@tolgee/react';
-import Link from 'next/link';
-import { BackButton, Container } from '@haqq/shell-ui-kit/server';
+import { Container } from '@haqq/shell-ui-kit/server';
+import { BackToStaking } from './components/back-to-staking';
 import { ValidatorInfo } from './components/validator-info';
 
 export function ValidatorDetailsPage({ address }: { address: string }) {
-  const { t } = useTranslate();
   return (
     <Fragment>
       <Container>
-        <div className="py-[18px] sm:py-[26px] lg:py-[34px]">
-          <Link href="/staking">
-            <BackButton>{t('staking', 'Staking', { ns: 'common' })}</BackButton>
-          </Link>
-        </div>
+        <BackToStaking />
       </Container>
 
       <ValidatorInfo validatorAddress={address} />
