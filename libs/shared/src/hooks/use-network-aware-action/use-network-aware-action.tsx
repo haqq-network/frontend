c6a0a9eefa998ebd96388fd394ba7221da3759dc
garageinc <9-b-rinat@rambler.ru>
2025-07-07 19:03:28 +0300

    fix: claim rewards without precompiles

--- libs/shared/src/hooks/use-network-aware-action/use-network-aware-action.tsx
+++ libs/shared/src/hooks/use-network-aware-action/use-network-aware-action.tsx
@@ -1,19 +1,24 @@
 import { useCallback } from 'react';
 import { useWallet } from '../../providers/wallet-provider';
+import { useToast } from '../use-toast/use-toast';
 
 export function useNetworkAwareAction() {
   const { isNetworkSupported, openSelectChain } = useWallet();
+
+  const toast = useToast();
 
   const executeIfNetworkSupported = useCallback(
     (callback: () => void) => {
       if (isNetworkSupported) {
         callback();
       } else {
-        console.warn('Network not supported');
+        toast.error(
+          'Network not supported, please switch to supported network',
+        );
         openSelectChain();
       }
     },
-    [isNetworkSupported, openSelectChain],
+    [isNetworkSupported, openSelectChain, toast],
   );
 
   return { executeIfNetworkSupported };
