27db21f86d6b9b0cac6463d7c929417a06556c93
garageinc <9-b-rinat@rambler.ru>
2025-09-12 11:58:30 +0300

    fix: rpc url

--- libs/shared/src/utils/bridge.ts
+++ libs/shared/src/utils/bridge.ts
@@ -1,6 +1,6 @@
 import { sepolia } from 'viem/chains';
 
-const haqqDevnet1RpcUrl = 'https://65.21.69.176:8545/';
+const haqqDevnet1RpcUrl = 'https://rpc.devnet2.dev.haqq.network/';
 export const haqqDevnet1 = {
   id: 64322, // 0xfb42 in hex
   name: 'HAQQ L2',
