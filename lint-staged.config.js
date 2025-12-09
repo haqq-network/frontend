59ead76a864092e15b22bd1aec11611c777f4405
garageinc <9-b-rinat@rambler.ru>
2025-12-09 18:51:47 +0300

    chore: save bun migration

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   '*.{ts,js,tsx,jsx,cjs,mjs}': [
-    () => 'pnpm exec nx affected:lint --fix --uncommitted',
-    () => 'pnpm format',
+    () => 'bun run nx affected:lint --fix --uncommitted',
+    () => 'bun run format',
   ],
-  '*.{css,json,md,mdx,html,yaml,yml}': ['pnpm exec prettier --write'],
+  '*.{css,json,md,mdx,html,yaml,yml}': ['bun run prettier --write'],
 };
