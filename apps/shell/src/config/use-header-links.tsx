dcd082f9285a6cd0e5b1322efe61397f67a8f025
garageinc <9-b-rinat@rambler.ru>
2026-01-27 16:06:09 +0300

    chore: hide link

--- apps/shell/src/config/use-header-links.tsx
+++ apps/shell/src/config/use-header-links.tsx
@@ -11,12 +11,6 @@ export const useHeaderLinks = (): HeaderLink[] => {
   return [
     {
       type: 'link',
-      label: t('waitlist', 'Waitlist'),
-      href: '/waitlist',
-      chains: allowedChains,
-    },
-    {
-      type: 'link',
       label: t('bridge', 'Bridge to L2'),
       href: '/bridge',
       chains: allowedChains,
