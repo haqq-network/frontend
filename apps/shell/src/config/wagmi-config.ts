7d95cb554347c21c81f9ea1afa00bb7e293c1d3a
garageinc <9-b-rinat@rambler.ru>
2026-02-26 20:47:12 +0300

    chore: change rest cosmos url

--- apps/shell/src/config/wagmi-config.ts
+++ apps/shell/src/config/wagmi-config.ts
@@ -22,7 +22,7 @@ const supportedChainsTransports = supportedChains.reduce(
 );
 
 // TODO: temporary RPC override for burn testing on mainnet
-supportedChainsTransports[11235] = http('https://rest.haqqdev.subto.ru', {
+supportedChainsTransports[11235] = http('https://rest-haqqdev.subto.ru', {
   batch: true,
 });
 
